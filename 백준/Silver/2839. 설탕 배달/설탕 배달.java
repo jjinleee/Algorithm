f2a6f33f8627f0747273e96b2c8a6aab9e16f888
진 <93428123+jjinleee@users.noreply.github.com>
2025-02-06 22:26:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,24 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int N=Integer.parseInt(br.readLine());
-    
-        //5킬로 3킬로 하나씩 늘려가면서 N이 되는것 구하기
-        int result=-1;
+        int n=Integer.parseInt(br.readLine());
 
-        for(int i=0;i<=1000;i++){
-            for(int j=0;j<1700;j++){
-                if(5*i+3*j==N) {
-                    result=i+j;
-                } 
+        int cnt=0;
+
+        while(n>=0){
+            if(n%5==0) {
+                cnt+=n/5;
+                System.out.println(cnt);
+                return;
             }
+            n-=3;
+            cnt++;
         }
+        cnt= -1;
 
-        System.out.println(result);
-       
+        System.out.println(cnt);
     }
 }
