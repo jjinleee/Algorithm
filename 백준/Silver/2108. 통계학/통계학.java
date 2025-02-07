6d4569cda1c9c55e45bad47dd2c6bbf8e37a5460
진 <93428123+jjinleee@users.noreply.github.com>
2025-02-08 00:29:31 +0900

    [Silver III] Title: 통계학, Time: 612 ms, Memory: 65256 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,42 +4,49 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        
-        int N=Integer.parseInt(br.readLine());
-        int[] nums= new int[N];
-        Map<Integer, Integer> time=new HashMap<>();
-        int sum=0;
+        StringBuilder sb= new StringBuilder();
 
-        for(int i=0;i<N;i++){
-            nums[i]=Integer.parseInt(br.readLine());
-            sum+=nums[i];
-            time.put(nums[i], time.getOrDefault(nums[i], 0)+1);
+        int n=Integer.parseInt(br.readLine());
+        int[] nums= new int[n];
+        HashMap<Integer,Integer> cnt=new HashMap<>();
+
+        int sum=0;
+        for(int i=0;i<n;i++){
+            int number=Integer.parseInt(br.readLine());
+            nums[i]=number;
+            sum+=number;
+            cnt.put(number, cnt.getOrDefault(number, 0)+1);
         }
         Arrays.sort(nums);
 
-        StringBuilder sb=new StringBuilder();
-        //산술평균
-        double mean=(double)sum/N;
-        sb.append(Math.round(mean)+"\n");
-        //중앙값
-        sb.append(nums[N/2]+"\n");
-        //최빈값
-        List<Map.Entry<Integer, Integer>> timeList=new ArrayList<>(time.entrySet());
-        timeList.sort((a,b)->{
-            if(b.getValue().equals(a.getValue())){
-                return Integer.compare(a.getKey(), b.getKey());
+        int avg=(int) Math.round((double)sum/n);  //산술평균
+        int mid=nums[n/2];  //중앙값(n은홀수)
+
+        //최빈값, 여러개 있을땐 두번째로 작은값
+        List<Map.Entry<Integer,Integer>> keyList=new ArrayList<>(cnt.entrySet());
+        keyList.sort((o1,o2)-> {
+            if(o1.getValue().equals(o2.getValue())){
+                return Integer.compare(o1.getKey(), o2.getKey());
             }
-            return Integer.compare(b.getValue(), a.getValue());
+            return Integer.compare(o2.getValue(), o1.getValue());
         });
 
-        if(timeList.size()>1 && timeList.get(0).getValue().equals(timeList.get(1).getValue())){
-            sb.append(timeList.get(1).getKey()+"\n");
-        } else sb.append(timeList.get(0).getKey()+"\n");
-        //범위
-        //양수,음수
-        sb.append(nums[N-1]-nums[0]);
+        int mode;
+        if (keyList.size() > 1 && keyList.get(0).getValue().equals(keyList.get(1).getValue())) {
+            mode = keyList.get(1).getKey(); // 두 번째로 작은 최빈값 선택
+        } else {
+            mode = keyList.get(0).getKey(); // 단일 최빈값이면 그대로 반환
+        }
+
+
+        //범위:최대-최소
+        int range=Math.abs(nums[n-1]-nums[0]);
+        sb.append(avg).append("\n")
+        .append(mid).append("\n")
+        .append(mode).append("\n")
+        .append(range);
 
         System.out.println(sb.toString());
 
-    }
+    }       
 }
