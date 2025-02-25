6df1e8aa22b244cf58f90829c61d1264cdc6e6af
진 <93428123+jjinleee@users.noreply.github.com>
2025-02-25 15:03:13 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,24 +1,24 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
 
-        int cnt=0;
 
+        int cnt=0;
+        
         while(n>=0){
-            if(n%5==0) {
+            if(n%5==0){
                 cnt+=n/5;
                 System.out.println(cnt);
                 return;
             }
-            n-=3;
             cnt++;
+            n-=3;
         }
-        cnt= -1;
-
-        System.out.println(cnt);
+        
+        System.out.println(-1);
     }
 }
