bc9dd2cdfa995333ee369d0e97538a3e780de23a
진 <93428123+jjinleee@users.noreply.github.com>
2025-02-25 17:28:07 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 688 ms, Memory: 57128 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -4,25 +4,24 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
 
-        int[][] arr=new int[N][2];
         StringTokenizer st;
-
-        for(int i=0;i<N;i++){
-            String s=br.readLine();
-            st=new StringTokenizer(s);
-            arr[i][0]= Integer.parseInt(st.nextToken());
-            arr[i][1]= Integer.parseInt(st.nextToken());
+        int[][] dot=new int[n][2];
+        for(int i=0;i<n;i++){
+            st=new StringTokenizer(br.readLine());
+            dot[i][0]=Integer.parseInt(st.nextToken());
+            dot[i][1]=Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (o1,o2) -> (o1[1] == o2[1] ? o1[0] - o2[0] : o1[1] - o2[1]));
+        Arrays.sort(dot, (a,b)->(a[1]==b[1]? a[0]-b[0]:a[1]-b[1]));
 
-
-        for(int i=0;i<N;i++){
-            System.out.println(arr[i][0]+" "+arr[i][1]);
+        StringBuilder sb= new StringBuilder();
+        for(int[] p:dot){
+            sb.append(p[0]+" "+p[1]+"\n");
         }
 
+        System.out.println(sb.toString());
     }
 }
