f67694aa9704fb3d61a0ba47332704a9a847f3a4
진 <93428123+jjinleee@users.noreply.github.com>
2025-02-26 16:34:17 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1084 ms, Memory: 50676 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,34 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String s=br.readLine();
-        StringTokenizer st=new StringTokenizer(s);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n=Integer.parseInt(st.nextToken());
-       
-        HashMap<String, String> map=new HashMap<String,String>();
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i=0;i<n;i++){
-            s=br.readLine();
-            st=new StringTokenizer(s);
 
+
+        Set<String> have=new TreeSet<>(Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             String name=st.nextToken();
-            String state=st.nextToken();
+            String log=st.nextToken();
 
-            if(map.containsKey(name)) {map.remove(name);}
-            else  map.put(name,state);
+            if(log.equals("enter")){
+                have.add(name);
+            } else if(log.equals("leave")){
+                have.remove(name);
+            }
+        }   
+
+        for(String s: have){
+            System.out.println(s);
         }
-        List<String> enter_people=new ArrayList<>(map.keySet());
-
-        Collections.sort(enter_people,Collections.reverseOrder());
-
-        for(String t: enter_people){
-            System.out.println(t);
-        }
-       
+        
     }
 }
