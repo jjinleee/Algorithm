ed0e56f836fd5a30bf1b279d250fa6ef91c3cce8
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-01 15:38:05 +0900

    [level 1] Title: 모의고사, Time: 2.61 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,31 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] patterns={
-            {1,2,3,4,5},
-            {2,1,2,3,2,4,2,5},
-            {3,3,1,1,2,2,4,4,5,5}
-        };
+        int[] answer = {};
+        int[] p1={1,2,3,4,5};
+        int[] p2={2,1,2,3,2,4,2,5};
+        int[] p3={3,3,1,1,2,2,4,4,5,5};
         
-        int[] scores=new int[3];
+        int[] s=new int[3];
         
         for(int i=0;i<answers.length;i++){
-            for(int j=0;j<patterns.length;j++){
-                if(answers[i]==patterns[j][i%patterns[j].length]){
-                    scores[j]++;
-                }
-            }
-        }
-        int maxScore=Arrays.stream(scores).max().getAsInt();
-        ArrayList<Integer> answer=new ArrayList<>();
-        
-        for(int i=0;i<scores.length;i++){
-            if(scores[i]==maxScore){
-                answer.add(i+1);
-            }
+            if(answers[i]==p1[i%5]) s[0]++;
+            if(answers[i]==p2[i%8]) s[1]++;
+            if(answers[i]==p3[i%10]) s[2]++;
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int maxScore = Math.max(s[0], Math.max(s[1], s[2]));
+
+        
+        List<Integer> grade=new ArrayList<>();
+        for(int i=0;i<3;i++){
+            if(maxScore==s[i]){
+                grade.add(i+1);
+            }
+        }
+        
+        
+        return grade.stream().mapToInt(i->i).toArray();
     }
 }
