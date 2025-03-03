7d6a00e5097b0190299198b3a50fa9a8e403bba1
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-04 00:20:52 +0900

    [Silver III] Title: 소수 구하기, Time: 276 ms, Memory: 19676 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,32 +1,32 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
 
         int m=Integer.parseInt(st.nextToken());
         int n=Integer.parseInt(st.nextToken());
         
-        StringBuilder sb= new StringBuilder();
+        StringBuilder sb =new StringBuilder();
 
         for(int i=m;i<=n;i++){
             if(isPrime(i)) sb.append(i).append("\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb);
     }
 
-    private static boolean isPrime(int i){
-        if(i<2) return false;
-        if(i==2) return true;
-        if(i%2==0) return false;
-        
-        for(int j=3;j<=Math.sqrt(i);j++){
-            if(i%j==0) return false;
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
         }
-        
+
         return true;
     }
 }
