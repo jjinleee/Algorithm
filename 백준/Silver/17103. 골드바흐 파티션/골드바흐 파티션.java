23c6d9e356891520b86eb728af51e2f88fbf36a7
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-08 15:52:55 +0700

    [Silver II] Title: 골드바흐 파티션, Time: 184 ms, Memory: 15372 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,35 +1,46 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static boolean[] isPrime;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb= new StringBuilder();
 
-        int n=Integer.parseInt(br.readLine());
+        int n= Integer.parseInt(br.readLine());
 
-        boolean[] primes=new boolean[1000001];
-        primes[0]=primes[1]=true;
+        sieve(1000000);
 
-        //수 입력받기
-        for(int i=2;i*i<=1000000;i++){
-            if(!primes[i]){
-                for(int j=i+i;j<=1000000;j+=i){
-                    primes[j]=true;
+        for(int i=0;i<n;i++){
+            int num=Integer.parseInt(br.readLine());
+
+            sb.append(count(num)).append('\n');
+        }
+        
+        
+        System.out.print(sb);
+    }
+    public static void sieve(int max){
+        isPrime=new boolean[max+1];
+        for(int i=2;i<=max;i++){
+            isPrime[i]=true;
+        }
+        for(int i=2;i*i<=max;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=max;j+=i){
+                    isPrime[j]=false;
                 }
             }
         }
-
-        for(int i=0;i<n;i++){
-            int temp=Integer.parseInt(br.readLine());
-            int ans=0;
-            for(int j=2;j<=temp/2;j++){
-                if(!primes[j] && !primes[temp-j]) ans++;
-            }
-            System.out.println(ans);
-        }
-
-
     }
 
-   
+    public static int count(int n) {
+        int cnt=0;
+        for(int a=2;a<=n/2;a++){
+            int b=n-a;
+            if(isPrime[a] && isPrime[b]) cnt++;
+        }
+        return cnt;
+    }
 }
