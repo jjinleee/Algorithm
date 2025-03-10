0fb9dae34c63ac39122c70b453e8dceac7880eae
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-10 23:04:50 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
 
-        int t=Integer.parseInt(br.readLine());
-        String[] answer=new String[t];
+        StringBuilder sb= new StringBuilder();
 
-        for(int i=0;i<t;i++){
+        for(int i=0;i<n;i++){
+            Stack<Character> stack= new Stack<>();
             String s=br.readLine();
-            if(isVPS(s)) answer[i]="YES";
-            else answer[i]="NO";
-        }
+            boolean isValid=true;
 
-        for(String a:answer){
-            System.out.println(a);
-        }
-    }
-
-    public static boolean isVPS(String t){
-        Stack<Character> data=new Stack<>();
-        for(int i=0;i<t.length();i++){{
-            if(t.charAt(i)=='(') data.push('(');
-            else {
-                if(data.isEmpty()) return false;
-                data.pop();
+            for(char c: s.toCharArray()){
+                if(c=='(') stack.push('(');
+                else{
+                    if(stack.isEmpty()){
+                        isValid=false;
+                        break;
+                    }
+                    stack.pop();
+                }
             }
-        }}
-        if(data.isEmpty()) return true;
-        else return false;
+
+            if(isValid && stack.isEmpty()) sb.append("YES").append('\n');
+            else sb.append("NO").append('\n');
+        }
+
+        System.out.println(sb);
     }
 }
