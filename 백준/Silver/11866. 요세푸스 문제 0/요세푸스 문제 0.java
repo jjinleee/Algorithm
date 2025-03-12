10143f2e4cfe6f68f56e02f7d527b2e34a906dc8
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-12 14:02:24 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 124 ms, Memory: 16520 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -2,37 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        Queue<Integer> queue = new LinkedList<>();
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=1;i<=n;i++){
-            queue.add(i);
-        }
-
-        while(!queue.isEmpty()){
-            for(int i=0;i<k-1;i++){//뒤로보냄
-                queue.offer(queue.poll());
-            }
-            result.add(queue.poll()); //k번째 빼서 result에 추가
-        }
+        int n=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
 
         StringBuilder sb= new StringBuilder();
         sb.append("<");
 
-        for(int i=0;i<n;i++){
-            sb.append(result.get(i));
-            if(i!=result.size()-1) sb.append(", ");
+        ArrayDeque<Integer> list=new ArrayDeque<>();
+        for(int i=1;i<=n;i++ ){
+            list.add(i);
         }
+        while(list.size()>1){
+            for(int i=0;i<k-1;i++){
+                list.addLast(list.pollFirst());
+            }
+            sb.append(list.pollFirst()).append(", ");
+        }
+        sb.append(list.peek());
         sb.append(">");
 
+        System.out.println(sb);
 
-        System.out.println(sb.toString());
     }
 }
