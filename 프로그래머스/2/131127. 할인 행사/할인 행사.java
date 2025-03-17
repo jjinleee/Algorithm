6ed54e73efb74f680b08a2023e1c8668dad907f5
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-17 16:36:22 +0900

    [level 2] Title: 할인 행사, Time: 94.87 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,22 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String,Integer> my=new HashMap<>();
+        HashMap<String,Integer> stuff= new HashMap<>();
         for(int i=0;i<want.length;i++){
-            my.put(want[i],number[i]);
+            stuff.put(want[i],number[i]);
         }
+        
         for(int i=0;i<discount.length-9;i++){
             HashMap<String,Integer> market=new HashMap<>();
             for(int j=i;j<i+10;j++){
-              if(my.containsKey(discount[j])){
-                  market.put(discount[j],market.getOrDefault(discount[j],0)+1);
-              }  
+               if(stuff.containsKey(discount[i])){
+                   market.put(discount[j],market.getOrDefault(discount[j],0)+1);
+               }
             }
-            
-            if(market.equals(my)) answer++;
-
+            if(market.equals(stuff)) answer++;
         }
-        
         return answer;
     }
 }
