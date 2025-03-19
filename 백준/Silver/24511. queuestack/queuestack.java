f3ba5509fffa797bbe6345f9d104508b8b17bf74
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-19 22:55:40 +0900

    [Silver III] Title: queuestack, Time: 548 ms, Memory: 58896 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.java
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -4,43 +4,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-
-        //입력받기 
         int n=Integer.parseInt(br.readLine());
-        int[] A= new int[n]; //자료 구조 종류
-
-        StringTokenizer st=new StringTokenizer(br.readLine());
+        
+        int[] A= new int[n];
+        int[] B= new int[n];
+        StringTokenizer st= new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             A[i]=Integer.parseInt(st.nextToken());
         }
-
-        int[] B= new int[n];  //자료구조원소
-        st=new StringTokenizer(br.readLine());
+        st= new StringTokenizer(br.readLine());
         for(int i=0;i<n;i++){
             B[i]=Integer.parseInt(st.nextToken());
         }
 
         int m=Integer.parseInt(br.readLine());
-        int[] C= new int[m];    //삽입할 원소
-        st=new StringTokenizer(br.readLine());
+        int[] C= new int[m];
+        st= new StringTokenizer(br.readLine());
         for(int i=0;i<m;i++){
             C[i]=Integer.parseInt(st.nextToken());
         }
 
-        Deque<Integer> q= new ArrayDeque<>();
+        Deque<Integer> d=new ArrayDeque<>();
 
         for(int i=0;i<n;i++){
-            if(A[i]==0){        //큐면 원소변경
-                q.add(B[i]);
-            }
+            if(A[i]==0) d.add(B[i]);
         }
 
         StringBuilder sb= new StringBuilder();
         for(int i=0;i<m;i++){
-            q.offerFirst(C[i]);
-            sb.append(q.pollLast()+" ");
+            d.offerFirst(C[i]);
+            sb.append(d.pollLast()).append(" ");
         }
-
-        System.out.println(sb.toString());
+        System.out.println(sb);
     }
 }
