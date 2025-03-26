2fd1e3950eb5c3bea269aac0d8f5e175ca488bc8
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-26 14:33:54 +0900

    [Silver II] Title: 통계학, Time: 500 ms, Memory: 49768 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,49 +4,46 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb= new StringBuilder();
-
+  
         int n=Integer.parseInt(br.readLine());
-        int[] nums= new int[n];
-        HashMap<Integer,Integer> cnt=new HashMap<>();
 
+        int[] nums= new int[n];
+        int[] cnt=new int[8001];        
         int sum=0;
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
         for(int i=0;i<n;i++){
-            int number=Integer.parseInt(br.readLine());
-            nums[i]=number;
-            sum+=number;
-            cnt.put(number, cnt.getOrDefault(number, 0)+1);
+            int k=Integer.parseInt(br.readLine());
+            nums[i]=k;
+            sum+=k;
+            cnt[k+4000]++;
+            max=Math.max(max, k);
+            min=Math.min(min,k);
         }
+
         Arrays.sort(nums);
 
-        int avg=(int) Math.round((double)sum/n);  //산술평균
-        int mid=nums[n/2];  //중앙값(n은홀수)
+        int mean=Math.round((float)sum/n);
+        int median= nums[n/2];
+        int modeMax=0;
 
-        //최빈값, 여러개 있을땐 두번째로 작은값
-        List<Map.Entry<Integer,Integer>> keyList=new ArrayList<>(cnt.entrySet());
-        keyList.sort((o1,o2)-> {
-            if(o1.getValue().equals(o2.getValue())){
-                return Integer.compare(o1.getKey(), o2.getKey());
-            }
-            return Integer.compare(o2.getValue(), o1.getValue());
-        });
-
-        int mode;
-        if (keyList.size() > 1 && keyList.get(0).getValue().equals(keyList.get(1).getValue())) {
-            mode = keyList.get(1).getKey(); // 두 번째로 작은 최빈값 선택
-        } else {
-            mode = keyList.get(0).getKey(); // 단일 최빈값이면 그대로 반환
+        for(int c:cnt){
+            if(c>modeMax) modeMax=c;
         }
 
+        List<Integer> modes= new ArrayList<>();
+        for(int i=0;i<8001;i++){
+            if(cnt[i]==modeMax){
+                modes.add(i-4000);
+            }
+        }
+        int mode = modes.size() > 1 ? modes.get(1) : modes.get(0);
+        int range=max-min;
 
-        //범위:최대-최소
-        int range=Math.abs(nums[n-1]-nums[0]);
-        sb.append(avg).append("\n")
-        .append(mid).append("\n")
-        .append(mode).append("\n")
-        .append(range);
-
-        System.out.println(sb.toString());
-
-    }       
+        System.out.println(mean);
+        System.out.println(median);
+        System.out.println(mode);
+        System.out.println(range);
+    }
 }
