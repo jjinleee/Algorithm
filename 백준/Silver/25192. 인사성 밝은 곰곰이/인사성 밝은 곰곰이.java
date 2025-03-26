90862721129022065750b4b31c0fcddb5edd6645
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-26 10:45:25 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 260 ms, Memory: 26004 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -4,23 +4,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        
-        int N=Integer.parseInt(br.readLine());
-        Set<String> users=new HashSet<>();
-        int sum=0;
-        
+  
+        int n=Integer.parseInt(br.readLine());
+        int answer=0;
 
-        for(int i=0;i<N;i++){
+        Set<String> people=new HashSet<>();
+        for(int i=0;i<n;i++){
             String s=br.readLine();
-            if(s.equals("ENTER")) users.clear();
-            else {
-                if(!users.contains(s)) {
-                    users.add(s);
-                    sum++;
-                }
+            if(s.equals("ENTER")){
+                people.clear();
+            } else if(!people.contains(s)) {
+                people.add(s);
+                answer++;
             }
         }
-        System.out.println(sum);
         
+
+        System.out.println(answer);
     }
 }
