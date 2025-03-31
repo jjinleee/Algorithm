e6257f1a31930120170cd5dd9b8942babe6bff73
진 <93428123+jjinleee@users.noreply.github.com>
2025-03-31 10:14:42 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 296 ms, Memory: 48160 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -2,49 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-       static  char[][] pattern;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
+    static char[][] pattern;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        
         pattern=new char[n][n];
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                pattern[i][j]=' ';
-            }
-        }
-
-        star(0,0,n);
-
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                sb.append(pattern[i][j]);
+        
+        StringBuilder sb = new StringBuilder();
+        toEmpty(n, 0, 0);
+        for(char[] r: pattern){
+            for(char c: r){
+                sb.append(c);
             }
             sb.append('\n');
         }
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 
-    public static void star(int x, int y,int n){
-        if(n==3){
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    if(i==1 && j==1) continue;
-                    pattern[x+i][y+j]='*';
-                }
-            }
+    //재귀적으로 가운데를 공백으로 치환
+    static void toEmpty(int n, int x, int y){
+        if(n==1){
+            pattern[x][y]='*';
             return;
         }
-
-        int div=n/3;
-
+        int size=n/3;
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                if(i==1 && j==1) continue;
-                star(x+div*i, y+div*j, div);
+                if(i==1 && j==1){
+                    blank(size,x+size*i, y+size*j);
+                } else {
+                    toEmpty(size, x+size*i, y+size*j);
+                }
+            }
+        }
+    }
+    static void blank(int n, int x, int y){
+        for(int i=x;i<x+n;i++){
+            for(int j=y;j<y+n;j++){
+                pattern[i][j]=' ';
             }
         }
     }
