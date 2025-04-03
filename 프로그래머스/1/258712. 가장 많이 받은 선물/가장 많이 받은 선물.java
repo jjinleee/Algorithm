2501aff84d012c0889178cc5285bba0967d96ba7
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-03 16:49:07 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 20.27 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,31 +1,64 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < friends.length; i++) {
-            map.put(friends[i], i);
+        int answer=0;
+        int n=friends.length;
+        
+        //이름과 인덱스 매칭
+        Map<String,Integer> name= new HashMap<>();
+        for(int i=0;i<n;i++){
+            name.put(friends[i],i);
         }
-        int[] index = new int[friends.length];
-        int[][] record = new int[friends.length][friends.length];
-
-        for (String str : gifts) {
-            String[] cur = str.split(" ");
-            index[map.get(cur[0])]++;
-            index[map.get(cur[1])]--;
-            record[map.get(cur[0])][map.get(cur[1])]++;
+        
+        //테이블표 생성, 초기화
+        int[][] table=new int[n][n];
+        for(int[] t:table){
+            Arrays.fill(t, 0);
         }
+        //테이블에 주고받은 선물 입력
+        for(int i=0;i<gifts.length;i++){
+            String[] names=gifts[i].split(" ");
+            int give=name.get(names[0]);
+            int take=name.get(names[1]);
+            table[give][take]++;
+        }
+        
 
-       int ans = 0;
-       for (int i = 0; i < friends.length; i++) {
-           int cnt = 0;
-           for (int j = 0; j < friends.length; j++) {
-               if(i == j) continue;
-               if (record[i][j] > record[j][i]) cnt++;
-               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
-           }
-           ans = Math.max(cnt, ans);
-       }
-        return ans;
+        //선물지수=준선물-받은선물=행더함-열더함
+        int[] grade=new int[n];
+        for(int i=0;i<n;i++){
+            int gave=0;
+            int took=0;
+            for(int j=0;j<n;j++){
+                gave+=table[i][j];
+                took+=table[j][i];
+            }
+            grade[i]=gave-took;
+        }
+        
+        
+        //다음달 받을 선물 계산하기
+        int[] next=new int[n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+               if(i==j) continue;
+                
+                if(table[i][j]>table[j][i]) next[i]++;
+                else if(table[i][j]==table[j][i]){
+                    if(grade[i]>grade[j]) next[i]++;
+                }
+            }
+        }
+        
+        //최댓값구하기
+        int max=0;
+        for(int p:next){
+            max=Math.max(max,p);
+        }
+    
+        
+
+        return max;
     }
 }
