257ceb66b78f5ddf2eb9ae45794f3d019d2e64f2
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-09 10:54:17 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 496 ms, Memory: 16396 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -2,30 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int cnt1;
-    static int cnt2;
+    static int cnt1=0;
+    static int cnt2=0;
+    static int[] f;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());  
-        
+        int n=Integer.parseInt(br.readLine());
+
+        f=new int[n+1];
+
         fib(n);
         fibonacci(n);
 
+    
         System.out.println(cnt1+" "+cnt2);
+       
     }
-    public static int fib(int n){
+    static int fib(int n){
         if(n==1 || n==2) {
             cnt1++;
             return 1;
         }
-        else return fib(n-1)+fib(n-2);
+        else return (fib(n-1)+fib(n-2));
     }
-    public static int fibonacci(int n){
-        int[] f=new int[n+1];
+
+    static int fibonacci(int n){
         f[1]=f[2]=1;
         for(int i=3;i<=n;i++){
-            cnt2++;
             f[i]=f[i-1]+f[i-2];
+            cnt2++;
         }
         return f[n];
     }
