1b4b1d4bbbd1dda5351006fcb938841864166ca1
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-11 10:06:32 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,25 +1,23 @@
 import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Main {    
+    static int mod=1000000000;
     public static void main(String[] args) throws IOException {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        int div=1000000000;
-        int n= Integer.parseInt(br.readLine());
-        int[][] dp= new int[n+1][10];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n=Integer.parseInt(br.readLine());
+        long[][] dp= new long[n+1][10];
 
         for(int i=1;i<=9;i++){
             dp[1][i]=1;
         }
-
+        
         for(int i=2;i<=n;i++){
             for(int j=0;j<=9;j++){
-                if(j>0){
-                    dp[i][j]+=dp[i-1][j-1];
-                }
-                if(j<9){
-                    dp[i][j]+=dp[i-1][j+1];
-                }
-                dp[i][j]%=div;
+                if(j>0) dp[i][j]+=dp[i-1][j-1];
+                if(j<9) dp[i][j]+=dp[i-1][j+1];
+                dp[i][j]%=mod;
             }
         }
 
@@ -28,8 +26,6 @@ public class Main {
             result+=dp[n][i];
         }
 
-        result%=div;
-        System.out.println(result);
-
+        System.out.println(result%mod);
     }
 }
