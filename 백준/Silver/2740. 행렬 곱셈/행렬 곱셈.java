03e05fab1467947dc97788c1685de241e2bb05ea
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-17 16:24:53 +0900

    [Silver V] Title: 행렬 곱셈, Time: 164 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -1,50 +1,56 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+
+public class Main { 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st= new StringTokenizer(br.readLine());
 
-        int n= Integer.parseInt(st.nextToken());
-        int m= Integer.parseInt(st.nextToken());
+        int n=Integer.parseInt(st.nextToken());
+        int m=Integer.parseInt(st.nextToken());
+        int[][] a=new int[n][m];
 
-        int[][] A=new int[n][m];
         for(int i=0;i<n;i++){
             st=new StringTokenizer(br.readLine());
             for(int j=0;j<m;j++){
-                A[i][j]=Integer.parseInt(st.nextToken());
+                a[i][j]=Integer.parseInt(st.nextToken());
             }
         }
 
-
         st=new StringTokenizer(br.readLine());
-        m= Integer.parseInt(st.nextToken());
-        int k= Integer.parseInt(st.nextToken());
+        m=Integer.parseInt(st.nextToken());
+        int k=Integer.parseInt(st.nextToken());
+        int[][] b=new int[m][k];
 
-        int[][] B=new int[m][k];
         for(int i=0;i<m;i++){
             st=new StringTokenizer(br.readLine());
             for(int j=0;j<k;j++){
-                B[i][j]=Integer.parseInt(st.nextToken());
+                b[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-
-
-        StringBuilder sb= new StringBuilder();
+        int[][] result=new int[n][k];
 
         for(int i=0;i<n;i++){
             for(int j=0;j<k;j++){
                 int sum=0;
                 for(int t=0;t<m;t++){
-                    sum+=A[i][t]*B[t][j];
+                    sum+=a[i][t]*b[t][j];
                 }
-                sb.append(sum+" ");
+                result[i][j]=sum;
             }
-            sb.append("\n");
+        }
+    
+        StringBuilder sb= new StringBuilder();
+        for(int[] r: result){
+            for(int num:r){
+                sb.append(num).append(" ");
+            }
+            sb.append('\n');
         }
 
         System.out.println(sb);
-    } 
+        
+    }
 }
