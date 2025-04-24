68dcbb02c696f31ad2afb6d39c8179ce83a664ca
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-24 13:19:20 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,20 @@
+-- 코드를 작성해주세요
+SELECT E.EMP_NO, EMP_NAME, 
+     CASE 
+     when AVG(SCORE)>=96 then 'S'
+     WHEN AVG(SCORE)>=90 THEN 'A'
+     WHEN AVG(SCORE)>=80 THEN 'B'
+     ELSE 'C'
+     END  AS GRADE,
+     CASE
+     when AVG(SCORE)>=96 then E.SAL*0.2
+     WHEN AVG(SCORE)>=90 THEN E.SAL*0.15
+     WHEN AVG(SCORE)>=80 THEN E.SAL*0.1
+     ELSE 0
+     END  AS BONUS
+     
+FROM HR_EMPLOYEES E
+JOIN HR_GRADE G ON E.EMP_NO=G.EMP_NO
+GROUP BY E.EMP_NO
+ORDER BY E.EMP_NO;
+     
