d65d876f5c33de9b0bd221938f32c825980587a1
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-28 11:20:04 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,18 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        Map<String,Integer> codi=new HashMap<>();
-        for(int i=0;i<clothes.length;i++){
-            codi.put(clothes[i][1],codi.getOrDefault(clothes[i][1],0)+1);
+        Map<String,Integer> map= new HashMap<>();
+        for(String[] c:clothes){
+            map.put(c[1],map.getOrDefault(c[1],0)+1);
         }
         
-        int mul=1;  
-       
-        for(String c:codi.keySet()){
-            answer*=(codi.get(c)+1);
+        for(String k:map.keySet()){
+            answer*=(map.get(k)+1);
         }
-        answer-=1;
-
-        return answer;
+        return --answer;
     }
 }
