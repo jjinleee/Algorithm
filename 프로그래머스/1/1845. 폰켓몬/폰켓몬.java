70c82662f883d7bd9af52c8a0d2a8f0f4f0b5bed
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-04-28 10:40:03 +0900

    [level 1] Title: 폰켓몬, Time: 6.63 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,15 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int n=nums.length;
-        Map<Integer,Integer> pocket=new HashMap<>();
-        
-        for(int i=0;i<n;i++){
-            pocket.put(nums[i],pocket.getOrDefault(nums[i],0)+1);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
         }
-        if(pocket.size()>=n/2) answer=n/2;
-        else answer=pocket.size();
+        
+        int n=nums.length;
+        if(map.size()>=n/2) answer=n/2;
+        else answer=map.size();
         
         return answer;
     }
