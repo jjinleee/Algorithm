4addc16037a1fecb3292255792c04e8a12611b88
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-01 17:45:02 +0900

    [level 2] Title: 프로세스, Time: 1.84 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,41 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
+        int answer = 0;
+        int n=priorities.length;
         
-        Queue<Process> q= new LinkedList<>();
-        List<Integer> list=new ArrayList<>();
-        
-        for(int i=0;i<priorities.length;i++){
-            q.offer(new Process(priorities[i],i));
-            list.add(priorities[i]);
+        Queue<int[]> queue=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            //우선순위, 인덱스 저장
+            queue.add(new int[]{priorities[i],i});
         }
-        list.sort(Collections.reverseOrder());
-
-        int order=0;
         
-        while(!q.isEmpty()){
-            Process current=q.poll();
+        while(!queue.isEmpty()){
+            int[] cur=queue.poll();
+            boolean high=false;
             
-            if(current.priority==list.get(0)){
-                order++;
-                list.remove(0);
-                
-                if(current.index==location){
-                    return order;
+            for(int[] p:queue){
+                if(p[0]>cur[0]){    //큐안에 우선순위가 더높은게있는지확인
+                    high=true;
+                    break;
                 }
-            } else q.offer(current);
+            }
+            
+            if(high){ //있으면 큐에 다시 넣기
+                queue.add(cur);
+            } else{
+                answer++; 
+                if(cur[1]==location) return answer; //없고 찾는 location이면 return
+            }
         }
-        
-        
         return -1;
-    }
-    static class Process{
-        int priority;
-        int index;
-        
-        public Process(int priority, int index){
-            this.priority=priority;
-            this.index=index;
-        }
     }
 }
