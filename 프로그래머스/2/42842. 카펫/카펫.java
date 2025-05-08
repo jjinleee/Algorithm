0288e5141e077137bb31a87b3d07d0cf14ca8912
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-08 11:35:23 +0900

    [level 2] Title: 카펫, Time: 0.18 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,25 +1,24 @@
 import java.util.*;
+
 class Solution {
-    List<int[]> y=new ArrayList<>();
-    
     public int[] solution(int brown, int yellow) {
         int[] answer = {};
         
-        //yellow 가로 세로 조합
-        for(int i=1;i*i<=yellow;i++){
-            if(yellow%i==0) y.add(new int[]{yellow/i,i});
-        }
-        
-        //brown길이 확인
-        for(int[] length:y){
-            int x=length[0]+2;
-            int y=length[1];
-            
-            if(2*x+2*y==brown){
-                return new int[]{x,y+2};
+        //yellow가 되는 경우의 수(1부터 시작했으니까 [1]이 더김->가로가됨)
+        List<int[]> wh=new ArrayList<>();
+        for(int i=1;i<=Math.sqrt(yellow);i++){
+            if(yellow%i==0){
+                wh.add(new int[]{i,yellow/i});
             }
         }
         
+        for(int[] t: wh){
+            int bw=(t[1]+2);
+            int bh=2+t[0];
+            if(2*bw+2*bh-4==brown){
+                return new int[]{bw,bh};
+            }
+        }
         return answer;
     }
 }
