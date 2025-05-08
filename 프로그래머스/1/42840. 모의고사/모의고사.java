49e7b636b1f1bf4fd2ffcdeb8afd38075a177749
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-08 10:08:58 +0900

    [level 1] Title: 모의고사, Time: 5.12 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,28 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] p1={1,2,3,4,5};
-        int[] p2={2,1,2,3,2,4,2,5};
-        int[] p3={3,3,1,1,2,2,4,4,5,5};
+        int[][] p={{1,2,3,4,5},
+                   {2,1,2,3,2,4,2,5},
+                   {3,3,1,1,2,2,4,4,5,5}};
         
-        int[] cnt=new int[3];
-        
-        int n= answers.length;
-        for(int i=0;i<n;i++){
-            if(answers[i]==p1[i%5]) cnt[0]++;
-            if(answers[i]==p2[i%8]) cnt[1]++;
-            if(answers[i]==p3[i%10]) cnt[2]++;
-        }
-        
-        int max=Math.max(cnt[0],Math.max(cnt[1],cnt[2]));
-        
-        List<Integer> result= new ArrayList<>();
+        int[][] cnt= new int[3][2];
         for(int i=0;i<3;i++){
-            if(cnt[i]==max) result.add(i+1);
+            int count=0;
+            for(int j=0;j<answers.length;j++){
+                if(answers[j]==p[i][j%p[i].length]) count++;
+            }
+            cnt[i][0]=i;
+            cnt[i][1]=count;
         }
         
-        Collections.sort(result);
+        //coun 내름차순
+        Arrays.sort(cnt,(a,b)-> b[1]-a[1]);
+        int max=cnt[0][1];
         
-        return result.stream().mapToInt(i->i).toArray();
+        List<Integer> answer=new ArrayList<>();
+        for(int[] m : cnt){
+            if(m[1]==max) answer.add(m[0]+1);
+        }
+        
+        return answer.stream().mapToInt(i->i).toArray();
+        
     }
 }
