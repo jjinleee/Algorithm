fedd943806f9527c7a0fdd404d9b93ba5a8ff6c3
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-12 13:14:55 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.01 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,44 +1,45 @@
 import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        
-    
-        //차례대로 하나씩 전선끊음
+        int answer=Integer.MAX_VALUE;
         for(int i=0;i<wires.length;i++){
-            //그래프초기화
-            List<List<Integer>> graph=new ArrayList<>();
+            List<List<Integer>> tree= new ArrayList<>();
             for(int j=0;j<=n;j++){
-                graph.add(new ArrayList<>());
-            }
-            for(int j=0;j<wires.length;j++){
-                if(i==j) continue; //끊음
-                int a=wires[j][0];
-                int b=wires[j][1];
+                tree.add(new ArrayList<>());
+            } 
                 
-                graph.get(a).add(b);
-                graph.get(b).add(a);
+            //트리구성
+            for(int j=0;j<wires.length;j++){
+                if(i==j) continue;
+                int v1=wires[j][0];
+                int v2=wires[j][1];
+
+                tree.get(v1).add(v2);
+                tree.get(v2).add(v1);
             }
-            
-            boolean[] visited=new boolean[n+1];
-            
-            int cnt=dfs(visited, graph, 1);
+                
+            boolean[] visited= new boolean[n+1];
+
+            int cnt=dfs(tree, visited, 1);
             int diff=Math.abs(n-2*cnt);
-            
             answer=Math.min(answer, diff);
+                
         }
         return answer;
     }
     
-    static int dfs(boolean[] visited, List<List<Integer>> graph, int node){
+    
+    static int dfs(List<List<Integer>> tree, boolean[] visited, int node){
         visited[node]=true;
         int cnt=1;
         
-        for(int next:graph.get(node)){
+        for(int next : tree.get(node)){
             if(!visited[next]){
-                cnt+=dfs(visited, graph, next);
+                cnt+=dfs(tree, visited, next);
             }
         }
         return cnt;
     }
+      
 }
