c8bf5fb2aa37b8e8ed2ee34400843ddae767fbd9
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-13 22:49:12 +0900

    [level 2] Title: 모음 사전, Time: 12.79 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,19 +1,22 @@
 import java.util.*;
+
 class Solution {
-    List<String> dict= new ArrayList<>();
+    List<String> list= new ArrayList<>();
     String[] vowels={"A","E","I","O","U"};
     
     public int solution(String word) {
+        int cnt = 0;
         dfs("",0);
         
-        return dict.indexOf(word)+1;
+        return list.indexOf(word)+1;
     }
-    void dfs(String current, int depth){
-        if(depth>5) return;
+    void dfs(String current, int d){
+        if(d>5) return;
         
-        if(!current.isEmpty()) dict.add(current);
-        for(String v:vowels){
-            dfs(current+v,depth+1);
+        if(!current.isEmpty()) list.add(current);
+        for(String s : vowels){
+            dfs(current+s, d+1);
         }
     }
+
 }
