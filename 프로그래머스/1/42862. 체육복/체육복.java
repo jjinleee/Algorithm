e8ef60778f502c50f043a2d61b3cf49324f26c4d
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-14 13:51:03 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,36 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        List<Integer> lostPeople = new ArrayList<>();
-        List<Integer> reservePeople = new ArrayList<>();
+        int[] students = new int[n + 2]; // 학생 번호: 1~n, 앞뒤 검사 위해 +2
 
-        // 여벌이 있지만 도난당한 경우 제거
+        // 여벌 체육복 표시 (+1)
+        for (int r : reserve) {
+            students[r]++;
+        }
+
+        // 도난 처리 (-1)
         for (int l : lost) {
-            boolean isSelfReserve = false;
-            for (int i = 0; i < reserve.length; i++) {
-                if (l == reserve[i]) {
-                    reserve[i] = -1; // 사용 표시
-                    isSelfReserve = true;
-                    break;
+            students[l]--;
+        }
+
+        // 체육복 빌려주기 로직
+        for (int i = 1; i <= n; i++) {
+            if (students[i] == -1) { // 체육복 없는 학생
+                if (students[i - 1] == 1) { // 앞 사람이 여벌 있음
+                    students[i]++;
+                    students[i - 1]--;
+                } else if (students[i + 1] == 1) { // 뒷 사람이 여벌 있음
+                    students[i]++;
+                    students[i + 1]--;
                 }
             }
-            if (!isSelfReserve) lostPeople.add(l); // 진짜로 체육복 없는 사람만 추가
         }
 
-        // 빌려주기
-        for (int r : reserve) {
-            if (r == -1) continue;
-            if (lostPeople.contains(r - 1)) {
-                lostPeople.remove(Integer.valueOf(r - 1));
-            } else if (lostPeople.contains(r + 1)) {
-                lostPeople.remove(Integer.valueOf(r + 1));
-            }
+        // 체육복 있는 학생 수 계산
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (students[i] >= 0) answer++;
         }
 
-        return n - lostPeople.size();
+        return answer;
     }
 }
