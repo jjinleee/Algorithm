92b388b631bc846d4c085c28549f44ebafb260df
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-16 21:52:21 +0900

    [level 3] Title: 베스트앨범, Time: 4.51 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,54 +2,33 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String,Integer> genreTotal=new HashMap<>(); //장르별 재생수
-        Map<String, List<Song>> music=new HashMap<>(); //장르별 노래목록
+        //장르 횟수 저장
+        Map<String,Integer> genreCount =new HashMap<>();
+        //장르별 노래 인덱스 저장
+        Map<String, List<Integer>> genreSong=new HashMap<>();
         
-        //노래데이터저장
-        for(int i=0;i<genres.length;i++){
-            String genre=genres[i];
-            int play=plays[i];
-            
-            //장르별 총 재생수 누적
-            genreTotal.put(genre, genreTotal.getOrDefault(genre,0)+play);
-            //장르별 노래 리스트에 추가
-            List<Song> list=music.getOrDefault(genre, new ArrayList<>());
-            list.add(new Song(i,play)); //song객체(노래번호, 재생수)
-            music.put(genre,list); //map에 다시 저장
+        int i=0;
+        for(String g: genres){
+            genreCount.put(g,genreCount.getOrDefault(g,0)+plays[i]); //장르카운트
+            genreSong.putIfAbsent(g,new ArrayList<>());   //장르별 노래 고유번호저장
+            genreSong.get(g).add(i);
+            i++;
         }
-        //장르를 재생수 기준으로 내림차순
-        List<String> genreSorted=new ArrayList<>(genreTotal.keySet());
-        genreSorted.sort((a,b)->genreTotal.get(b)-genreTotal.get(a));
-            
-        List<Integer> answer=new ArrayList<>();
-            
-        //장르별 작업
-        for(String genre:genreSorted){
-            List<Song> list=music.get(genre);
-            
-            //해당 장르의 노래를 재생수 내림차순(같으면 고유번호 오름차순)
-            list.sort((a,b)->{
-                if(b.plays==a.plays) return a.index-b.index;
-                return b.plays-a.plays;
-            });
-            
-            answer.add(list.get(0).index); //첫번째노래
-            if(list.size()>1) answer.add(list.get(1).index); //두번째노래
-        }
+        //장르정렬
+        List<String> sortedGenre=new ArrayList<>(genreCount.keySet());
+        sortedGenre.sort((a,b)->genreCount.get(b)-genreCount.get(a)); //내림차순
         
-
-        //int[]로 변환
-        return answer.stream().mapToInt(i->i).toArray();
-    }
-}
-
-//노래정보 
-class Song{
-    int index; //고유번호
-    int plays; //재생수
-    
-    Song(int index, int plays){
-        this.index=index;
-        this.plays=plays;
+        List<Integer> result=new ArrayList<>();
+        //많이 재생된 장르부터
+        for(String max : sortedGenre){
+            //해당 장르에 속한 고유번호 리스트
+            List<Integer> tmp=genreSong.get(max);
+            //많이 재생된 순, 같을 시 고유번호 작은순
+            tmp.sort((c,d)-> plays[c]==plays[d] ? c-d : plays[d]-plays[c]);
+            
+            result.add(tmp.get(0));
+            if(tmp.size()>1) result.add(tmp.get(1));
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
