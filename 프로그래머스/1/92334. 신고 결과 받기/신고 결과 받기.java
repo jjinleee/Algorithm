585a3e219a0ba4264c7917e1aec5199e11507148
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-16 12:13:51 +0900

    [level 1] Title: 신고 결과 받기, Time: 225.43 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,36 +1,38 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        //신고당한, 신고한
-        Map<String, HashSet<String>> reportMap= new HashMap<>();
-        //유저별 메일개수
-        Map<String,Integer> mail=new HashMap<>();
-        
-        for(String id: id_list){
-            reportMap.put(id, new HashSet<>());
-            mail.put(id,0);
+        //각 유저별 메일횟수저장(map은 순서보장이 안됨)
+        Map<String,Integer> users=new HashMap<>();
+        for(String id : id_list){
+            users.put(id,0);
+        }
+        //신고당한 사람별 신고한 사람 목록(한사람의 동일 유저 중복신고안됨->Set사용)
+        Map<String,Set<String>> reported=new HashMap<>();
+        for(String r : report){
+            String reportPerson=r.split(" ")[0];
+            String reportedPerson=r.split(" ")[1];
+            reported.putIfAbsent(reportedPerson,new HashSet<>());
+            reported.get(reportedPerson).add(reportPerson);
         }
         
-        for(String s: report){
-            String[] temp= s.split(" ");
-            String reporter=temp[0];
-            String reported=temp[1];
-            reportMap.get(reported).add(reporter);
-        }
-        
-        for(String reported:reportMap.keySet()){
-            if(reportMap.get(reported).size()>=k){
-                for(String reporter: reportMap.get(reported)){
-                    mail.put(reporter, mail.get(reporter)+1);
+        //k번이상 신고됐는지 체크
+        for(String s : reported.keySet()){
+            //정지된 유저를 신고한 유저에게 메일
+            if(reported.get(s).size()>=k){
+                Set<String> mail=reported.get(s);
+                for(String name : mail){
+                    users.put(name,users.get(name)+1);
                 }
             }
         }
         
-        int[] result=new int[id_list.length];
-        for(int i=0;i<id_list.length;i++){
-            result[i]=mail.get(id_list[i]);
+        int[] answer=new int[id_list.length];
+        int y=0;
+        for(String id : id_list){
+            answer[y++]=users.get(id);
         }
         
-        return result;
+        return answer;
     }
 }
