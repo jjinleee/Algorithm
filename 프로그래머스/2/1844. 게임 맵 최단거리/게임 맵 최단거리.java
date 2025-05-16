e17e0699984908a1bc32c8fd3ff72fe50ad877ed
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-17 00:47:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.62 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,42 @@
 import java.util.*;
 
 class Solution {
-    //이동방향
-    private static final int[] row={0,0,1,-1};
-    private static final int[] col={1,-1,0,0};
-        
-    private static class Node{
-        int r,c;
-        public Node(int r,int c){
-                this.r=r;
-                this.c=c;
-        }
-    }  
-    
     public int solution(int[][] maps) {
-        int answer = 0;
-        
         int n=maps.length;
         int m=maps[0].length;
-        //최단거리를 저장할 배열
-        int[][] dist= new int[n][m];
-        //bfs탐색큐
-        ArrayDeque<Node> queue=new ArrayDeque<>();
         
-        queue.addLast(new Node(0,0));
-        dist[0][0]=1;
+        //이동
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         
+        //방문배열, 큐 생성
+        boolean[][] visited= new boolean[n][m];
+        Queue<int[]> queue=new LinkedList<>();
+        queue.offer(new int[]{0,0}); //시작
+        visited[0][0]=true; //방문체크
+        
+        //큐가빌때까지
         while(!queue.isEmpty()){
-            Node now=queue.pollFirst();
+            int[] now=queue.poll();
+            int x=now[0];
+            int y=now[1];
             
+            //상하좌우로 이동
             for(int i=0;i<4;i++){
-                int nr=now.r+row[i];
-                int nc=now.c+col[i];
-                
-                if(nr<0 ||nc<0||nr>=n||nc>=m) continue;
-                
-                if(maps[nr][nc]==0) continue;
-                
-                if(dist[nr][nc]==0){
-                    queue.addLast(new Node(nr,nc));
-                    dist[nr][nc]=dist[now.r][now.c]+1;
-                }
+               int nx=x+dx[i];
+               int ny=y+dy[i];
+               
+               if(nx>=0 && nx<n && ny>=0 && ny<m){ //범위체크
+                    if(!visited[nx][ny] && maps[nx][ny]==1){//미방문 && 길
+                        //큐에넣고 방문체크
+                        queue.offer(new int[]{nx,ny});
+                        visited[nx][ny]=true;
+                        maps[nx][ny]=maps[x][y]+1;//현재거리+1
+                    }
+               }
             }
         }
         
-        return dist[n-1][m-1]==0 ? -1 : dist[n-1][m-1];
+        return maps[n-1][m-1]==1 ? -1 : maps[n-1][m-1];
     }
 }
