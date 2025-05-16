e4b05f1d375c8b8db285b93868bf36cc816105f7
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-16 10:42:09 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.72 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,60 +3,27 @@ import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
         StringBuilder sb= new StringBuilder();
-        int n=survey.length;
-        char[][] alpha={
-            {'R','T'},
-            {'C','F'},
-            {'J','M'},
-            {'A','N'}
-        };
         
         //성격유형 저장(array로?->순서변경감지어떻게)
         Map<Character,Integer> personal=new HashMap<>();
-        personal.put('R',0);
-        personal.put('T',0);
-        personal.put('C',0);
-        personal.put('F',0);
-        personal.put('J',0);
-        personal.put('M',0);
-        personal.put('A',0);
-        personal.put('N',0);
-        
-        int k=0;
-        //각 유형 카운트
-        for(String s : survey){
-            char p1=s.charAt(0);
-            char p2=s.charAt(1);
-            int choice=choices[k++];
-            
-            if(choice==0) break; //4면 아무일도없음
-            int[] type=personalCount(choice);
-            
-            //각 성격 해시맵으로 가져와서 수정
-            if(type[0]==0) personal.put(p1,personal.get(p1)+type[1]);
-            else personal.put(p2,personal.get(p2)+type[1]);
+        for (char c :"RTCFJMAN".toCharArray()){
+            personal.put(c,0);
         }
         
-        //지표별 카운트 큰거 추가(코드가 너무 안좋은듯)
-        for(int i=0;i<4;i++){
-            char c1=alpha[i][0];
-            char c2=alpha[i][1];
-            if(personal.get(c1)>personal.get(c2)) sb.append(c1);
-            else if(personal.get(c1)<personal.get(c2)) sb.append(c2);
-            else sb.append(c1>c2 ? c2 : c1);
+        for(int i=0;i<survey.length;i++){
+            char left=survey[i].charAt(0);
+            char right=survey[i].charAt(1);
+            int choice=choices[i];
+            
+            if(choice<4) personal.put(left, personal.get(left)+(4-choice));
+            else if(choice>4) personal.put(right, personal.get(right)+(choice-4));
         }
+        
+        sb.append(personal.get('R')>=personal.get('T')? 'R' : 'T');
+        sb.append(personal.get('C') >= personal.get('F') ? 'C' : 'F');
+        sb.append(personal.get('J') >= personal.get('M') ? 'J' : 'M');
+        sb.append(personal.get('A') >= personal.get('N') ? 'A' : 'N');
+        
         return sb.toString();
-    }
-    //지표
-    static int[] personalCount(int choice){
-        switch(choice){
-            case 1 : return new int[]{0,3};
-            case 2 : return new int[]{0,2};
-            case 3 : return new int[]{0,1};
-            case 5 : return new int[]{1,1};
-            case 6 : return new int[]{1,2};
-            case 7 : return new int[]{1,3};
-            default : return new int[]{0,0};
-        }
     }
 }
