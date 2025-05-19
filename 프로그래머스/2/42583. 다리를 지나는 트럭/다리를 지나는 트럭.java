98895c8d18a5adc87c26771c524f18e2780e8b91
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-19 17:41:00 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 11.71 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,30 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Queue<int[]> queue=new LinkedList<>(); //트럭무게,다리진입시간
-        int time=0;
-        int totalWeight=0;
-        int index=0; //다음에올라갈 트럭 인덱스
+        Queue<int[]> bridge= new LinkedList<>();
         
-        while(index<truck_weights.length || !queue.isEmpty()){
+        int time=0;
+        int total=0;
+        int i=0;
+        
+        
+        while(i<truck_weights.length || !bridge.isEmpty()){
             time++;
             
-            //다리를 다 건넜으면 제거
-            if(!queue.isEmpty() && time-queue.peek()[1]>=bridge_length){
-                totalWeight-=queue.poll()[0];
+            //다리 다건너면 빼기(현재시간-들어간시간=bridge_length가 되면)
+            if(!bridge.isEmpty() && time-bridge.peek()[1]==bridge_length) {
+                total-=bridge.poll()[0];
             }
-            
-            //다음트럭이 올라올수있으면 올림
-            if(index<truck_weights.length && totalWeight + truck_weights[index]<=weight){
-                queue.offer(new int[]{truck_weights[index],time});
-                totalWeight+=truck_weights[index];
-                index++;
-            }
+        
+            //개수, 무게확인하고 다리에 올라감
+            if(i<truck_weights.length && total+truck_weights[i]<=weight){
+                bridge.offer(new int[]{truck_weights[i],time});
+                total+=truck_weights[i++];
+            } 
+
         }
         
         return time;
-        
-        
     }
 }
