75bbfc1ff4d231c4120c36861c89a28574ad479f
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-19 15:09:59 +0900

    [level 2] Title: 올바른 괄호, Time: 18.58 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,18 @@
 import java.util.*;
 
+//괄호문제는 거의 스택 유형
 class Solution {
     boolean solution(String s) {
-        
         Stack<Character> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='('){
-                stack.push(s.charAt(i));
-            } else {
-                if(stack.isEmpty()) return false;
-                else stack.pop();
-            }
+            if(s.charAt(i)=='('){
+                stack.push('(');
+            } else if(!stack.isEmpty() && s.charAt(i)==')'){
+                stack.pop();
+            } else return false;
         }
         
-        if(!stack.isEmpty()) return false;
-        else return true;
-
+        if(stack.isEmpty()) return true;
+        else return false;
     }
 }
