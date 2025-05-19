3dc0c479249e588feffe657c6fb4065a1ca9c294
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-19 15:04:47 +0900

    [level 2] Title: 기능개발, Time: 3.37 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,32 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answer=new ArrayList<>();
-        
-        int n=progresses.length;
-        int[] days=new int[n];
-        
-        for(int i=0;i<n;i++){
-            days[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
+        //남은작업일(올림사용)
+        int[] left=new int[progresses.length];
+        for(int i=0;i<speeds.length;i++){
+            left[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
         }
         
-        int cnt=1;
-        int max=days[0];
-        
-        for(int i=1;i<n;i++){
-            if(days[i]<=max){
-                cnt++;
-            } else {
-                answer.add(cnt);
-                cnt=1;
-                max=days[i];
+        List<Integer> answer=new ArrayList<>();
+        Queue<Integer> queue=new LinkedList<>();
+
+        for(int l : left){
+            if(queue.isEmpty()) queue.offer(l);
+            else{
+                if(queue.peek()<l){ //먼저완성시 나감
+                    answer.add(queue.size());
+                    queue.clear();
+                    queue.offer(l);
+                } else { //뒤에거가 더 일찍끝나면 같이 나감
+                    queue.offer(l); 
+                }
             }
         }
+        answer.add(queue.size());
         
-        answer.add(cnt);
+        //마지막것도 넣어줘야함
+        
         
         return answer.stream().mapToInt(i->i).toArray();
     }
