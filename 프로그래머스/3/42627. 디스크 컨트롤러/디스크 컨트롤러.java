76b374c173eac71052d8182f6429269b2cbf46a0
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-20 14:00:43 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.62 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,31 +1,36 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[][] jobs) {
+        //요청시각 빠른 순으로 정렬
+        Arrays.sort(jobs, (a,b)->a[0]-b[0]);
         
-        Arrays.sort(jobs, (a,b)->a[0]-b[0]); //요청시각 기준 정렬
-        
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);//소요시간 짧은순
+        //소요시간 순으로 정렬
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[1]-b[1]);
         
         int time=0;
-        int idx=0;
         int total=0;
-        int cnt=0;
+        int i=0; //현재 큐에들어가는것
+        int count=0; //실행완료된것
+        int n=jobs.length;
         
-        while(cnt<jobs.length){
-            while(idx<jobs.length && jobs[idx][0]<=time){
-                pq.offer(jobs[idx++]);
+        while(count<n){    
+            //현재 시간이하의 요청작업을 큐에 삽입
+            while(i<n && jobs[i][0]<=time){
+                pq.offer(jobs[i++]);
             }
-            
-            if(!pq.isEmpty()){
-                int[] cur=pq.poll();
-                time+=cur[1]; //현재시간+작업소요시간
-                total+=(time-cur[0]); //작업종료시간-요청시각
-                cnt++;
-            } else time=jobs[idx][0]; //처리할 작업 없으면 다음 작업시각으로 이동
+            //실행완료된건 뺴고 반환시각계산, 반환시간=끝난시간-요청시각
+            //실행완료된지 알려면 지금시간-실행시작시간>=소요시간 체크?
+            if(!pq.isEmpty()) {
+                int[] job=pq.poll();
+                time+=job[1]; //작업수행시간
+                total+=time-job[0]; //대기+수행시간c
+                count++;
+            } else {
+                //대기할 작업 없을시 시간만흐름
+                time=jobs[i][0];
+            }
         }
-        
-        return total/jobs.length;
+        return total/n;
     }
 }
