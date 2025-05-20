1b63777f489666c70ef21888dec7d39b693b641e
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-20 14:26:31 +0900

    [level 3] Title: 이중우선순위큐, Time: 104.06 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,29 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = {0,0};
         PriorityQueue<Integer> pq=new PriorityQueue<>();
         PriorityQueue<Integer> pq2=new PriorityQueue<>(Collections.reverseOrder());
         
         for(String o : operations){
-            //최댓값 삭제
-            if(!pq.isEmpty() && o.equals("D 1")){
-                pq.remove(pq2.poll());
+            String[] op=o.split(" ");
+            
+            if(op[0].equals("I")){
+                pq.offer(Integer.parseInt(op[1]));
+                pq2.offer(Integer.parseInt(op[1]));
             }
-            //최솟값 삭제
-            else if (!pq.isEmpty() && o.equals("D -1")){
-                pq2.remove(pq.poll());
-            } else if(o.contains("I")){
-                int num= Integer.parseInt(o.split(" ")[1]);
-                pq.add(num);
-                pq2.add(num);
-            }
+            else if(!pq.isEmpty() ){
+                if(op[1].equals("-1")){
+                    pq2.remove(pq.poll());
+                } else pq.remove(pq2.poll());
+            }            
         }
-        if(!pq.isEmpty()){
-            answer[0]=pq2.peek();
-            answer[1]=pq.peek();
-        } 
         
-        return answer;
+        return pq.isEmpty() ? new int[]{0,0} : new int[]{pq2.peek(),pq.peek()};
     }
 }
