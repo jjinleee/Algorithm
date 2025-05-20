ae7899cd9ef979adc6adc70cfb95e8d3ed82c448
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-20 16:31:23 +0900

    [level 1] Title: 모의고사, Time: 3.77 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,30 +1,32 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
+        List<Integer> answer=new ArrayList<>();
+        int[] score=new int[3];
         int[][] p={{1,2,3,4,5},
                    {2,1,2,3,2,4,2,5},
                    {3,3,1,1,2,2,4,4,5,5}};
         
-        int[][] cnt= new int[3][2];
-        for(int i=0;i<3;i++){
-            int count=0;
-            for(int j=0;j<answers.length;j++){
-                if(answers[j]==p[i][j%p[i].length]) count++;
-            }
-            cnt[i][0]=i;
-            cnt[i][1]=count;
+        
+        //각 맞힌 개수 셈
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==p[0][i%p[0].length]) score[0]++;
+            if(answers[i]==p[1][i%p[1].length]) score[1]++;
+            if(answers[i]==p[2][i%p[2].length]) score[2]++;
         }
         
-        //coun 내름차순
-        Arrays.sort(cnt,(a,b)-> b[1]-a[1]);
-        int max=cnt[0][1];
         
-        List<Integer> answer=new ArrayList<>();
-        for(int[] m : cnt){
-            if(m[1]==max) answer.add(m[0]+1);
+        //최대점수
+        int max=Math.max(score[0],Math.max(score[1],score[2]));
+        
+        //가장높은 점수를 받은 사람의 인덱스 넣기(자동 오름차순))
+        for(int i=0;i<3;i++){
+            if(score[i]==max){
+                answer.add(i+1);
+            }
         }
         
         return answer.stream().mapToInt(i->i).toArray();
-        
     }
 }
