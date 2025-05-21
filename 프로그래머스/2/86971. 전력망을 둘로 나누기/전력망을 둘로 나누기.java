66db40801b49238f7c420610ae6d20173c23402b
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-21 14:18:52 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.10 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,42 +4,49 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer=Integer.MAX_VALUE;
         for(int i=0;i<wires.length;i++){
+            //트리 초기화
             List<List<Integer>> tree= new ArrayList<>();
             for(int j=0;j<=n;j++){
                 tree.add(new ArrayList<>());
-            } 
-                
-            //트리구성
+            }
+            //간선 하나씩 제거
             for(int j=0;j<wires.length;j++){
                 if(i==j) continue;
-                int v1=wires[j][0];
-                int v2=wires[j][1];
-
-                tree.get(v1).add(v2);
-                tree.get(v2).add(v1);
-            }
+                int a=wires[j][0];
+                int b=wires[j][1];
                 
-            boolean[] visited= new boolean[n+1];
-
-            int cnt=dfs(tree, visited, 1);
+                tree.get(a).add(b);
+                tree.get(b).add(a);
+            }
+            
+            int cnt=bfs(1,tree,n);
             int diff=Math.abs(n-2*cnt);
             answer=Math.min(answer, diff);
-                
-        }
+        }        
         return answer;
     }
-    
-    
-    static int dfs(List<List<Integer>> tree, boolean[] visited, int node){
-        visited[node]=true;
+    //한쪽 네트워크에 연결된 노드 수를 셈
+    static int bfs(int start, List<List<Integer>> tree,int n){
+        //외부에 전달할 필요없이 내부에서 독립적으로 세면되니까 내부에서 visited 정의
+        boolean[] visited=new boolean[n+1];
+        Queue<Integer> queue=new LinkedList<>();
+        
+        queue.offer(start); //시작노드삽입
+        visited[start]=true; 
+        
         int cnt=1;
         
-        for(int next : tree.get(node)){
-            if(!visited[next]){
-                cnt+=dfs(tree, visited, next);
+        while(!queue.isEmpty()){
+            int current=queue.poll();
+            for(int next : tree.get(current)){
+                if(!visited[next]){
+                    visited[next]=true;
+                    queue.offer(next);
+                    cnt++;
+                }
             }
         }
+        
         return cnt;
     }
-      
 }
