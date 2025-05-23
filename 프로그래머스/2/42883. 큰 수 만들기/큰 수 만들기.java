837c033a0e842a028b8bd0f8a2df6390dbbffe4b
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-23 16:57:08 +0900

    [level 2] Title: 큰 수 만들기, Time: 67.59 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,19 +2,32 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder answer= new StringBuilder();
+        Stack<Character> stack=new Stack<>();
         
+        
+        //내 앞에 나보다 작은게 있다면 뺌
         for(int i=0;i<number.length();i++){
-            char c =number.charAt(i);
-            //새로넣으려는 숫자가 더 크면 삭제하기 
-            while(k>0 && answer.length()>0 && answer.charAt(answer.length()-1)<c){
-               answer.deleteCharAt(answer.length()-1);
+            char current=number.charAt(i);
+            
+            //스택의 탑과 비교해서 더  작다면 뺌
+            while(!stack.isEmpty() && stack.peek()<current && k>0){
+                stack.pop();
                 k--;
             }
-            answer.append(c);
+            
+            stack.push(current);
+        }
+        //작은걸 뻈는데 k개가 안된다면 스택의 탑부터 뺌
+        while(k>0){
+            stack.pop();
+            k--;
         }
         
-        return answer.substring(0,answer.length()-k);
+        StringBuilder sb= new StringBuilder();
+        for(char c : stack){
+            sb.append(c);
+        }
         
+        return sb.toString();
     }
 }
