0572e74d36037e378f0e39fcff1e120931e93894
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-23 14:01:27 +0900

    [level 1] Title: 체육복, Time: 0.92 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,37 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] students = new int[n + 2]; // 학생 번호: 1~n, 앞뒤 검사 위해 +2
-
-        // 여벌 체육복 표시 (+1)
-        for (int r : reserve) {
-            students[r]++;
-        }
-
-        // 도난 처리 (-1)
-        for (int l : lost) {
-            students[l]--;
-        }
-
-        // 체육복 빌려주기 로직
-        for (int i = 1; i <= n; i++) {
-            if (students[i] == -1) { // 체육복 없는 학생
-                if (students[i - 1] == 1) { // 앞 사람이 여벌 있음
-                    students[i]++;
-                    students[i - 1]--;
-                } else if (students[i + 1] == 1) { // 뒷 사람이 여벌 있음
-                    students[i]++;
-                    students[i + 1]--;
-                }
+        Set<Integer> lostPeople=new HashSet<>();
+        Set<Integer> reservePeople= new HashSet<>();
+        
+        for(int l : lost) lostPeople.add(l);
+        for(int r : reserve) reservePeople.add(r);
+        
+        Set<Integer> intersection=new HashSet<>(lostPeople);
+        intersection.retainAll(reservePeople);
+        lostPeople.removeAll(intersection);
+        reservePeople.removeAll(intersection);
+        
+        //treeset은 자동정렬기능이 있어서 정렬순서로 여벌 체육복을 
+        //가진 학생들을 순회할 수 있다. 
+        for(int l : new TreeSet<>(reservePeople)){
+            if(lostPeople.contains(l-1)){
+                lostPeople.remove(l-1);
+            } else if( lostPeople.contains(l+1)){
+                lostPeople.remove(l+1);
             }
         }
-
-        // 체육복 있는 학생 수 계산
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (students[i] >= 0) answer++;
-        }
-
-        return answer;
+        
+        return n-lostPeople.size();
     }
 }
