0af706c22c80cb0e7340187e18094dc6a09eb2c2
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-27 16:58:10 +0900

    [level 3] Title: 단어 변환, Time: 3.89 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,42 @@
 import java.util.*;
 
 class Solution {
+    static int answer=Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
-        //단어에 타겟없으면 바로 반환
+        //변환못하면 return 0
         if(!Arrays.asList(words).contains(target)) return 0;
-        
-        //큐에넣고 방문배열
-        Queue<Word> queue=new LinkedList<>();
         boolean[] visited=new boolean[words.length];
-        queue.offer(new Word(begin,0));
         
-        while(!queue.isEmpty()){
-            Word current=queue.poll();
-            //타겟에 도착시 반환
-            if(current.word.equals(target)) return current.depth;
-            
-            for(int i=0;i<words.length;i++){
-                //방문도안했고 한글자만 다르다면 큐에넣음
-                if(!visited[i] && isDifferent(current.word,words[i])){
-                    queue.offer(new Word(words[i],current.depth+1));
-                    visited[i]=true;
-                }                
+        
+        for(int i=0;i<words.length;i++){
+            if(!visited[i]){
+                dfs(visited, begin,target, words,0);
             }
-
         }
         
-        return 0;
+        return answer;
     }
     
-    //한글자만 다른지 확인
+    static void dfs(boolean[] visited, String current,String target, String[] words, int cnt){
+        if(current.equals(target)){
+            answer=Math.min(answer,cnt);
+            return;
+        }
+        
+        for(int i=0;i<words.length;i++){
+            if(!visited[i] && isDifferent(current, words[i])){
+                visited[i]=true;
+                dfs(visited, words[i],target, words, cnt+1 );
+                visited[i]=false;
+            }
+        } 
+    }
+    //한글자만 다른지 체크
     static boolean isDifferent(String a, String b){
         int cnt=0;
         for(int i=0;i<a.length();i++){
             if(a.charAt(i)!=b.charAt(i)) cnt++;
         }
-        if(cnt>1) return false;
-        else return true;
+        return cnt==1 ? true : false;
     }
-    
-    //word 클래스
-    class Word{
-        String word;
-        int depth;
-        
-        Word(String word, int depth){
-            this.word=word;
-            this.depth=depth;
-        }
-        
-    } 
 }
