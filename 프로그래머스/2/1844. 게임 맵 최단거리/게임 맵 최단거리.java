018a9386ea916f3da7de41995acabffefb1c8b9a
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-05-29 17:21:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.35 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int[] dx={-1,1,0,0};
-        int[] dy={0,0,1,-1};
-        
+        int[] dy={0,0,-1,1};
         int n=maps.length;
         int m=maps[0].length;
         
         boolean[][] visited= new boolean[n][m];
+        
         Queue<int[]> queue=new LinkedList<>();
         queue.add(new int[]{0,0});
         visited[0][0]=true;
@@ -22,17 +22,17 @@ class Solution {
                 int nx=x+dx[i];
                 int ny=y+dy[i];
                 
+                // 범위체크
                 if(nx>=0 && nx<n && ny>=0 && ny<m){
-                    if(!visited[nx][ny] && maps[nx][ny]==1){
-                        queue.offer(new int[]{nx,ny});
+                    //벽이 아니고 방문안했다면
+                    if(maps[nx][ny]==1 && !visited[nx][ny]){
+                        queue.add(new int[]{nx,ny});
+                        maps[nx][ny]=maps[x][y]+1; //개수누적
                         visited[nx][ny]=true;
-                        maps[nx][ny]=maps[x][y]+1;
                     }
                 }
             }
-            
         }
-        
         return maps[n-1][m-1]==1 ? -1 : maps[n-1][m-1];
     }
 }
