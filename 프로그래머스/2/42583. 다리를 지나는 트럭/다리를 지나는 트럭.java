741338a3f53d0e99bf817c7a5aa3f75490711a2e
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-07 16:07:35 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 19.07 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
         int time = 0;
-        int totalWeight = 0;
-        int idx = 0;
-
-        // 다리 길이만큼 초기화
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.offer(0);  // 다리에 트럭 없을 때는 0으로 채움
+        int total=0;
+        int index=0;
+        
+        Queue<Integer> bridge=new LinkedList<>();
+        //0으로채워넣음
+        for(int i=0;i<bridge_length;i++){
+            bridge.offer(0);
         }
-
-        while (idx < truck_weights.length) {
+        
+        while(index<truck_weights.length){
             time++;
-
-            // 1초 흐르고, 다리 앞쪽 트럭 제거
-            totalWeight -= bridge.poll();
-
-            // 다음 트럭 올릴 수 있는지 확인
-            if (totalWeight + truck_weights[idx] <= weight) {
-                bridge.offer(truck_weights[idx]);
-                totalWeight += truck_weights[idx];
-                idx++;
-            } else {
-                // 못 올라가면 0을 채워서 시간만 흐르게 함
-                bridge.offer(0);
-            }
+            
+            //무조건 시간 지남에 따라 흐름
+            total-=bridge.poll();
+            //무게 안넘으면 넣음
+            if(total+truck_weights[index]<=weight){
+                bridge.offer(truck_weights[index]);
+                total+=truck_weights[index++];
+            } else bridge.offer(0); //못넣으면 0을 넣음
         }
-
-        // 마지막 트럭이 다리 끝까지 가는 시간 추가
-        return time + bridge_length;
+        //마지막 트럭 빠져나오는 시간
+        time+=bridge_length;
+        
+        return time;
     }
 }
