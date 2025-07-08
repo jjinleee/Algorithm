21c5cdc3f7ef93ac399cc651aa7753b8caf60b51
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-08 13:25:08 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,18 @@
 import java.util.*;
 
-//의상 종류별 카운트 개수세서 +1 한것을 곱함-1 하면 경우의 수 구할 수 있다
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String,Integer> map=new HashMap<>();
-        for(String[] c: clothes){
-            map.put(c[1], map.getOrDefault(c[1],0)+1);
+        Map<String, Integer> type=new HashMap<>();
+        for(String[] c : clothes){
+            type.put(c[1],type.getOrDefault(c[1],0)+1);
         }
         
-        int answer=1;
-
-        for(String type : map.keySet()){
-            answer*=(map.get(type)+1);
+        int result=1;
+        for(Integer i : type.values()){
+            result*=(i+1);
         }
-        answer--; //아무것도 안입은 경우 제외
         
-        return answer; 
+        return result-1;
+        
     }
 }
