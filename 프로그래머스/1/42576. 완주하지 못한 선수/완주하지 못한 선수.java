27255c3b4dbcd44cc9a0a4b633f5a564a422d473
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-08 13:02:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.56 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,25 +1,23 @@
 import java.util.*;
 
-//동명이인이 있기때문에 map으로 카운트해야함-> 해시 유형
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        //이름별 카운트
-        Map<String,Integer> runners=new HashMap<>();
-        for(String people : participant){
-            runners.put(people, runners.getOrDefault(people,0)+1);
-        }
+        Map<String,Integer> people=new HashMap<>();
         
-        //완주한 이름-1
-        for(String c : completion){
-            int n=runners.get(c);
-            runners.put(c, n-1);
+        
+        for(String p :  participant){
+            people.put(p,people.getOrDefault(p,0)+1);
         }
-        //값이 0이 아닌 선수는 완주하지 못한것
-        for(String fail : runners.keySet()){
-            if(runners.get(fail)!=0){
-                return fail;
+        for(String c : completion){
+            if(people.containsKey(c)){
+                people.put(c,people.get(c)-1);                
             }
         }
+        
+        for(String f : people.keySet()){
+            if(people.get(f)!=0) return f;
+        }
+        
         return "";
     }
 }
