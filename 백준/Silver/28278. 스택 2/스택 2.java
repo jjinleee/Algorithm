105b5274ec7850c1f1a2101323583b93deb0c095
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-10 09:11:55 +0900

    [Silver IV] Title: 스택 2, Time: 932 ms, Memory: 308992 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,37 +1,33 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-
+        
         StringBuilder sb= new StringBuilder();
-        Stack<Integer> stack= new Stack<>();
+        Stack<Integer> stack=new Stack<>();
         for(int i=0;i<n;i++){
-            StringTokenizer st= new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-
-            if(a==1) stack.push(Integer.parseInt(st.nextToken()));
-            else if(a==2){
-                if(!stack.isEmpty()) {
+            String[] s=br.readLine().split(" ");
+            if(s[0].equals("1")){
+                stack.push(Integer.parseInt(s[1]));
+            } else if(s[0].equals("2")){
+                if(!stack.isEmpty()){
                     sb.append(stack.pop()).append('\n');
-                }
+                } else sb.append(-1).append('\n');
+            } else if(s[0].equals("3")){
+                sb.append(stack.size()).append('\n');
+            } else if(s[0].equals("4")){
+                if(!stack.isEmpty()) sb.append(0).append('\n');
+                else sb.append(1).append('\n');
+            } else{
+                if(!stack.isEmpty()) sb.append(stack.peek()).append('\n');
                 else sb.append(-1).append('\n');
-            }
-            else if(a==3) sb.append(stack.size()).append('\n');
-            else if(a==4) {
-                if(stack.isEmpty()) sb.append(1).append('\n');
-                else sb.append(0).append('\n');
-            }
-            else if(a==5){
-                if(stack.isEmpty()) sb.append(-1).append('\n');
-                else sb.append(stack.peek()).append('\n');
             }
         }
 
         System.out.println(sb.toString());
+
     }
 }
