bc2f394ed2964695830641f4f56ab3d9116e11d5
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-10 09:37:27 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 180 ms, Memory: 17400 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,43 +1,43 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        
         StringBuilder sb= new StringBuilder();
-
         while(true){
-            String s=br.readLine();
+            String s= br.readLine();
             if(s.equals(".")) break;
-
             boolean isValid=true;
 
-            Stack<Character> stack= new Stack<>();
-            for(char c: s.toCharArray()){
-                if(c=='[') stack.push('[');
-                else if(c=='(') stack.push('(');
-                else if(c==']'){
-                    if(!stack.isEmpty() && stack.peek()=='[') stack.pop();
-                    else {
+            Stack<Character> stack=new Stack<>();
+            for(char c : s.toCharArray()){
+                if(c=='(') stack.push('(');
+                else if(c==')'){
+                    if(!stack.isEmpty() && stack.peek()=='('){
+                        stack.pop();
+                    } else{
                         isValid=false;
                         break;
                     }
-                }
-                else if(c==')'){
-                    if(!stack.isEmpty() && stack.peek()=='(') stack.pop();
-                    else{
+                } else if(c=='['){
+                    stack.push('[');
+                } else if(c==']'){
+                    if(!stack.isEmpty() && stack.peek()=='[' ){
+                        stack.pop();
+                    } else{
                         isValid=false;
                         break;
                     }
                 }
             }
+            if(!stack.isEmpty() || !isValid) sb.append("no").append('\n');
+            else sb.append("yes").append('\n');
 
-            if(stack.isEmpty() && isValid) sb.append("yes").append('\n');
-            else sb.append("no").append('\n');
         }
-        
-        System.out.println(sb);
+
+        System.out.println(sb.toString());
+
     }
 }
