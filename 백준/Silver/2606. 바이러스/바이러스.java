71ab20672f5d8e49815c919e52d83b6c2ac7fbde
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-11 14:00:48 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,47 @@
 import java.io.*;
 import java.util.*;
 
-
-public class Main { 
+public class Main {
+    static StringBuilder sb =new StringBuilder();
+    static List<List<Integer>> graph=new ArrayList<>();
     static int cnt=0;
-    static boolean[] visited;
-    static ArrayList<Integer>[] graph;
-        public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
         int m=Integer.parseInt(br.readLine());
-        
-        visited=new boolean[n+1];
-        graph=new ArrayList[n+1];
-        for(int i=1;i<=n;i++){
-            graph[i]=new ArrayList<>();
+
+        //그래프초기화
+        for(int i=0;i<=n;i++){
+            graph.add(new ArrayList<>());
         }
 
         for(int i=0;i<m;i++){
-            StringTokenizer st= new StringTokenizer(br.readLine());
-
+            StringTokenizer  st=new StringTokenizer(br.readLine());
             int a=Integer.parseInt(st.nextToken());
             int b=Integer.parseInt(st.nextToken());
 
-            graph[a].add(b);
-            graph[b].add(a);
+            //양방향 연결
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
-        
-        dfs(1);
-        System.out.println(cnt-1);
-    }
-    static void dfs(int node){
+
+        for (List<Integer> neighbors : graph) {
+            Collections.sort(neighbors);
+        }
+
+        boolean[] visited=new boolean[n+1];
+        dfs(1,visited);
+
+        System.out.println(cnt-1); //1번컴퓨터 제외
+    }    
+    static void dfs(int node, boolean[] visited){
         visited[node]=true;
         cnt++;
-
-        for(int n:graph[node]){
-            if(!visited[n]){
-                dfs(n);
+        for(int next: graph.get(node)){
+            if(!visited[next]){
+                dfs(next, visited);
             }
         }
     }
+  
 }
