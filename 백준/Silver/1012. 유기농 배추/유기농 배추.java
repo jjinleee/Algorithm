1495865dfad981d73293412ec44623b84762101f
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-11 16:45:05 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15956 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,63 +1,76 @@
 import java.io.*;
 import java.util.*;
 
+public class Main {
+    static StringBuilder sb= new StringBuilder();
+    static int[][] ground;
+    static boolean[][] visited;
+    static int[] dx={-1,1,0,0};
+    static int[] dy={0,0,-1,1};
+    static int m,n;
 
-public class Main {    
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t=Integer.parseInt(br.readLine());   
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
 
-        for(int i=0;i<t;i++){
-            StringTokenizer st= new StringTokenizer(br.readLine());
-            int m=Integer.parseInt(st.nextToken());
-            int n=Integer.parseInt(st.nextToken());
+
+        for(int test=0;test<t;test++){
+            int cnt=0;
+            StringTokenizer st=new StringTokenizer(br.readLine());
+
+            m=Integer.parseInt(st.nextToken());
+            n=Integer.parseInt(st.nextToken());
             int k=Integer.parseInt(st.nextToken());
 
-            //밭 정보
-            int[][] ground=new int[n][m];
-            boolean[][] visited=new boolean[n][m];
-            for(int j=0;j<k;j++){
+            ground=new int[m][n];
+            
+            
+            for(int i=0;i<k;i++){
                 st=new StringTokenizer(br.readLine());
                 int x=Integer.parseInt(st.nextToken());
                 int y=Integer.parseInt(st.nextToken());
 
-                ground[y][x]=1;
+                ground[x][y]=1;
+                
             }
-            int cnt=0;
 
-            for (int y = 0; y < n; y++) {
-                for (int x = 0; x < m; x++) {
-                    if (ground[y][x] == 1 && !visited[y][x]) {
-                        dfs(n, m, y, x, ground, visited);
+            visited=new boolean[m][n];
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                    if(!visited[i][j] && ground[i][j]==1){
+                        bfs(i,j);
                         cnt++;
                     }
                 }
-            }
-            System.out.println(cnt);
+            }     
+            sb.append(cnt).append('\n');       
         }
-    }
-    static void dfs(int n, int m, int x, int y, int[][] ground, boolean[][] visited){
-        int[] dx={-1,1,0,0};
-        int[] dy={0,0,1,-1};
 
-        Stack<int[]> stack=new Stack<>();
-        stack.push(new int[]{x,y});
-        visited[x][y]=true;
+        System.out.println(sb.toString());
+ 
+    }    
+    
+    static void bfs(int x, int y){
+       Queue<int[]> q=new LinkedList<>();
+       q.offer(new int[]{x,y});
+       visited[x][y]=true;
 
-        while(!stack.isEmpty()){
-            int[] cur=stack.pop();
+       while(!q.isEmpty()){
+            int[] curr=q.poll();
 
-            for(int d=0;d<4;d++){
-                int nx=cur[0]+dx[d];
-                int ny=cur[1]+dy[d];
+            for(int i=0;i<4;i++){
+                int nx=curr[0]+dx[i];
+                int ny=curr[1]+dy[i];
 
-                if(nx>=0 && nx<n && ny>=0 && ny<m ){
+                if(nx>=0 && ny>=0 && nx<m && ny<n){
                     if(!visited[nx][ny] && ground[nx][ny]==1){
+                        q.offer(new int[]{nx,ny});
                         visited[nx][ny]=true;
-                        stack.push(new int[]{nx,ny});
-                    }
+                    }                   
                 }
+
             }
-        }
+       }
+
     }
 }
