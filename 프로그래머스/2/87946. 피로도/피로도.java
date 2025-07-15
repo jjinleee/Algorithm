3f7f570c717e8d6a6ae399b9865deb68b1f6900b
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-15 09:28:11 +0900

    [level 2] Title: 피로도, Time: 3.09 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,25 @@
 import java.util.*;
 
 class Solution {
-    static int max=0;
+    static int n;
+    static int answer=0;
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited=new boolean[dungeons.length];
-        dfs(k,dungeons,visited,0);
+        n=dungeons.length;
+        boolean[] visited=new boolean[n];
         
-        return max;
+        dfs(k,dungeons,visited,0);
+        return answer;
     }
-    //모든경로를 탐색하는 dfs에 오류있음
-    static void dfs(int k, int[][] dungeons, boolean[] visited,int cnt){   
-        max=Math.max(max, cnt);
-            
-        for(int i=0;i<dungeons.length;i++){
-            //방문하지 않았고 최소필요도 만족하면
+    //던전순서
+    static void dfs(int k, int[][] dungeons, boolean[] visited, int cnt ){
+        answer=Math.max(answer, cnt);
+                
+        
+        for(int i=0;i<n;i++){
             if(!visited[i] && k>=dungeons[i][0]){
                 visited[i]=true;
-                dfs(k-dungeons[i][1], dungeons, visited,cnt+1);
+                dfs(k-dungeons[i][1], dungeons, visited, cnt+1);
                 visited[i]=false;
-                
             }
         }
     }
