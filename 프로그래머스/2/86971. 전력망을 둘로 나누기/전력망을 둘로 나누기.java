23a592d7942e9bf4f1097ddf11db76c6c56c603c
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-15 11:23:43 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.72 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -2,51 +2,48 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer=Integer.MAX_VALUE;
+        int answer=n;
+        boolean[] visited=new boolean[n+1];
+        
         for(int i=0;i<wires.length;i++){
-            //트리 초기화
-            List<List<Integer>> tree= new ArrayList<>();
+            List<List<Integer>> graph=new ArrayList<>();
             for(int j=0;j<=n;j++){
-                tree.add(new ArrayList<>());
+                graph.add(new ArrayList<>());
             }
-            //간선 하나씩 제거
-            for(int j=0;j<wires.length;j++){
+            //하나씩 끊어봄 
+            for(int j=0;j< wires.length;j++){
                 if(i==j) continue;
                 int a=wires[j][0];
                 int b=wires[j][1];
-                
-                tree.get(a).add(b);
-                tree.get(b).add(a);
+                graph.get(a).add(b);
+                graph.get(b).add(a);
             }
-            
-            int cnt=bfs(1,tree,n);
+            visited=new boolean[n+1];
+            int cnt=bfs(1,visited,graph);
             int diff=Math.abs(n-2*cnt);
             answer=Math.min(answer, diff);
-        }        
+        }
         return answer;
+    
     }
-    //한쪽 네트워크에 연결된 노드 수를 셈
-    static int bfs(int start, List<List<Integer>> tree,int n){
-        //외부에 전달할 필요없이 내부에서 독립적으로 세면되니까 내부에서 visited 정의
-        boolean[] visited=new boolean[n+1];
-        Queue<Integer> queue=new LinkedList<>();
+    //송전탑몇개인지 카운트
+    static int bfs(int start,boolean[] visited, List<List<Integer>> graph){
+        int count=1;
+        Queue<Integer> q= new LinkedList<>();
+        q.offer(start);
+        visited[start]=true;
         
-        queue.offer(start); //시작노드삽입
-        visited[start]=true; 
-        
-        int cnt=1;
-        
-        while(!queue.isEmpty()){
-            int current=queue.poll();
-            for(int next : tree.get(current)){
-                if(!visited[next]){
-                    visited[next]=true;
-                    queue.offer(next);
-                    cnt++;
+        while(!q.isEmpty()){
+            int curr=q.poll();
+            for(int n : graph.get(curr)){
+                if(!visited[n]){
+                    q.offer(n);
+                    visited[n]=true;
+                    count++;
                 }
             }
         }
-        
-        return cnt;
+        return count;
     }
+
 }
