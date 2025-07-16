445e28846daf30d93a5ae1e656e51a3f3ee45a36
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-07-16 09:42:21 +0900

    [level 2] Title: 모음 사전, Time: 33.34 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
-//dfs로 a,e,i,o,u 들어갈때마다 카운트증가
 class Solution {
-    static String[] vowels={"A","E","I","O","U"};
+    static List<String> list=new ArrayList<>();
+    static char[] c={'A','E','I','O','U'};
     public int solution(String word) {
-        //리스트에 순차적으로 담고 해당 단어의 인덱스+1을 가져오면 됨
-        List<String> list= new ArrayList<>();
-        dfs(list, "",0);
-        int answer=list.indexOf(word);
-        
-        return answer;
+        dfs("",0);
+        for(int i=0;i<list.size();i++){
+            if(word.equals(list.get(i))){
+                return i;
+            }
+        }
+        return 0;
     }
-    static void dfs(List<String> list, String current, int idx){
-        if(idx>5){
-            return;
+    static void dfs(String str,int dept ){
+        list.add(str);
+        if(dept==5) return;
+        for(int i=0;i<5;i++){
+            dfs(str+c[i], dept+1);
         }
         
-        list.add(current);
-        for(String s :vowels){
-            dfs(list, current+s, idx+1);
-        }
     }
 }
