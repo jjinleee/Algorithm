cf4e29db7139fbdc2a3e6c8eaf923992f4568103
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-09-24 22:49:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.40 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,19 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String,Integer> people=new HashMap<>();
-        
-        
-        for(String p :  participant){
+        for(String p : participant){
             people.put(p,people.getOrDefault(p,0)+1);
         }
+        
         for(String c : completion){
-            if(people.containsKey(c)){
-                people.put(c,people.get(c)-1);                
-            }
+            people.put(c,people.get(c)-1);
         }
         
-        for(String f : people.keySet()){
-            if(people.get(f)!=0) return f;
+        for(String a : people.keySet()){
+            if(people.get(a)!=0) return a;
         }
         
         return "";
