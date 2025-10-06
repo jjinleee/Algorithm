baf6e27992b3ea112cac6c12f7bb6bfc8aa6e623
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-06 12:00:20 +0900

    [level 2] Title: 기능개발, Time: 5.19 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,31 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue=new LinkedList<>();
-        List<Integer> list=new ArrayList<>(); 
+        List<Integer> result=new ArrayList<>();
+        Queue<Integer> queue= new LinkedList<>();
         
-        
-        //남은일수저장
-        int[] day=new int[progresses.length];
-        for(int i=0;i<progresses.length;i++){
-            day[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
+        int n=progresses.length;
+        int[] left=new int[n];
+        for(int i=0;i<n;i++){
+            left[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
+            System.out.println(left[i]);
         }
         
-        for(int d : day){
-            if(queue.isEmpty()) queue.offer(d);
-            else{
-                //먼저 끝나면 먼저 나감
+        int cnt=0;
+        for(int d : left){
+            if(queue.isEmpty()){
+                queue.offer(d);
+            } else{
                 if(queue.peek()<d){
-                    list.add(queue.size());
+                    result.add(queue.size());
                     queue.clear();
                     queue.offer(d);
-                } else{ //기다림
+                } else {
                     queue.offer(d);
                 }
             }
         }
-        list.add(queue.size());
+        result.add(queue.size());
         
-        return list.stream().mapToInt(i->i).toArray();
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
