e59ab61d4b9a319b9ea695c0c99b64e480ae691b
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-08 19:03:50 +0900

    [level 3] Title: 이중우선순위큐, Time: 56.54 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,20 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> min=new PriorityQueue<>();
-        PriorityQueue<Integer> max=new PriorityQueue<>(Collections.reverseOrder());
-        for(String o:operations){
-            String[] command=o.split(" ");
-            if(command[0].equals("I")){
-                min.add(Integer.parseInt(command[1]));
-                max.add(Integer.parseInt(command[1]));
-            }
-            else if(command[1].equals("1")){ //최댓값삭제
-                min.remove(max.poll());
-            } else { //최솟값삭제
-                max.remove(min.poll());
+        PriorityQueue<Integer> max= new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(String o : operations){
+            StringTokenizer st= new StringTokenizer(o);
+            char cmd=st.nextToken().charAt(0);
+            int n=Integer.parseInt(st.nextToken());
+            
+            if(cmd=='I'){
+                min.offer(n);
+                max.offer(n);
+            } else {
+                if(n==1) {
+                    min.remove(max.poll());
+                } else max.remove(min.poll());
             }
         }
         
-        return min.isEmpty() ? new int[]{0,0} : new int[]{max.peek(),min.peek()};
+        return !min.isEmpty() ? new int[]{max.peek(), min.peek()} : new int[]{0,0};
     }
 }
