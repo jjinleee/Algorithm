2d3f2480953248d29aca28be4cd7c65ca76f8aa8
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-09 10:50:01 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,18 +3,13 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] result=new int[commands.length];
-        
-        for(int i=0;i<commands.length;i++){
-            int start=commands[i][0];
-            int end=commands[i][1];
-            int index=commands[i][2];
-            
-            int[] temp=Arrays.copyOfRange(array,start-1, end);
-            Arrays.sort(temp);
-            
-            result[i]=temp[index-1];
-            
+        int i=0;
+        for(int[] c :commands){
+            int[] cop=Arrays.copyOfRange(array,c[0]-1, c[1]);
+            Arrays.sort(cop);
+            result[i++]=cop[c[2]-1];
         }
+        
         return result;
     }
 }
