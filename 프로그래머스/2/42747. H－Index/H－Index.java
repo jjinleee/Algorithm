4d768ddbac45ed5cccfea12d1de0753a67290870
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-09 11:41:01 +0900

    [level 2] Title: H-Index, Time: 1.00 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,9 +1,10 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
         int n=citations.length;
+        Arrays.sort(citations);
+        int mid=n/2;
         
         for(int i=0;i<n;i++){
             int h=n-i;
