b9e74980b84438926b226cde935780c5504bddde
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-09 17:20:42 +0900

    [level 1] Title: 최소직사각형, Time: 6.49 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,21 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int maxWidth=0;
-        int maxHeight=0;
+        int n=sizes.length;
+        int[] w= new int[n];
+        int[] h= new int[n];
         
-        //더 긴걸 세로로 놓기
+        int i=0;
         for(int[] s : sizes){
-            if(s[0]<=s[1]){
-                if(s[0]>maxWidth) maxWidth=s[0];
-                if(s[1]>maxHeight) maxHeight=s[1];
-            } else {
-                if(s[1]>maxWidth) maxWidth=s[1];
-                if(s[0]>maxHeight) maxHeight=s[0];
-            }
+            Arrays.sort(s);
+            w[i]=s[0];
+            h[i++]=s[1];
         }
+        Arrays.sort(w);
+        Arrays.sort(h);
         
-        
-        return maxHeight*maxWidth;
+    
+        return w[n-1]*h[n-1];
     }
 }
