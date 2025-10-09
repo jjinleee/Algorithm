c3369e62a7c0718aeffdb16459a3b2c75ed73177
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-10 00:12:02 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.89 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -10,8 +10,8 @@ class Solution {
             for(int j=0;j<=n;j++){
                 graph.add(new ArrayList<>());
             }
-            //하나씩 끊어봄 
-            for(int j=0;j< wires.length;j++){
+            
+            for(int j=0;j<wires.length;j++){
                 if(i==j) continue;
                 int a=wires[j][0];
                 int b=wires[j][1];
@@ -19,16 +19,17 @@ class Solution {
                 graph.get(b).add(a);
             }
             visited=new boolean[n+1];
-            int cnt=bfs(1,visited,graph);
+            int cnt=bfs(1, visited, graph);
             int diff=Math.abs(n-2*cnt);
             answer=Math.min(answer, diff);
         }
+        
         return answer;
-    
     }
-    //송전탑몇개인지 카운트
-    static int bfs(int start,boolean[] visited, List<List<Integer>> graph){
+    
+    static int bfs(int start, boolean[] visited, List<List<Integer>> graph){
         int count=1;
+        
         Queue<Integer> q= new LinkedList<>();
         q.offer(start);
         visited[start]=true;
@@ -43,7 +44,7 @@ class Solution {
                 }
             }
         }
+        
         return count;
     }
-
 }
