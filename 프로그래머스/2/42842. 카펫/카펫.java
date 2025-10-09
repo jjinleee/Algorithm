9b92cae0ca206371ffa99a6114c9f1bba0aee88d
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-09 22:26:36 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,18 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        
         for(int i=1;i<=yellow;i++){
             if(yellow%i==0){
-                int yx=i;
-                int yy=yellow/i; 
+                int yx=yellow/i;
                 
-                if(2*(yx+2) + 2*yy==brown) {
-                    return yx>=yy ?new int[]{yx+2, yy+2} : new int[]{yy+2,yx+2};
-                }
+                if(2*(yx+2)+2*i==brown) return new int[]{yx+2,i+2};
             }
-
         }
+        
         return new int[]{0,0};
     }
 }
