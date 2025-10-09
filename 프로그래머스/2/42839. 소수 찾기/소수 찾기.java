ad4a28eda0122fc9fa03367cc300cf475b2b215f
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-09 21:23:05 +0900

    [level 2] Title: 소수 찾기, Time: 139.96 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,42 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer> list=new HashSet<>();
+    static Set<Integer> list= new HashSet<>();
     public int solution(String numbers) {
+        
+        
         boolean[] visited=new boolean[numbers.length()];
         
-        dfs("",visited, numbers);
-        int cnt=0;
+        dfs(visited, numbers, "");
+        int cnt=0; 
+        
         for(int l : list){
-            //stem.out.println("소수 : "+ l);
             if(isPrime(l)) cnt++;
         }
-        
         return cnt;
     }
-    
-    static void dfs(String current, boolean[] visited, String numbers){
-        if(!current.isEmpty()){
-            list.add(Integer.parseInt(current));
-
+    static void dfs(boolean[] visited,String numbers, String num){
+        if(!num.isEmpty()){
+            list.add(Integer.parseInt(num));
         }
-        
         for(int i=0;i<numbers.length();i++){
             if(!visited[i]){
                 visited[i]=true;
-                dfs(current+numbers.charAt(i), visited, numbers);
+                dfs(visited, numbers, num+numbers.charAt(i));
                 visited[i]=false;
             }
         }
+        
     }
-    
     static boolean isPrime(int n){
-        if(n==1) return false;
-        if(n==2 || n==3 || n==5) return true;
-        if(n%2==0 || n% 3==0 || n%5==0) return false;
+        if(n==0 || n==1) return false;
+        if(n==2 || n==3 || n==5)  return true;
+        if(n%2==0 || n%3==0 || n%5==0) return false;
         for(int i=7;i<n;i++){
             if(n%i==0) return false;
         }
+        
         return true;
     }
 }
