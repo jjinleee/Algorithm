d8c700607e536db330ec2acc73462008c4e249eb
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-10 21:11:54 +0900

    [level 1] Title: 체육복, Time: 0.17 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        Set<Integer> lostPeople=new HashSet<>();
-        Set<Integer> reservePeople= new HashSet<>();
+        Set<Integer> lo=new HashSet<>();
+        Set<Integer> re=new HashSet<>();
         
-        for(int l : lost) lostPeople.add(l);
-        for(int r : reserve) reservePeople.add(r);
+        for(int l : lost) lo.add(l);
+        for(int r : reserve){
+            if(lo.contains(r)){
+                lo.remove(r);
+            } else re.add(r);
+        }
+        int max=n-lo.size();
         
-        Set<Integer> intersection=new HashSet<>(lostPeople);
-        intersection.retainAll(reservePeople);
-        lostPeople.removeAll(intersection);
-        reservePeople.removeAll(intersection);
-        
-        //treeset은 자동정렬기능이 있어서 정렬순서로 여벌 체육복을 
-        //가진 학생들을 순회할 수 있다. 
-        for(int l : new TreeSet<>(reservePeople)){
-            if(lostPeople.contains(l-1)){
-                lostPeople.remove(l-1);
-            } else if( lostPeople.contains(l+1)){
-                lostPeople.remove(l+1);
+        for(int l : new ArrayList<>(lo)){
+            if(re.contains(l-1)){
+                max++;
+                re.remove(l-1);
+            } else if( re.contains(l+1)){
+                max++;
+                re.remove(l+1);
             }
         }
         
-        return n-lostPeople.size();
+        return max;
     }
 }
