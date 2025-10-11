7332093a40cfe7631c40af8aa6349f1e7dd7d6d5
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-11 17:50:36 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
-SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
-FROM DEVELOPER_INFOS
-WHERE SKILL_1='Python' OR SKILL_2='Python' OR SKILL_3='Python'
-ORDER BY ID;
+select id, email, first_name, last_name
+from developer_infos
+where 'Python' in (skill_1, skill_2, skill_3)
+order by id 
