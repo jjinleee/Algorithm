a68c0ed895fe14307f686ca3abe25f940e004641
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-13 18:59:02 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,14 +1,30 @@
--- 코드를 작성해주세요
-SELECT 
-( CASE 
-  WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY LIKE 'FRONT%') AND 
- SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME='PYTHON') THEN 'A'
- WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME='C#') THEN 'B'
- WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY LIKE 'FRONT%') THEN 'C'
- ELSE NULL
- END) AS GRADE
- , ID, EMAIL
- FROM DEVELOPERS
- GROUP BY GRADE, ID, EMAIL
- HAVING GRADE IS NOT NULL
- ORDER BY GRADE, ID
+WITH flags AS (
+  SELECT
+    d.id,
+    d.email,
+    -- Front End 스킬 보유 여부(개수>0)
+    SUM(CASE WHEN s.category = 'Front End' AND (d.skill_code & s.code) <> 0 THEN 1 ELSE 0 END) AS has_fe,
+    -- Python 보유 여부
+    MAX(CASE WHEN s.name = 'Python' AND (d.skill_code & s.code) <> 0 THEN 1 ELSE 0 END) AS has_python,
+    -- C# 보유 여부
+    MAX(CASE WHEN s.name = 'C#' AND (d.skill_code & s.code) <> 0 THEN 1 ELSE 0 END) AS has_csharp
+  FROM developers d
+  JOIN skillcodes s
+    ON (d.skill_code & s.code) <> 0
+  GROUP BY d.id, d.email
+)
+SELECT
+  CASE
+    WHEN has_fe > 0 AND has_python = 1 THEN 'A'
+    WHEN has_csharp = 1 THEN 'B'
+    WHEN has_fe > 0 THEN 'C'
+  END AS grade,
+  id,
+  email
+FROM flags
+WHERE
+  -- 등급이 존재하는 개발자만
+  (has_fe > 0 AND has_python = 1) OR
+  (has_csharp = 1) OR
+  (has_fe > 0)
+ORDER BY grade, id;
