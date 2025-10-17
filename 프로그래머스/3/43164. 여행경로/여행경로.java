c88cb5ff3fac694bbbe6e458a0cb08b203a5b768
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-17 18:42:02 +0900

    [level 3] Title: 여행경로, Time: 0.75 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,41 +1,35 @@
 import java.util.*;
 
-
 class Solution {
-    
-    static List<String> answer;
+    static List<String> answer=new ArrayList<>();
     public String[] solution(String[][] tickets) {
-        Arrays.sort(tickets, (a,b)-> a[0].compareTo(b[0])==0 ? a[1].compareTo(b[1]): a[0].compareTo(b[0]));
-        
-        boolean[] visited= new boolean[tickets.length];
-
+        boolean[] visited=new boolean[tickets.length];
         List<String> w=new ArrayList<>();
+        
+        answer.clear();
+        Arrays.sort(tickets, (a,b)-> a[0].compareTo(b[0])==0 ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
         w.add("ICN");
-        answer=null;
-        dfs("ICN",tickets, visited,w);
-        
-        
+        dfs(tickets, visited, "ICN",w);
+       
         return answer.toArray(new String[0]);
     }
-    static void dfs(String current, String[][] tickets, boolean[] visited, List<String> way){
-        if(way.size()==tickets.length+1){
+    static void dfs(String[][] tickets,boolean[] visited,  String current, List<String> way){
+        if(way.size()==tickets.length+1) {
             answer=new ArrayList<>(way);
             return;
         }
+        
         for(int i=0;i<tickets.length;i++){
             if(!visited[i] && current.equals(tickets[i][0])){
-                if(answer==null){
-                    
+                if(answer.isEmpty()){
+                    visited[i]=true;
+                    way.add(tickets[i][1]);
+                    dfs(tickets, visited, tickets[i][1], way);    
+                    visited[i]=false;
+                    way.remove(way.size()-1);
+                }
+
                 
-                way.add(tickets[i][1]);
-                current=tickets[i][1];
-                visited[i]=true;
-                dfs(current,tickets, visited, way);
-                //백트래킹
-                current=tickets[i][0];
-                visited[i]=false;
-                way.remove(way.size()-1);
-            }
             }
         }
     }
