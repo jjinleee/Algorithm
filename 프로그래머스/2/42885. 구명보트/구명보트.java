4211e870912a4a035da680f2fb211ef7ebe9b29e
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-28 23:39:07 +0900

    [level 2] Title: 구명보트, Time: 11.38 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,24 +2,21 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
-        int answer = 0;
         Arrays.sort(people);
-        
+        int cnt = 0;
         int left=0;
         int right=people.length-1;
         
         while(left<=right){
-            //가장 가벼운 +무거운 <=제한 이면 둘다태움
             if(people[left]+people[right]<=limit){
                 left++;
                 right--;
-                answer++;
-            } else{ //안되면 무거운 사람만 태움
+                cnt++;
+            } else{
                 right--;
-                answer++;
+                cnt++;
             }
         }
-        
-        return answer;
+        return cnt;
     }
 }
