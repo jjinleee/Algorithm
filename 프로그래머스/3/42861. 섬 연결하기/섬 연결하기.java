e8f741f41a1c29cbce81fd978cae6afd03a4ab20
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-10-29 20:09:13 +0900

    [level 3] Title: 섬 연결하기, Time: 0.82 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -3,14 +3,10 @@ import java.util.*;
 class Solution {
     static int[] parent;
     public int solution(int n, int[][] costs) {
-        //자기 자신을 루트노드로 초기화
         parent=new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
+        for(int i=0;i<n;i++) parent[i]=i;
         
-        //비용순으로 정렬
-        Arrays.sort(costs, (a,b)->a[2]-b[2]);
+        Arrays.sort(costs, (a,b)-> Integer.compare(a[2],b[2]));
         
         int totalCost=0;
         int cnt=0;
@@ -20,25 +16,24 @@ class Solution {
             int b=edge[1];
             int cost=edge[2];
             
-            //서로 연결되지 않았다면 연결
             if(find(a)!=find(b)){
                 union(a,b);
                 totalCost+=cost;
                 cnt++;
-                if(cnt==n-1) break; //모든 간선 연결했으면 종료
+                if(cnt==n-1) break;
             }
         }
-        
         return totalCost;
     }
+    //모든섬이 통행가능한지 확인
     static int find(int x){
         if(parent[x]==x) return x;
-        else return parent[x]=find(parent[x]); //이부분 
+        return parent[x]=find(parent[x]);
     }
     
     static void union(int a, int b){
-        int roota=parent[a];
-        int rootb=parent[b];
+        int roota=find(a);
+        int rootb=find(b);
         if(roota<rootb) parent[rootb]=roota;
         else parent[roota]=rootb;
     }
