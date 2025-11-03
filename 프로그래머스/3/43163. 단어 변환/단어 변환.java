7a20c87c3f03729a244f0a795d4806b641b70ee7
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-11-03 22:20:01 +0900

    [level 3] Title: 단어 변환, Time: 0.34 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,44 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static class Word{
-        String word;
-        int cnt;
-        
-        Word(String word, int cnt){
-            this.word=word;
-            this.cnt=cnt;
-        }
-    }
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        Queue<Word> q=new LinkedList<>();
-        boolean[] visited= new boolean[words.length];
+        boolean[] visited=new boolean[words.length];
+        int cnt=dfs(begin,target,words, 0 , visited);
         
-        q.offer(new Word(begin,0));
-        while(!q.isEmpty()){
-            Word c=q.poll();
-            
-            if(c.word.equals(target)) return c.cnt;
-            
-            for(int i=0;i<words.length;i++){
-                if(isDiff(c.word,words[i]) && !visited[i]){
-                    q.offer(new Word(words[i],c.cnt+1));
-                    visited[i]=true;
-                }
-            }
-        }
-        return 0;
+        return cnt==Integer.MAX_VALUE ? 0 : cnt;
     }
-                            
+    static int dfs(String current, String target, String[] words, int depth, boolean[] visited){
+        if(current.equals(target)) return depth;        
+        
+        int best=Integer.MAX_VALUE;
+        for(int i=0;i<words.length;i++){
+            if(!visited[i] && isDiff(current, words[i])){
+                visited[i]=true;
+                int res=dfs(words[i], target, words, depth+1, visited);
+                if(res<best) best=res;
+                visited[i]=false; 
+            }            
+        }
+        
+        return best;
+    }
     static boolean isDiff(String a, String b){
         int cnt=0;
         
-        for(int i=0;i<b.length();i++){
-            if(a.charAt(i)!=b.charAt(i)) cnt++;
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)!= b.charAt(i)) cnt++;
         }
-        
         return cnt==1 ? true : false;
     }
-                  
 }
