594df8a2501ebc9069356620a20cdb1f459c5cea
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-11-14 19:03:56 +0900

    [level 2] Title: 삼각 달팽이, Time: 45.31 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -2,35 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n*(n+1))/2];
-        int[][] matrix = new int[n][n];
-
-        int x = -1, y = 0;
-        int num = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) { 	
-                if (i % 3 == 0) {         //세로
-                    x++;
-                } else if (i % 3 == 1) {  //가로
-                    y++;
-                } else if (i % 3 == 2) {  //대각선
-                    x--;
-                    y--;
-                }
-                matrix[x][y] = num++;
+        int[][] map=new int[n][n];
+        int max=n*(n+1)/2;
+        
+        int r=0,c=0,dir=0;
+        int[] dr={1,0,-1};
+        int[] dc={0,1,-1};
+        
+        int num=1;
+        while(num<=max){
+            map[r][c]=num++;
+            //방향 변경 여부 판단
+            int nr=r+dr[dir];
+            int nc=c+dc[dir];
+            if(nr <0 || nc<0 || nr>n-1 || nc>n-1 || map[nr][nc]!=0){
+                dir=(dir+1)%3; //방향변경
+                nr=r+dr[dir];
+                nc=c+dc[dir];
+                
+            } 
+           r=nr;
+            c=nc;
+        }
+        
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(map[i][j]!=0) list.add(map[i][j]);
             }
         }
         
-        int k = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == 0) 
-                	break;
-                answer[k++] = matrix[i][j];
-            }
-        }
-
-        return answer;
+        return list.stream().mapToInt(i->i).toArray();
     }
 }
