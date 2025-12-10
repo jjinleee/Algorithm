45ce80a56a8616688f42517ad8bacaaeb9a3cb66
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-12-10 22:21:09 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 40.74 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -2,45 +2,49 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-    int n = diffs.length;
-
-    int left = 1;
-    int right = 1_000_000_000; // 혹은 max(diffs) 같은 적당한 상한
-    int answer = right;
-
-    while (left <= right) {
-        int mid = (left + right) / 2;
-
-        if (canSolve(diffs, times, limit, mid)) {
-            answer = mid;
-            right = mid - 1; // 더 낮은 level로도 가능한지 확인
-        } else {
-            left = mid + 1;  // 더 높은 level 필요
-        }
-    }
-
-    return answer;
-}
-
-private boolean canSolve(int[] diffs, int[] times, long limit, int level) {
-    int n = diffs.length;
-    long time = times[0];
-
-    for (int i = 1; i < n; i++) {
-        long cur = times[i];
-        long prev = times[i-1];
-
-        if (diffs[i] <= level) {
-            time += cur;
-        } else {
-            int d = diffs[i] - level;
-            // 이 부분은 long 캐스팅 조심
-            time += ((long)prev + (long)cur) * d + cur;
+        int n=diffs.length;
+        int maxDiff = 0;
+        for (int d : diffs) {
+            if (d > maxDiff) maxDiff = d;
         }
 
-        if (time > limit) return false; // 가지치기
+        int left = 1;
+        int right = maxDiff;
+        int answer = maxDiff;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (canClear(diffs, times, limit, mid)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
     }
 
-    return time <= limit;
-}
+    private boolean canClear(int[] diffs, int[] times, long limit, int level) {
+        int n = diffs.length;
+        long time = times[0];
+
+        for (int i = 1; i < n; i++) {
+            long cur = times[i];
+            long prev = times[i - 1];
+
+            if (diffs[i] <= level) {
+                time += cur;
+            } else {
+                int diffGap = diffs[i] - level;
+                long add = ((long) prev + cur) * diffGap + cur;
+                time += add;
+            }
+
+            if (time > limit) return false;
+        }
+
+        return time <= limit;
+    }
 }
