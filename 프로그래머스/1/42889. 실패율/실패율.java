19e4d81adac834e04346843b635e67714c5bdfb1
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-12-13 15:39:13 +0900

    [level 1] Title: 실패율, Time: 4.78 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,44 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] count = new int[N + 2];     // 각 스테이지에 머무는 사람 수
-
-        for (int s : stages) {
-            if (s <= N) {
-                count[s]++;
+        int[] count = new int[N+2];
+        int total=stages.length;
+        
+        for(int s : stages){
+            if(s<=N) count[s]++;
+        }
+        
+        double[] fail=new double[N+1];
+        for(int i=1;i<=N;i++){
+            if(total==0) fail[i]=0.0;
+            else{
+                fail[i]=(double)count[i]/total;
+                total-=count[i];
             }
+            
         }
-
-        double[] fail = new double[N + 1]; // 1 ~ N
-        int players = stages.length;       // 해당 스테이지에 도달한 사람 수
-
-        for (int i = 1; i <= N; i++) {
-            if (players == 0) {
-                fail[i] = 0.0;
-            } else {
-                fail[i] = (double) count[i] / players;
-                players -= count[i];
-            }
+        Integer[] order=new Integer[N];
+        for(int i=0;i<N;i++){
+            order[i]=i+1;
         }
-
-        // 스테이지 번호 배열
-        Integer[] order = new Integer[N];
-        for (int i = 0; i < N; i++) {
-            order[i] = i + 1;
-        }
-
-        // 실패율 내림차순, 같으면 스테이지 번호 오름차순
-        Arrays.sort(order, (a, b) -> {
-            if (fail[b] > fail[a]) return 1;
-            else if (fail[b] < fail[a]) return -1;
-            else return Integer.compare(a, b);
+        
+        Arrays.sort(order, (a,b)-> {
+            if(fail[b]>fail[a]) return 1;
+            else if (fail[b]<fail[a]) return -1;
+            else return Integer.compare(a,b);
         });
-
-        int[] answer = new int[N];
-        for (int i = 0; i < N; i++) {
-            answer[i] = order[i];
+            
+        int[] answer=new int[N];
+        for(int i=0;i<N;i++){
+            answer[i]=order[i];
         }
-
+            
+            
         return answer;
     }
 }
