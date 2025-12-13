7c6a770fadf7a8fb45f87b305abdfbde7a9c1bbc
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-12-14 02:15:01 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 7.52 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 class Solution {
-    Map<String,Integer> countMap=new HashMap<>();
+    Map<String,Integer> countMap=new HashMap<>(); //조합별, 횟수
     public String[] solution(String[] orders, int[] course) {
         List<String> answer=new ArrayList<>();
         
-        for(int c : course){
+        for(int c : course){ //길이만큼 조합
             countMap.clear();
             
-            for(String order: orders){
+            for(String order: orders){ //각 주문에서 만들수 있는 조합
                 char[] arr=order.toCharArray();
-                Arrays.sort(arr);
+                Arrays.sort(arr); //정렬먼저
                 dfs(arr,0,c,new StringBuilder());
             }
             
+            //최대횟수조합 찾기
             int max=0;
             for(int v : countMap.values()){
                 if(v>=2) max=Math.max(max,v);
@@ -36,10 +37,10 @@ class Solution {
             return;
         }
         
-        for(int i=idx; i<arr.length;i++){
+        for(int i=idx; i<arr.length;i++){ //idx부터 시작하는게 핵심
             sb.append(arr[i]);
             dfs(arr, i+1, targetLen, sb);
-            sb.deleteCharAt(sb.length()-1);
+            sb.deleteCharAt(sb.length()-1); //이게 들어가야 다른 조합도 모두 찾을 수 있음(다음 i로 넘어가서 다른선택)
         }
     }
 }
