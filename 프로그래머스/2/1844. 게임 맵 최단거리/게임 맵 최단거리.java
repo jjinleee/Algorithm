591a7be67adf25378711de847bafa3c8a18ece7c
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-12-14 13:26:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.24 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,41 +1,37 @@
 import java.util.*;
 
 class Solution {
-    static int[] dx={-1,1,0,0};
-    static int[] dy={0,0,-1,1};
-    static int n,m;
+    int[] dx={-1,1,0,0};
+    int[] dy={0,0,-1,1};
+    int n;
+    int m;
     public int solution(int[][] maps) {
         n=maps.length;
         m=maps[0].length;
-        boolean[][] visited= new boolean[n][m];
         
-        int answer=bfs(maps, visited);
-        
-        return answer==1 ? -1 : answer;
-    }
-    static int bfs(int[][] maps, boolean[][] visited ){
-        Queue<int[]> q=new LinkedList<>();
-        
-        visited[0][0]=true;
+        boolean[][] visited= new boolean[n+1][m+1];
+        Queue<int[]> q= new LinkedList<>();
         q.offer(new int[]{0,0});
+        visited[0][0]=true;
         
         while(!q.isEmpty()){
-            int[] current=q.poll();
-            int x=current[0];
-            int y=current[1];
-            
+            int[] cur=q.poll();
+            int x=cur[0];
+            int y=cur[1];;
             for(int i=0;i<4;i++){
                 int nx=x+dx[i];
                 int ny=y+dy[i];
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && maps[nx][ny]==1 ){
-                    visited[nx][ny]=true;
-                    q.offer(new int[]{nx,ny});
-                    maps[nx][ny]+=maps[x][y];
+                if(nx>=0 && nx<n && ny>=0  && ny<m){
+                    if(!visited[nx][ny] && maps[nx][ny]==1){
+                        q.offer(new int[]{nx,ny});
+                        visited[nx][ny]=true;
+                        maps[nx][ny]=maps[x][y]+1;
+                    }
                 }
             }
         }
-        return maps[n-1][m-1];
         
+        return maps[n-1][m-1]==1 ? -1 : maps[n-1][m-1];
     }
 }
