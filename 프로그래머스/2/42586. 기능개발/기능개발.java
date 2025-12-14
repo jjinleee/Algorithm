ca5569e0d5c43afaae78130d2c025753113d053d
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2025-12-14 11:49:23 +0900

    [level 2] Title: 기능개발, Time: 3.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,26 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int n=progresses.length;
-        int[] remain=new int[n];
+        int[] day=new int[n];
         for(int i=0;i<n;i++){
-            remain[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
+            day[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
         }
         
         List<Integer> list=new ArrayList<>();
-        int start=remain[0];
-        int cnt=1;
-        for(int i=1;i<n;i++){
-            //덜걸리면 함꼐 배포
-            if(start>=remain[i]){
-                cnt++;
+        Deque<Integer> q=new ArrayDeque<>();
+        for(int d : day){
+            if(!q.isEmpty() && q.peekFirst()<d){
+                list.add(q.size());
+                q.clear();
+                q.offer(d);
             } else{
-                list.add(cnt);
-                cnt=1;
-                start=remain[i];
+                q.offerLast(d);
             }
         }
-        
-        list.add(cnt); //마지막 기능
+        list.add(q.size());
         
         return list.stream().mapToInt(i->i).toArray();
     }
