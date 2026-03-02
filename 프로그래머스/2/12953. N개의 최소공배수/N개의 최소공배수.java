e2dbb8b388369e03eab2bd975d002c301ae64f8f
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-02 15:24:01 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for(int i=1;i<arr.length;i++){
+            answer=lcm(arr[i],answer);
+        }
+        
+        return answer;
+    }
+    int lcm(int a, int b){
+        return a*b/gcd(b,a);
+    }
+    int gcd(int a, int b){
+        while(b!=0){
+            int tmp=a%b;
+            a=b;
+            b=tmp;
+            gcd(b, tmp);
+        }
+        return a;
+    }
+}
