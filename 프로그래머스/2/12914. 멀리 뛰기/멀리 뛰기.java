50fbfdd0b682c12052b4d11ec15142f1bcb76031
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-02 15:35:46 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.12 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        int[] arr=new int[2001];
+        arr[1]=1;
+        arr[2]=2;
+        arr[3]=3;
+        
+        for(int i=4;i<=n;i++){
+            arr[i]=(arr[i-1]+arr[i-2])%1234567;
+        }
+        return arr[n];
+    }
+}
