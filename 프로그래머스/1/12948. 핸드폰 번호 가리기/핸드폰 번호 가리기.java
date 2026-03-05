00fba667f8d81bbcb028eedba65469bbabfe9cd1
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-05 19:15:46 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.18 ms, Memory: 93 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String phone_number) {
+        int len=phone_number.length();
+        String answer="*".repeat(len-4)+phone_number.substring(len-4);
+        return answer;
+    }
+}
