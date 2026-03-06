51912de409780b7b4318b9cc0fcdead473639396
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-06 22:14:08 +0900

    [level 2] Title: 점 찍기, Time: 21.03 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        long dd = (long) d * d;
+
+        for (long x = 0; x <= d; x += k) {
+            long maxY = (long) Math.sqrt(dd - x * x);
+            answer += (maxY / k) + 1;
+        }
+
+        return answer;
+    }
+}
