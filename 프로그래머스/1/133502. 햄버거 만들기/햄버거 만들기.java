102f317972f84a3a90ddda356d41b1e17799e5af
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-07 20:06:26 +0900

    [level 1] Title: 햄버거 만들기, Time: 14.99 ms, Memory: 144 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        int[] stack=new int[ingredient.length];
+        
+        int idx=0;
+        for(int i : ingredient){  
+            stack[idx++]=i;
+            if(idx>=4){
+                //뒤에 4개 검사
+                if(stack[idx-1]==1 && stack[idx-2]==3 && stack[idx-3]==2 && stack[idx-4]==1){
+                    answer++;
+                    idx-=4;
+                }
+            }
+            
+
+        }
+        return answer;
+    }
+}
