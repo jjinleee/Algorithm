cb950775155af6c707645316ae44baf040be28bb
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-13 19:34:31 +0900

    [level 3] Title: 네트워크, Time: 0.86 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,23 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        boolean[] visited=new boolean[n];
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<n;i++) graph.add(new ArrayList<>());
         
-        boolean[] visited= new boolean[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(computers[i][j]==1){
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
+            }
+        }
         for(int i=0;i<n;i++){
             if(!visited[i]){
-               dfs(computers,n, i, visited);
-               answer++;
+                dfs(i, visited,graph );
+                answer++;
             }
         }
         return answer;
     }
-    static void dfs(int[][] computers,int n, int node, boolean[] visited ){
-        visited[node]=true;
-        
-        for(int i=0;i<n;i++){
-            if(computers[node][i]==1 && !visited[i]){
-                dfs(computers,n, i, visited);
+    void dfs(int node, boolean[] visited, List<List<Integer>> graph){
+        if(!visited[node]){
+            visited[node]=true;
+            for(int next : graph.get(node)){
+                dfs(next, visited, graph);
             }
         }
+        
+        return;
     }
 }
