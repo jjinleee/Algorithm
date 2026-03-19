2f0eab7793685f6e8b1da7a6860ea1e397b44f5b
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-03-19 21:52:06 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,6 @@
 -- 코드를 입력하세요
-SELECT NAME, DATETIME
-FROM ANIMAL_INS 
-WHERE ANIMAL_ID NOT IN (
-    SELECT ANIMAL_ID
-    FROM ANIMAL_OUTS
-)
-ORDER BY DATETIME
-FETCH FIRST 3 ROWS ONLY
+SELECT i.name, i.datetime
+from animal_ins i left join animal_outs o on i.animal_id=o.animal_id
+where o.animal_id is null
+order by i.datetime
+limit 3
