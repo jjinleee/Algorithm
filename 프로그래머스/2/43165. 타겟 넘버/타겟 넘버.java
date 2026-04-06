9e271abf4655f2c149377dd20e2bef257b8af571
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-04-06 23:07:57 +0900

    [level 2] Title: 타겟 넘버, Time: 4.49 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -4,16 +4,15 @@ class Solution {
     int answer=0;
     public int solution(int[] numbers, int target) {
         dfs(0,0,numbers, target);
-        
         return answer;
     }
-    void dfs(int cur, int idx, int[] numbers, int target){
-        if(idx==numbers.length){
+    void dfs(int cur,int i, int[] numbers, int target){
+        if(i==numbers.length){
             if(cur==target) answer++;
             return;
         }
         
-        dfs(cur+numbers[idx], idx+1, numbers, target);
-        dfs(cur-numbers[idx], idx+1, numbers, target);
+        dfs(cur+numbers[i],i+1, numbers,target);
+        dfs(cur-numbers[i],i+1, numbers,target);
     }
 }
