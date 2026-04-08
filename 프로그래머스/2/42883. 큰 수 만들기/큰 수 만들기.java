7649ff32df8b5e98aba1ccb7ad46fdbc074ab7f8
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-04-08 15:32:44 +0900

    [level 2] Title: 큰 수 만들기, Time: 90.87 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -5,9 +5,7 @@ class Solution {
         Stack<Integer> stack=new Stack<>();
         for(int i=0;i<number.length();i++){
             int n=number.charAt(i)-'0';
-            
-            //뒤의 숫자가 더크면 앞에 삭제하기
-            while(!stack.isEmpty() && stack.peek()<n && k>0){
+            while(!stack.isEmpty() && k>0 && stack.peek()<n){
                 stack.pop();
                 k--;
             }
@@ -18,7 +16,6 @@ class Solution {
             stack.pop();
             k--;
         }
-        
         StringBuilder sb= new StringBuilder();
         while(!stack.isEmpty()) sb.append(stack.pop());
         String answer=sb.reverse().toString();
