7445b1097d1ddf84a126fd92bb71edee6f5e1986
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-04-26 12:01:00 +0900

    [level 2] Title: 단체사진 찍기, Time: 501.21 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,53 +1,52 @@
 import java.util.*;
 
 class Solution {
-     char[] friends={'A','C','F','J','M','N','R','T'};
-     boolean[] visited=new boolean[8]; 
-     char[] line=new char[8]; //현재줄
-     int answer=0;
+    int[] tmp=new int[26];
+    char[] line=new char[8];
+    char[] friends={'A','C','F','J','M','N','R','T'};
+    boolean[] visited=new boolean[8];
+    int answer=0;
     public int solution(int n, String[] data) {
-
         dfs(0,data);
-        
         return answer;
     }
-    void dfs(int dept, String[] data){
-        if(dept==8){
+    void dfs(int depth,String[] data){
+        if(depth==8){
             if(isValid(data)) answer++;
             return;
         }
         
         for(int i=0;i<8;i++){
-            if(visited[i]) continue;
-            visited[i]=true;
-            line[dept]=friends[i];
-            dfs(dept+1, data);
-            visited[i]=false;
+            if(!visited[i]){
+                visited[i]=true;
+                line[depth]=friends[i];
+                dfs(depth+1,data);
+                visited[i]=false;
+            }
         }
     }
     boolean isValid(String[] data){
-        int[] pos=new int[26];
-        
         for(int i=0;i<8;i++){
-            pos[line[i]-'A']=i;
+            tmp[line[i]-'A']=i; //각 알파벳이 몇번째인지 저장
         }
         
-        for(String condition : data){
-            char a=condition.charAt(0);
-            char b=condition.charAt(2);
-            char op=condition.charAt(3);
-            int num=condition.charAt(4)-'0';
+        for(String d : data){
+            char a=d.charAt(0);
+            char b=d.charAt(2);
+            char cond=d.charAt(3);
+            int n=d.charAt(4)-'0';
             
-            int gap=Math.abs(pos[a-'A']-pos[b-'A'])-1;
+            int real=Math.abs(tmp[b-'A']-tmp[a-'A'])-1;
             
-            if(op=='='){
-                if(gap!=num) return false;
-            } else if(op=='<'){
-                if(gap>=num) return false;
-            } else if(op=='>'){
-                if(gap<=num) return false;
+            if(cond=='='){
+                if(real!=n) return false;
+            } else if(cond=='>'){
+                if(real<=n) return false;
+            } else if(cond=='<'){
+                if(real>=n) return false;
             }
         }
+        
         return true;
     }
 }
