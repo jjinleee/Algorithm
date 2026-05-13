323f54f1faca557808d58b7a422c8f510a87fc79
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-13 16:27:21 +0900

    [level 2] Title: 땅따먹기, Time: 10.28 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -3,22 +3,29 @@ import java.util.*;
 class Solution {
     int solution(int[][] land) {
         int n=land.length;
+        //dp초기화
         int[][] dp=new int[n][4];
         for(int i=0;i<4;i++) dp[0][i]=land[0][i];
+
         
         for(int i=1;i<n;i++){
             for(int j=0;j<4;j++){
                 int max=0;
                 for(int k=0;k<4;k++){
                     if(j==k) continue;
-                    max=Math.max(max, dp[i-1][k]);
+                    
+                    max=Math.max(max, dp[i-1][k]); //이전열에서 j열 제외한 최댓값
                 }
                 dp[i][j]=max+land[i][j];
             }
         }
-        int answer=0;
-        for(int i=0;i<4;i++) answer=Math.max(answer, dp[n-1][i]);
+        int answer = 0;
 
+        for (int j = 0; j < 4; j++) {
+
+            answer = Math.max(answer, dp[n - 1][j]);
+
+        }
         return answer;
     }
 }
