e7d1641b899a99f57e7868f44810ee978d1755d4
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-14 18:03:36 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.35 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,48 +3,46 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = n;
-        boolean[] visited;
+        
+        
+        
         for(int i=0;i<wires.length;i++){
+            //그래프초기화
             List<List<Integer>> graph=new ArrayList<>();
-            for(int j=0;j<=n;j++){
-                graph.add(new ArrayList<>());
+            for(int j=0;j<=n;j++) graph.add(new ArrayList<>());
+            
+            boolean[] visited=new boolean[n+1];
+            for(int j=0;j<wires.length;j++){
+                if(i==j) continue; //하나씩 끊기
+                graph.get(wires[j][0]).add(wires[j][1]);
+                graph.get(wires[j][1]).add(wires[j][0]);
             }
             
-            for(int j=0;j<wires.length;j++){
-                if(i==j) continue;
-                int a=wires[j][0];
-                int b=wires[j][1];
-                
-                graph.get(a).add(b);
-                graph.get(b).add(a);
-            }
-            visited=new boolean[n+1];
-            int cnt=bfs(1,visited,graph);
-            int diff=Math.abs(n-2*cnt);
+            //두개 차이 구하기
+            int cnt=bfs(1, visited, graph );
+            int diff=Math.abs(cnt*2-n);
             answer=Math.min(diff, answer);
         }
+        
         return answer;
     }
-    static int bfs(int start, boolean[] visited, List<List<Integer>> graph){
+    int bfs(int start, boolean[] visited, List<List<Integer>> graph){
         int cnt=1;
         
         visited[start]=true;
         Queue<Integer> q=new LinkedList<>();
-        
         q.offer(start);
         
         while(!q.isEmpty()){
             int cur=q.poll();
-                for(int next: graph.get(cur)){
-                    if(!visited[next]){
-                        q.offer(next);
-                        visited[next]=true;
-                        cnt++;
-                    }
+            for(int next : graph.get(cur)){
+                if(!visited[next]){
+                    q.offer(next);
+                    visited[next]=true;
+                    cnt++;
                 }
             }
+        }
         return cnt;
-        
-
     }
 }
