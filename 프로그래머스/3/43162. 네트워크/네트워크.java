8e8ee0fe747dc0da3e961d119990ab75d4a6f10c
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-14 13:45:46 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -3,31 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited=new boolean[n];
+        boolean[] visited= new boolean[n];
         for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i,visited, computers,n);
+            if(!visited[i]) {
+                dfs(n, visited, computers, i);
                 answer++;
             }
         }
         
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i]) dfs(i,visited,computers,n);
-        }
-        
         return answer;
     }
-    
-    void dfs(int node,boolean[] visited,int[][] computers,int n){
-        visited[node]=true;
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i] && computers[node][i]==1){
-                dfs(i,visited,computers,n);
+    void dfs(int n, boolean[] visited,int[][] computers, int node){
+        if(!visited[node]){
+            visited[node]=true;
+            for(int i=0;i<n;i++){
+                if(computers[node][i]==1 && !visited[i]){
+                    dfs(n,visited, computers, i);
+                }
             }
         }
-        
         return;
     }
 }
