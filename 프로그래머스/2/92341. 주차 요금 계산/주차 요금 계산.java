72268365742e6c6e02a6c2c90aa932e25914fab9
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-14 11:07:44 +0900

    [level 2] Title: 주차 요금 계산, Time: 7.31 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,49 +1,70 @@
 import java.util.*;
 
+
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        Map<Integer,Integer> car=new HashMap<>();
-        Map<Integer,Integer> total=new HashMap<>();
-        List<Integer> list=new ArrayList<>();
+        int n=records.length;
+        int basicTime=fees[0];
+        int basic=fees[1];
+        int per=fees[2];
+        int perFee=fees[3];
         
+        Map<Integer,Integer> totalTime=new HashMap<>(); //차번호, 누적시간 
+        Map<Integer,Integer> inTime=new HashMap<>(); //차번호, 입차시간 
+            
+        //차량별 누저시간 계산
         for(String r : records){
-            String time=r.split(" ")[0];
-            int carNum=Integer.parseInt(r.split(" ")[1]);
+            String[] tmp=r.split(" ");
+            int time=toMin(tmp[0]);
+            int num=Integer.parseInt(tmp[1]);
+            String exist= tmp[2];
             
-            //출차
-            if(car.containsKey(carNum)){
-                int spend=toMinuite(time)-car.get(carNum);
-                total.put(carNum, total.getOrDefault(carNum,0)+spend);
-                car.remove(carNum);
-            } else car.put(carNum, toMinuite(time)); //입차
-            
+            if(exist.equals("IN")){ //입차
+                inTime.put(num, time);
+            } else { //출차
+                int use=time-inTime.get(num); //이용시간계산
+                totalTime.put(num, totalTime.getOrDefault(num,0)+use);
+                inTime.remove(num); //입차기록 삭제
+            }
         }
-        //출차가 안찍힌차 처리
-        for(int carNum: car.keySet()){
-            int spend=23*60+59-car.get(carNum);
-            total.put(carNum, total.getOrDefault(carNum,0)+spend);
-        }
-        
-        List<Integer> order=new ArrayList<>(total.keySet());
-        Collections.sort(order);
-        for(int c : order){
-            int time=total.get(c);
-            if(time<=fees[0]) list.add(fees[1]);
-            else{
-                int extra=time-fees[0];
-                int cost=fees[1]+(int)Math.ceil((double)extra/fees[2])*fees[3];
-                list.add(cost);
+    
+        //출차기록 없는 차 처리
+        if(!inTime.isEmpty()){
+            for(int car : inTime.keySet()){
+                int in=inTime.get(car);
+                int use=23*60+59-in;
+                totalTime.put(car, totalTime.getOrDefault(car, 0)+use );
             }
         }
         
-        int[] answer= new int[list.size()];
-        for(int i=0;i<list.size();i++) answer[i]=list.get(i);
+        Map<Integer, Integer> totalMoney=new HashMap<>();//차량별 총 요금
+        
+        //요금계산
+        for(int car : totalTime.keySet()){
+            int money=basic;
+            int used=totalTime.get(car);
+            
+            //기본시간 초과
+            if(used>basicTime){
+                int extra=used-basicTime;
+                money+=(int)Math.ceil((double)extra/per) *perFee; //올림
+            }
+            totalMoney.put(car, money);
+        }
+        
+        //차량번호 오름차순으로 요금 리턴
+        List<Integer> sorted=new ArrayList<>(totalMoney.keySet());
+        Collections.sort(sorted);
+        int idx=0;
+        int[] answer=new int[sorted.size()];
+        for(int s : sorted) answer[idx++]=totalMoney.get(s);
+        
         
         return answer;
     }
-    int toMinuite(String time){
-        int h=Integer.parseInt(time.split(":")[0]);
-        int m=Integer.parseInt(time.split(":")[1]);
+    int toMin(String s){
+        int h=Integer.parseInt(s.split(":")[0]);
+        int m=Integer.parseInt(s.split(":")[1]);
         
         return 60*h+m;
     }
