d20c87c5c97fbed74bd3d7b6b649fd496203b1d8
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-14 12:27:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 238.54 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,33 +4,43 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int n=id_list.length;
         int[] answer = new int[n];
+        Map<String, Integer> reported=new HashMap<>(); //유저별 신고당한횟수
+        Map<String,List<String>> reporter=new HashMap<>(); //유저별 신고한 유저
+        Map<String,List<String>> reporters=new HashMap<>(); //유접별 신고이력
+        //순서유지하려면 treemap
+        for(String id : id_list) reporters.put(id, new ArrayList<>());
         
-        Map<String,Set<String>> reporting=new HashMap<>();
-        for(int i=0;i<n;i++){
-            reporting.put(id_list[i],new HashSet<>());
-        }
-        Map<String,Integer> reported= new HashMap<>();
-        for(String r: report){
+        //한 유저가 여러번 신고한 경우 제거
+        Set<String> set=new HashSet<>();
+        for(String r : report) set.add(r);
+        
+        for(String r : set){
             String[] tmp=r.split(" ");
-            String from=tmp[0];
-            String to=tmp[1];
+            String a=tmp[0];
+            String b=tmp[1];
             
-            if(reporting.get(from).add(to)){
-                reported.put(to, reported.getOrDefault(to,0)+1);
-            }
+            reported.put(b,reported.getOrDefault(b,0)+1);
+            reporter.computeIfAbsent(b,j->new ArrayList<>()).add(a);
+            reporters.get(a).add(b); 
         }
         
-        for(int i=0;i<n;i++){
-            String user=id_list[i];
+        //정지 유저
+        List<String> stop=new ArrayList<>();
+        for(String p : reported.keySet()){
+            if(reported.get(p)>=k) stop.add(p);
+        }
+        
+        //발송메일 카운트
+        int idx=0;
+        for(String p : id_list){
+            List<String> list=reporters.get(p);
             int cnt=0;
-            
-            for(String target : reporting.get(user)){
-                if(reported.getOrDefault(target,0)>=k) cnt++;
+            for(String l : list){
+                if(stop.contains(l)) cnt++;
             }
-            answer[i]=cnt;
+            
+            answer[idx++]=cnt;
         }
-        
-        
         return answer;
     }
 }
