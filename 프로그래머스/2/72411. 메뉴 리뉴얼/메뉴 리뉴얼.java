724d9e6441088181d175788d0d273e3e5c539dba
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-15 12:46:00 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.32 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,46 +1,47 @@
 import java.util.*;
 
 class Solution {
-    Map<String,Integer> countMap=new HashMap<>(); //조합별, 횟수
+    Map<String,Integer> map=new HashMap<>(); //조합별 주문횟수
     public String[] solution(String[] orders, int[] course) {
-        List<String> answer=new ArrayList<>();
+        List<String> list=new ArrayList<>();
         
-        for(int c : course){ //길이만큼 조합
-            countMap.clear();
+        for(int c : course){
+            map.clear();
             
-            for(String order: orders){ //각 주문에서 만들수 있는 조합
-                char[] arr=order.toCharArray();
-                Arrays.sort(arr); //정렬먼저
-                dfs(arr,0,c,new StringBuilder());
+            for(String order : orders){
+                char[] arr= order.toCharArray();
+                Arrays.sort(arr);
+                dfs(arr, 0, c, new StringBuilder());
             }
             
-            //최대횟수조합 찾기
+            //최대횟수조합
             int max=0;
-            for(int v : countMap.values()){
-                if(v>=2) max=Math.max(max,v);
+            for(int v : map.values()){
+                if(v>=2) max=Math.max(max, v);
             }
             
-            for(Map.Entry<String,Integer> e : countMap.entrySet()){
-                if(e.getValue()==max && max>=2) answer.add(e.getKey());
+            for(Map.Entry<String,Integer> e : map.entrySet()){
+                if(e.getValue()==max && max>=2) list.add(e.getKey());
             }
         }
+        Collections.sort(list);
 
-        Collections.sort(answer);
-        
-        
-        return answer.toArray(new String[0]);
+    
+    
+        return list.toArray(new String[0]);
     }
-    void dfs(char[] arr, int idx, int targetLen, StringBuilder sb){
-        if(sb.length()==targetLen){
+    void dfs(char[] arr, int idx, int max, StringBuilder sb){
+        if(sb.length()==max){
             String key=sb.toString();
-            countMap.put(key, countMap.getOrDefault(key,0)+1);
+            map.put(key, map.getOrDefault(key,0)+1);
             return;
         }
         
-        for(int i=idx; i<arr.length;i++){ //idx부터 시작하는게 핵심
+        for(int i=idx; i<arr.length;i++){
             sb.append(arr[i]);
-            dfs(arr, i+1, targetLen, sb);
-            sb.deleteCharAt(sb.length()-1); //이게 들어가야 다른 조합도 모두 찾을 수 있음(다음 i로 넘어가서 다른선택)
+            dfs(arr, i+1, max, sb);
+            sb.deleteCharAt(sb.length()-1);
         }
+        
     }
 }
