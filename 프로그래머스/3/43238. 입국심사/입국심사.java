bab103a0da73ebbff852ffc842630e5e2b9584c2
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-15 15:00:51 +0900

    [level 3] Title: 입국심사, Time: 53.63 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,29 +2,28 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        Arrays.sort(times); //오름차순 정렬
-        
-        //가장 긴시간을 right로 지정
-        long left=0;
-        long right=times[times.length-1] * (long)n;
+        Arrays.sort(times);
 
-        //이분탐색
-        while(left<=right){
-            long mid=(left+right)/2;
-            long complete=0;
-            //mid시간동안 검사할수있는 사람 수
-            for(int i=0;i<times.length;i++){
-                complete+=mid/times[i];
+        long left = 1;
+        long right = (long) times[times.length - 1] * n;
+        long answer = right;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long complete = 0;
+
+            for (int time : times) {
+                complete += mid / time;
             }
-            
-            //모두 검사를 못했다면 시간을 늘림
-            if(complete <n) left=mid+1;
-            else { //검사했다면 시간을 줄여봄
-                right=mid-1;
-                answer=mid;
+
+            if (complete < n) {
+                left = mid + 1;
+            } else {
+                answer = mid;
+                right = mid - 1;
             }
         }
+
         return answer;
     }
 }
