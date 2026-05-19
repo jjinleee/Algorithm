2525f5c45766f53769d48a92ac502b9e36c097c0
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-19 15:34:51 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 61.94 ms, Memory: 193 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 뒤에 있는 큰 수 찾기 - 154539 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539) 
 
 ### 성능 요약
 
-메모리: 231 MB, 시간: 76.73 ms
+메모리: 193 MB, 시간: 61.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 15일 15:11:05
+2026년 05월 19일 15:34:47
 
 ### 문제 설명
 
--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -2,19 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int n = numbers.length;
+        int n= numbers.length;
         int[] answer = new int[n];
+        
         Arrays.fill(answer, -1);
-
-        Deque<Integer> st = new ArrayDeque<>(); // 인덱스 스택
-
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && numbers[st.peek()] < numbers[i]) {
-                answer[st.pop()] = numbers[i];
+        Stack<Integer> stack=new Stack<>();
+        
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && numbers[stack.peek()]<numbers[i]){
+                 answer[stack.pop()]=numbers[i];
             }
-            st.push(i);
+            stack.push(i);
         }
-
+        
+        
         return answer;
     }
 }
