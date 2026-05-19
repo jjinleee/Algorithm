be08a70696800e729806808959e65b2c2724b9e1
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-19 14:52:46 +0900

    [level 2] Title: 방문 길이, Time: 17.79 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -2,33 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(String dirs) {
-        Set<String> visited = new HashSet<>(); // 방문한 길 저장
-        int x = 0, y = 0; // 현재 위치
+        int x=0, y=0;
         
-        Map<Character, int[]> directions = new HashMap<>();
-        directions.put('U', new int[]{0, 1});
-        directions.put('D', new int[]{0, -1});
-        directions.put('R', new int[]{1, 0});
-        directions.put('L', new int[]{-1, 0});
+        Map<Character, int[]> map=new HashMap<>();
+        map.put('U',new int[]{0,1});
+        map.put('D',new int[]{0,-1});
+        map.put('R',new int[]{1,0});
+        map.put('L',new int[]{-1,0});
 
-        for (char dir : dirs.toCharArray()) {
-            int nx = x + directions.get(dir)[0];
-            int ny = y + directions.get(dir)[1];
-
-            // 경계를 벗어나지 않는 경우만 처리
-            if (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5) {
-                // 방문한 길을 양방향으로 저장
-                String path1 = x + "," + y + "->" + nx + "," + ny;
-                String path2 = nx + "," + ny + "->" + x + "," + y;
-
-                visited.add(path1);
-                visited.add(path2);
-
-                // 현재 위치 갱신
-                x = nx;
-                y = ny;
+        Set<String> set =new HashSet<>(); //from->to를 배열로 담음
+        
+        for(char c : dirs.toCharArray()){
+            int nx=x+map.get(c)[0];
+            int ny=y+map.get(c)[1];
+            
+            //범위안에 있으면 양방향으로 넣어야함
+            if(nx>=-5 && nx<=5 && ny>=-5 && ny<=5){
+                set.add(x+","+y+"->"+nx+","+ny);
+                set.add(nx+","+ny+"->"+x+","+y);
+                x=nx;
+                y=ny;
             }
+            
+            
         }
-        return visited.size() / 2; // 양방향 저장했으므로 2로 나눠줌
+        return set.size()/2;
     }
 }
