0414506f4971ec0e621a718e0af6deef28facd77
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-05-21 22:16:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 4.54 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -3,35 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
+        String tmp=Integer.toString(n,k);
         
-        // n을 k진수 문자열로 변환
-        String s = Integer.toString(n, k);
-        
-        // 0 하나 이상을 기준으로 잘라서 후보 숫자들만 뽑기
-        String[] parts = s.split("0+");
-        
-        for (String part : parts) {
-            if (part.length() == 0) continue;   // 공백 방지
-            
-            long num = Long.parseLong(part);
-            if (isPrime(num)) {
-                answer++;
+        StringBuilder sb=new StringBuilder();
+        for(char c : tmp.toCharArray()){
+            if(c=='0' && sb.length()!=0){
+                if(isPrime(sb.toString())) answer++;
+                sb.setLength(0);
+            } else{
+                sb.append(c);
             }
         }
         
+        //마지막 처리
+        if(sb.length()!=0 && isPrime(sb.toString())) answer++;
+        
         return answer;
     }
-    
-    private boolean isPrime(long n) {
-        if (n < 2) return false;
-        if (n == 2) return true;
-        if (n % 2 == 0) return false;
+    boolean isPrime(String num){
+        long n=Long.parseLong(num);
         
-        // 3부터 sqrt(n)까지 홀수만 체크
-        for (long i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) return false;
+        if(n==1) return false;
+        if(n==2 || n==3 ||n==5) return true;
+        if(n%2==0 || n%3==0) return false;
+        
+        for(long i=5;i*i<=n;i++){
+            if(n%i==0) return false;
         }
         
         return true;
     }
-}
+} 
