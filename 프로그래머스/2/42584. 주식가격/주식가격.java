ee1d936fb4a9969bd586deaae04bd7dc4c477b81
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-06-06 08:57:19 +0900

    [level 2] Title: 주식가격, Time: 8.67 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] result=new int[prices.length];
+        int n=prices.length;
+        if(n==1) return new int[]{0};
         
-        for(int i=0;i< prices.length-1;i++){
-            int time=0;
-            for(int j=i+1;j<prices.length;j++){
-                time++;
-                if(prices[i]>prices[j]){
-                    break;
-                } 
+        int[] answer = new int[n];
+        for(int i=0;i<n-1;i++){
+            int cnt=0;
+            for(int j=i+1;j<n;j++){
+                cnt++;
+                if(prices[i]>prices[j]) break;
             }
-            result[i]=time;
+            answer[i]=cnt;
+            
         }
-        return result;
+        answer[n-1]=0; //마지막 원소
+        
+        return answer;
     }
 }
