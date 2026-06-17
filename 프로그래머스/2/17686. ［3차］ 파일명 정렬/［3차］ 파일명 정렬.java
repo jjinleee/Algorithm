657f2f7df677d9ef168de6ed042c8c65b35d8c01
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-06-17 15:58:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 8.13 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,48 +1,48 @@
 import java.util.*;
 
 class Solution {
-    static class File{
+    class File{
         String head;
         int number;
-        String original;
-    
-        File(String head, int number, String original){
+        String origin;
+        
+        File(String head, int number, String origin){
             this.head=head;
             this.number=number;
-            this.original=original;
+            this.origin=origin;
         }
     }
     public String[] solution(String[] files) {
-        List<File> list= new ArrayList<>();
+        List<File> list=new ArrayList<>();
         
         for(String f : files){
             int idx=0;
             while(idx<f.length() && !Character.isDigit(f.charAt(idx))){
                 idx++;
             }
-            String h=f.substring(0,idx);
-
-            int start=idx;
-            while(idx<f.length() && Character.isDigit(f.charAt(idx)) && idx-start<5){
-                idx++;
+            String head=f.substring(0,idx);
+            int idx2=idx;
+            while(idx2<f.length() && Character.isDigit(f.charAt(idx2))){
+                idx2++;
             }
+            String number=f.substring(idx,idx2 );
             
-            int n=Integer.parseInt(f.substring(start,idx));
-
-            list.add(new File(h,n,f));
+            list.add(new File(head, Integer.parseInt(number),f));
         }
         
-        Collections.sort(list, (a, b) -> {
-            int headCompare = a.head.toLowerCase().compareTo(b.head.toLowerCase());
-            if (headCompare != 0) return headCompare;
-
-            return a.number - b.number;
-        });
-
-        String[] answer = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i).original;
+        Collections.sort(list, (a,b)-> {
+            int headc=a.head.toLowerCase().compareTo(b.head.toLowerCase());
+            if(headc!=0) return headc;
+            
+            return a.number-b.number;
+            }
+        );
+            
+        String[] answer=new String[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i]=list.get(i).origin;
         }
+        
         return answer;
     }
 }
