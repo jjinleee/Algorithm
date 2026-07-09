71420872abb0008e6db1fbaf1eb6ec96e9281d4e
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-09 13:39:34 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.75 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -4,26 +4,20 @@ class Solution {
     public int solution(int n, int[][] wires) {
         int answer = n;
         
-        
-        
         for(int i=0;i<wires.length;i++){
-            //그래프초기화
             List<List<Integer>> graph=new ArrayList<>();
             for(int j=0;j<=n;j++) graph.add(new ArrayList<>());
             
             boolean[] visited=new boolean[n+1];
             for(int j=0;j<wires.length;j++){
-                if(i==j) continue; //하나씩 끊기
+                if(i==j) continue;
                 graph.get(wires[j][0]).add(wires[j][1]);
                 graph.get(wires[j][1]).add(wires[j][0]);
             }
             
-            //두개 차이 구하기
-            int cnt=bfs(1, visited, graph );
-            int diff=Math.abs(cnt*2-n);
-            answer=Math.min(diff, answer);
+            int cnt=bfs(1,visited, graph);
+            answer=Math.min(Math.abs(2*cnt-n),answer);
         }
-        
         return answer;
     }
     int bfs(int start, boolean[] visited, List<List<Integer>> graph){
@@ -37,12 +31,13 @@ class Solution {
             int cur=q.poll();
             for(int next : graph.get(cur)){
                 if(!visited[next]){
-                    q.offer(next);
                     visited[next]=true;
+                    q.offer(next);
                     cnt++;
                 }
             }
         }
+        
         return cnt;
     }
 }
