a53993c4554aae29202d56b53bf1b43031b2256f
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-10 15:44:09 +0900

    [level 2] Title: 호텔 대실, Time: 13.43 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -6,24 +6,22 @@ class Solution {
         Arrays.sort(book_time, (a,b)->a[0].compareTo(b[0]));
         
         for(String[] b : book_time){
-            int start=totime(b[0]);
-            int end=totime(b[1]);
+            int start=toMin(b[0]);
+            int end=toMin(b[1]);
             
-            //기존방종료시각<=시작시간이면 있는 방 씀
             if(!list.isEmpty() && start>=list.get(0)){
-                list.remove(0); //기존방을 대체
-            } 
-            list.add(end+10); 
-            Collections.sort(list); //종료시각 별로 정렬
-                
+                list.remove(0);
+            }
+            list.add(end+10);
+            Collections.sort(list);
         }
         return list.size();
     }
-    int totime(String s){
-        String[] tmp=s.split(":");
+    int toMin(String a){
+        String[] tmp=a.split(":");
         int h=Integer.parseInt(tmp[0]);
         int m=Integer.parseInt(tmp[1]);
         
-        return h*60+m;
+        return 60*h+m;
     }
 }
