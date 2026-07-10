921facc3ac411144a148449fb9252abaf1bbf3a8
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-10 15:08:00 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,29 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-
-        while (storey > 0) {
-            int digit = storey % 10;
-
-            if (digit > 5) {
-                answer += 10 - digit;
-                storey += 10;
-            } else if (digit < 5) {
-                answer += digit;
-            } else {
-                int next = (storey / 10) % 10;
-
-                if (next >= 5) {
-                    answer += 10 - digit;
-                    storey += 10;
-                } else {
-                    answer += digit;
-                }
+        int cur=0;
+        
+        while(storey!=0){
+            cur=storey%10;
+            if(cur<5) answer+=cur;
+            else if(cur>5) {
+                answer+=(10-cur);
+                storey+=10; //한자릿수 올림
+            } else { //5일때 다음자릿수
+                int next=storey/10%10;
+                
+                if(next>=5) {
+                    answer+=(10-cur);
+                    storey+=10;
+                    
+                } else answer+=5;
+                
             }
-
-            storey /= 10;
+            
+            storey/=10;
         }
-
+        
         return answer;
     }
 }
