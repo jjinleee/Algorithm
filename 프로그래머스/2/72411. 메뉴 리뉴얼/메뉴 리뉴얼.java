9f8c7992fd6959ef2e2e3e4b41d008e2498b807e
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-10 16:10:06 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 13.81 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,46 +1,45 @@
 import java.util.*;
 
 class Solution {
-    Map<String,Integer> map=new HashMap<>(); //조합별 주문횟수
+    Map<String, Integer> map;
     public String[] solution(String[] orders, int[] course) {
-        List<String> list=new ArrayList<>();
+        List<String> answer=new ArrayList<>();
         
         for(int c : course){
-            map.clear();
+            map=new HashMap<>();
             
             for(String order : orders){
-                char[] arr= order.toCharArray();
+                char[] arr=order.toCharArray();
                 Arrays.sort(arr);
-                dfs(arr, 0, c, new StringBuilder());
+                
+                dfs(arr,c,0,"");
             }
             
-            //최대횟수조합
             int max=0;
-            for(int v : map.values()){
-                if(v>=2) max=Math.max(max, v);
+            for(int count : map.values()){
+                max=Math.max(max, count);
             }
             
-            for(Map.Entry<String,Integer> e : map.entrySet()){
-                if(e.getValue()==max && max>=2) list.add(e.getKey());
+            if(max<2) continue;
+            for(String key : map.keySet() ){
+                if(map.get(key)==max) answer.add(key);
             }
         }
-        Collections.sort(list);
-
-    
-    
-        return list.toArray(new String[0]);
+        
+        Collections.sort(answer);
+        
+        return answer.toArray(new String[0]);
     }
-    void dfs(char[] arr, int idx, int max, StringBuilder sb){
-        if(sb.length()==max){
-            String key=sb.toString();
-            map.put(key, map.getOrDefault(key,0)+1);
+    void dfs(char[] arr, int len, int idx, String cur){
+        if(cur.length()==len){
+            map.put(cur, map.getOrDefault(cur,0)+1);
             return;
         }
         
-        for(int i=idx; i<arr.length;i++){
-            sb.append(arr[i]);
-            dfs(arr, i+1, max, sb);
-            sb.deleteCharAt(sb.length()-1);
+        if(idx==arr.length) return;
+        
+        for(int i=idx;i<arr.length;i++){
+            dfs(arr, len, i+1, cur+arr[i]);
         }
         
     }
