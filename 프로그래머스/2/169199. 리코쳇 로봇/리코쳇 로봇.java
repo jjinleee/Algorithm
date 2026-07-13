246893b170e7c96e625788183f83636e06583a17
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-13 15:16:07 +0900

    [level 2] Title: 리코쳇 로봇, Time: 2.22 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -7,56 +7,56 @@ class Solution {
         int answer = 0;
         int n=board.length;
         int m=board[0].length();
-        char[][] b=new char[n][m];
-        int sx=-1, sy=-1;
+        int[] cur=new int[2];
         
+        char[][] map=new char[n][m];
         for(int i=0;i<n;i++){
-            String s=board[i];
             for(int j=0;j<m;j++){
-                b[i][j]=s.charAt(j);
-                if(b[i][j]=='R') {
-                    sx=i;
-                    sy=j; 
-                }//로봇현위치 저장
+                char c=board[i].charAt(j);
+                map[i][j]=c;
+                if(c=='R') {
+                    cur[0]=i;
+                    cur[1]=j;
+                }
+                
             }
         }
         
         Queue<int[]> q=new LinkedList<>();
-        boolean[][] visited= new boolean[n][m];
-        q.offer(new int[]{sx,sy,0});
-        visited[sx][sy]=true;
+        boolean[][] visited=new boolean[n][m];
+        q.offer(new int[]{cur[0],cur[1],0});
+        visited[cur[0]][cur[1]]=true;
         
         while(!q.isEmpty()){
-            int[] cur=q.poll();
-            int cx=cur[0];
-            int cy=cur[1];
-            int dist=cur[2];
+            int[] cu=q.poll();
+            int cx=cu[0];
+            int cy=cu[1];
+            int dist=cu[2];
             
-            if(b[cx][cy]=='G') return dist;
+            if(map[cx][cy]=='G') return dist;
             
             for(int i=0;i<4;i++){
                 int nx=cx;
                 int ny=cy;
                 
-                //미끄러져서 멈출때까지 이동
                 while(true){
                     int tx=nx+dx[i];
                     int ty=ny+dy[i];
+                    if(tx<0 || tx>=n || ty<0 || ty>=m) break;
+                    if(map[tx][ty]=='D') break;
                     
-                    if (tx < 0 || tx >= n || ty < 0 || ty >= m) break;
-                    if (b[tx][ty] == 'D') break;
-
-                    nx = tx;
-                    ny = ty;      
+                    nx=tx;
+                    ny=ty;
+                    
                 }
                 if(!visited[nx][ny]){
                     visited[nx][ny]=true;
                     q.offer(new int[]{nx,ny,dist+1});
                 }
-
             }
+            
+            
         }
-        
         return -1;
     }
 }
