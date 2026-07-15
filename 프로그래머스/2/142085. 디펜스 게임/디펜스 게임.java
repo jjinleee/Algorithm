039ea144e73363e5c60811026a8860c9bac3952e
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-15 16:37:49 +0900

    [level 2] Title: 디펜스 게임, Time: 73.27 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,23 +1,18 @@
 import java.util.*;
 
 class Solution {
+    PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
     public int solution(int n, int k, int[] enemy) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
-        for (int i = 0; i < enemy.length; i++) {
-            n -= enemy[i];
-            pq.add(enemy[i]);
-
-            if (n < 0) {
-                if (k > 0) {
-                    n += pq.poll();
-                    k--;
-                } else {
-                    return i;
-                }
+        for(int i=0;i<enemy.length;i++){
+            n-=enemy[i];
+            pq.offer(enemy[i]);
+            
+            if(n<0){
+                if(k==0) return i;
+                n+=pq.poll();
+                k--;
             }
         }
-
         return enemy.length;
     }
 }
