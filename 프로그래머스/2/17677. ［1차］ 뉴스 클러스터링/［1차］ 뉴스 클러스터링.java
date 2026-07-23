abdd9b02897a1e05b6134ec42bb142b92b4eac5a
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-23 22:21:39 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 12.05 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -3,40 +3,38 @@ import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-
         str1=str1.toLowerCase();
         str2=str2.toLowerCase();
         
         List<String> l1=new ArrayList<>();
         List<String> l2=new ArrayList<>();
-        Map<String,Integer> m1=new HashMap<>();
-        Map<String,Integer> m2=new HashMap<>();
+        Map<String, Integer> m1=new HashMap<>();
+        Map<String, Integer> m2=new HashMap<>();
         
         for(int i=0;i<str1.length()-1;i++){
             String tmp=str1.substring(i,i+2);
-            if(tmp.matches("[a-z]{2}")) {
-                m1.put(tmp, m1.getOrDefault(tmp,0)+1);
-                l1.add(tmp);
-            }
+            if(!tmp.matches("[a-z]{2}")) continue;
+            m1.put(tmp, m1.getOrDefault(tmp,0)+1);
+            l1.add(tmp);
         }
         for(int i=0;i<str2.length()-1;i++){
             String tmp=str2.substring(i,i+2);
-            if(tmp.matches("[a-z]{2}")) {
-                m2.put(tmp, m2.getOrDefault(tmp,0)+1);
-                l2.add(tmp);
-            }
+            if(!tmp.matches("[a-z]{2}")) continue;
+            m2.put(tmp, m2.getOrDefault(tmp,0)+1);
+            l2.add(tmp);
         }
         
-        
+        //교집합
         int com=0;
         for(String m : m1.keySet()){
             if(m2.containsKey(m)) com+=Math.min(m1.get(m),m2.get(m));
         }
+        
         int sum=l1.size()+l2.size()-com;
         
         if(com==0 && sum==0) return 65536;
+
         
-        
-        return (int) ((double)com/sum*65536);
+        return (int)((double)com/sum * 65536);
     }
 }
