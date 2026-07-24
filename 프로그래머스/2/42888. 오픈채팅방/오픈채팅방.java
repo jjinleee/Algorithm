a923f0593388d6e72267b70ce13afd5a866b9e80
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-24 15:04:22 +0900

    [level 2] Title: 오픈채팅방, Time: 68.77 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,32 +1,36 @@
 import java.util.*;
 
 class Solution {
+    
     public String[] solution(String[] record) {
-        Map<String,String> member=new HashMap<>();
+        Map<String,String> map=new HashMap<>();
         List<String[]> list=new ArrayList<>();
         
         for(String r : record){
             String[] tmp=r.split(" ");
             String cmd=tmp[0];
-            String id=tmp[1];
+            String uid=tmp[1];
             
-            if(cmd.equals("Enter") || cmd.equals("Change")){
-                String name=tmp[2];
-
-                member.put(id,name);
+            if(cmd.equals("Leave")){
+                list.add(new String[]{"Leave",uid});
+                continue;
+            }
+            String name=tmp[2];
+            
+            if(cmd.equals("Enter")){
+                list.add(new String[]{"Enter",uid});
             } 
-            if(!cmd.equals("Change")) list.add(new String[]{id,cmd});
+            map.put(uid, name);
         }
         
-        int i=0;
         String[] answer=new String[list.size()];
-        for(String[] l : list){
-            String id=l[0];
-            String cmd=l[1];
-            
-            if(cmd.equals("Enter")) answer[i++]=member.get(id)+"님이 들어왔습니다.";
-            else if(cmd.equals("Leave")) answer[i++]=member.get(id)+"님이 나갔습니다.";
+        for(int i=0;i<list.size();i++){
+            String[] tmp=list.get(i);
+            String user=map.get(tmp[1]);
+            if(tmp[0].equals("Enter")) answer[i]=user+"님이 들어왔습니다.";
+            else answer[i]=user+"님이 나갔습니다.";
         }
+        
         return answer;
     }
 }
