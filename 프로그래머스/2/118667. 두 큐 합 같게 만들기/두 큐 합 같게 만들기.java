73e7b793513c5595030642420071688923e539d1
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-25 14:19:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 108.81 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -2,42 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        long sum=0;
-        long currentSum=0;
-        int answer=0;
+        int answer = 0;
+        int len=queue1.length;
+        long sum1=0;
+        long sum2=0;
         
-        for(int q : queue1) {
-            sum+=q;
-            currentSum+=q;
+        Queue<Integer> q1=new LinkedList<>();
+        Queue<Integer> q2=new LinkedList<>();
+        
+        for(int q : queue1){
+            sum1+=q;
+            q1.offer(q);
         }
-        for(int q : queue2) sum+=q;
-
-        if(sum%2==1) return -1; //홀수면 불가능
-        long target=sum/2;
+        for(int q : queue2){
+            sum2+=q;
+            q2.offer(q);
+        }
+        if((sum1+sum2)%2==1) return -1; //같게만들수없음
+        long sum=(sum1+sum2)/2;
         
-        int n=queue1.length;
-        int[] arr=new int[2*n];
-        for(int i=0;i<n;i++) arr[i]=queue1[i];
-        for(int i=0;i<n;i++) arr[n+i]=queue2[i];
-        
-        int left=0;
-        int right=queue1.length-1;
-        while(left<2*n && right<2*n){
-            if(currentSum==target) return answer;
-            
-            if(currentSum<target) {
-                if(right+1 >=2*n) break;
-                right++;
-                currentSum+=arr[right];
-                answer++;
+        while(sum1!=sum){
+            int n=0;
+            if(sum1>sum2){
+                n=q1.poll();
+                sum1-=n;
+                sum2+=n;
+                q2.offer(n); 
+            } else {
+                n=q2.poll();
+                sum2-=n;
+                sum1+=n;
+                q1.offer(n);
             }
-            else {
-                currentSum-=arr[left];
-                left++;
-                answer++;
-            }
+            answer++;  
+           
+            if(answer>3*len) return -1; 
         }
         
-        return -1;
+        return answer;
     }
 }
