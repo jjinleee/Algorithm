4a9368c94da1db6626f8b07844b19b8605f953b5
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-25 15:51:01 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.95 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,52 +1,53 @@
 import java.util.*;
 
 class Solution {
+    class Song{
+        String title;
+        int dur;
+        Song(String title, int dur){
+            this.title=title;
+            this.dur=dur;
+        }
+    }
     public String solution(String m, String[] musicinfos) {
-        String answer = "";
-        int maxRun=0;
-        m=convert(m);
-        
-        for(String music  : musicinfos){
+        String find=trans(m);
+        List<Song> list=new ArrayList<>();
+
+        for(String music : musicinfos){
             String[] tmp=music.split(",");
-            int start=toMin(tmp[0]);
-            int end=toMin(tmp[1]);
-            
-            int run=end-start;//재생시간
+            int dur=toTime(tmp[1])-toTime(tmp[0]);
             String title=tmp[2];
-            String melody=tmp[3];
+            String melody=trans(tmp[3]);
+            String total="";
             
-            melody=convert(melody);
-            
-            int plus=run/melody.length()+1;
-            
-            StringBuilder sb=new StringBuilder();
-            for(int i=0;i<plus;i++) sb.append(melody);
-            
-            String newMelody=sb.toString().substring(0,run);
-            
-            if(newMelody.contains(m)){
-                if(maxRun<run) {
-                    maxRun=run;
-                    answer=title;
-                }
+            if(melody.length()>=dur) {
+                total=melody.substring(0,dur);
+            }
+            else{
+                int rep=dur/melody.length();
+                int plus=dur%melody.length();
+                total=melody.repeat(rep)+melody.substring(0,plus);
             }
             
-             
+            if(total.contains(find)) {
+                list.add(new Song(title, dur));
+            }
+            
         }
-        return answer.isEmpty() ? "(None)" : answer;
-    }
-    int toMin(String s){
-        String[] tmp=s.split(":");
-        int h=Integer.parseInt(tmp[0]);
-        int m=Integer.parseInt(tmp[1]);
+        list.sort((a,b)-> b.dur-a.dur);
         
-        return 60*h+m;
+        
+        return list.isEmpty() ? "(None)" : list.get(0).title ;
     }
-    String convert(String origin){
-        return origin.replace("C#","1")
-            .replace("D#","2")
-            .replace("F#","3")
-            .replace("G#","4")
-            .replace("A#","5");
+    int toTime(String time){
+        String[] tmp=time.split(":");
+        return Integer.parseInt(tmp[0])*60+Integer.parseInt(tmp[1]);
+    }
+    String trans(String s){
+        return s.replaceAll("C#","H")
+                .replaceAll("D#","I")
+                .replaceAll("F#","J")
+                .replaceAll("G#","K")
+                .replaceAll("A#","L");
     }
 }
