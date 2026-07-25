47b83d63e69b76a5e4e4216ff9c2df3e4f884460
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-25 18:13:46 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 44.00 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -3,58 +3,55 @@ import java.util.*;
 class Solution {
     int maxCount=0;
     int maxSum=0;
-    int[] sales={10,20,30,40};
-    int[][] gUsers;
-    int[] gEmoticons;
-    
+    int[] discount={10,20,30,40};
+    int[][] gusers;
+    int[] gemoticons;
     public int[] solution(int[][] users, int[] emoticons) {
-        int[] applied=new int[emoticons.length];
-        this.gUsers=users;
-        this.gEmoticons=emoticons;
-        
-        dfs(0,applied);
+        this.gusers=users;
+        this.gemoticons=emoticons;
+    
+        int[] selected=new int[emoticons.length];
+        dfs(0,selected);
         
         return new int[]{maxCount, maxSum};
     }
-    void dfs(int idx, int[] applied){
-        if(idx==gEmoticons.length){
-            cal(applied);
+    void dfs(int idx, int[] selected ){
+        if(idx==gemoticons.length){
+            //사용자구매결과 계산
+            cal(selected);
             return;
         }
-        
-        for(int s : sales){
-            applied[idx]=s;
-            dfs(idx+1, applied);
+        for(int rate : discount){
+            selected[idx]=rate;
+            dfs(idx+1, selected);
         }
-        
     }
-    void cal(int[] applied){
-        int cnt=0;
+    void cal(int[] selected){
+        int[] money=new int[gusers.length];
+        int join=0;
+        
+        for(int i=0;i<gusers.length;i++){
+            int ratio=gusers[i][0];
+            int limit=gusers[i][1];
+            
+            for(int j=0;j<selected.length;j++){
+                if(selected[j]>=ratio) money[i]+=(gemoticons[j]*(100-selected[j])/100);
+            }
+            if(money[i]>=limit){
+                join++;
+                money[i]=0;
+            }
+        }
+        
         int sum=0;
+        for(int m : money) sum+=m; //총판매액
         
-        int m=applied.length;
-        for(int[] u : gUsers){
-            int total=0;
-            for(int i=0;i<m;i++){
-                if(applied[i]>=u[0]){ //임티삼
-                    total+=gEmoticons[i]*(100-applied[i])/100;
-                }
-            }
-            if(total>=u[1]){ //임플가입
-                cnt+=1;
-            } else {
-                sum+=total; //총합금액
-            }
-        }
-        
-        //우선순위 지켜서 갱신
-        if(maxCount<cnt){
-            maxCount=cnt;
+        if(maxCount<join){
+            maxCount=join;
             maxSum=sum;
-        } else if(cnt==maxCount && maxSum<sum){
+        } else if(maxCount==join && maxSum<sum){
+            maxCount=join;
             maxSum=sum;
-        }
-        
-        return;
+        } 
     }
 }
