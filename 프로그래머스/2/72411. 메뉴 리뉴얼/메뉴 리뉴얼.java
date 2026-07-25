ef49df5c23996606d794d7ea5344636e0ca2c1fd
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-25 14:38:25 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 15.68 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,47 +1,41 @@
 import java.util.*;
 
 class Solution {
-    Map<String,Integer> map;
+    Map<String, Integer> map;
     public String[] solution(String[] orders, int[] course) {
-        List<String> answer=new ArrayList<>();
+        List<String> list= new ArrayList<>();
         
         for(int c : course){
             map=new HashMap<>();
-            
-            for(String order :orders){
-                char[] tmp=order.toCharArray();
-                Arrays.sort(tmp);
-                dfs(c, new String(tmp), "", 0);
+            for(String order : orders){
+                char[] sort=order.toCharArray();
+                Arrays.sort(sort);
+                order=String.valueOf(sort);
+                dfs(order, c, "",0);
             }
             
             int max=0;
-            for(String m : map.keySet()){
-                if(max<map.get(m)) max=map.get(m);
-            }
+            for(String m : map.keySet()) max=Math.max(max, map.get(m));
             
-            if(max<2) break; //최소2명이상 손님으로부터 주문되어야함
-            
+            if(max<2) continue; //최소 2번이상 주문돼야함
             for(String m : map.keySet()){
-                if(map.get(m)==max){
-                    answer.add(m);
-                }
+                if(map.get(m)==max) list.add(m);
             }
         }
         
-        Collections.sort(answer);
+        list.sort((a,b)-> a.compareTo(b)); //사전순정렬
         
-        return answer.toArray(new String[0]);
+        return list.toArray(new String[0]);
     }
-    void dfs(int max, String order, String cur, int idx){
-        if(max==cur.length()){
+    void dfs(String order, int len, String cur, int idx){
+        if(cur.length()==len){
             map.put(cur, map.getOrDefault(cur,0)+1);
             return;
         }
-        if(idx==order.length()) return;
+        if(idx>=order.length()) return;
         
         for(int i=idx;i<order.length();i++){
-           dfs(max, order, cur+order.charAt(i),i+1);
+            dfs(order, len, cur+order.charAt(i), i+1);
         }
-        
     }
 }
