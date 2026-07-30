d8d65b961fb7c6920152425be6b589c21fece78f
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-30 17:52:57 +0900

    [level 2] Title: 더 맵게, Time: 647.54 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,18 +4,16 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int s : scoville){
-            pq.add(s);
-        }
-        if(pq.peek()>=K) return 0;
+        for(int s : scoville) pq.add(s);
         
         while(pq.size()>=2 && pq.peek()<K){
-            pq.add(pq.poll()+2*pq.poll());
+            int newfood=pq.poll()+pq.poll()*2;
+            pq.offer(newfood);
             answer++;
-            
-            if(pq.peek()>=K) return answer;
         }
         
-        return -1;
+        if(pq.peek()<K) return -1;
+        
+        return answer;
     }
 }
