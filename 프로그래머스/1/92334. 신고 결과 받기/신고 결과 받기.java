9bf28c213859d05fff8dde581eb2efc7e7ae8c24
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-30 17:42:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 119.26 ms, Memory: 169 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,46 +1,37 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int n=id_list.length;
-        int[] answer = new int[n];
-        Map<String, Integer> reported=new HashMap<>(); //유저별 신고당한횟수
-        Map<String,List<String>> reporter=new HashMap<>(); //유저별 신고한 유저
-        Map<String,List<String>> reporters=new HashMap<>(); //유접별 신고이력
-        //순서유지하려면 treemap
-        for(String id : id_list) reporters.put(id, new ArrayList<>());
-        
-        //한 유저가 여러번 신고한 경우 제거
+    public int[] solution(String[] id_list, String[] report, int k) {        
+        //중복신고제거
         Set<String> set=new HashSet<>();
-        for(String r : report) set.add(r);
+        for(String r : report){
+            set.add(r);
+        }
+        Map<String,Integer> reported=new HashMap<>(); //신고당한사람, 신고횟수
+        Map<String, List<String>> reporter=new HashMap<>(); //신고당한사람, 신고한 사람
+        Map<String,Integer> mail=new HashMap<>(); //유저, 메일횟수
+        for(String id : id_list) mail.put(id, 0);
         
-        for(String r : set){
-            String[] tmp=r.split(" ");
-            String a=tmp[0];
-            String b=tmp[1];
-            
-            reported.put(b,reported.getOrDefault(b,0)+1);
-            reporter.computeIfAbsent(b,j->new ArrayList<>()).add(a);
-            reporters.get(a).add(b); 
+        for(String s : set){
+            String[] tmp=s.split(" ");
+            reported.put(tmp[1],reported.getOrDefault(tmp[1],0)+1); //신고횟수증가
+            reporter.computeIfAbsent(tmp[1], r->new ArrayList<>()).add(tmp[0]);
         }
         
-        //정지 유저
-        List<String> stop=new ArrayList<>();
-        for(String p : reported.keySet()){
-            if(reported.get(p)>=k) stop.add(p);
-        }
-        
-        //발송메일 카운트
-        int idx=0;
-        for(String p : id_list){
-            List<String> list=reporters.get(p);
-            int cnt=0;
-            for(String l : list){
-                if(stop.contains(l)) cnt++;
+        for(String stop : reported.keySet()){
+            if(reported.get(stop)>=k){
+                List<String> tomail=reporter.get(stop); //정지당한 유저 신고한 유저
+                for(String m : tomail){ //메일 보냄
+                    mail.put(m, mail.get(m)+1);
+                }
             }
-            
-            answer[idx++]=cnt;
         }
+        
+        int[] answer= new int[id_list.length];
+        for(int i=0;i<id_list.length;i++){
+            answer[i]=mail.get(id_list[i]);
+        }
+        
         return answer;
     }
 }
