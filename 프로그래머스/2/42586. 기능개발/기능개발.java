1868a03f25fb066baa131a28f51b6385f7d5c305
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-30 19:01:49 +0900

    [level 2] Title: 기능개발, Time: 7.34 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,24 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> list=new ArrayList<>();
         int n=progresses.length;
-        int[] day=new int[n];
+        
+        //남은 작업일
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            day[i]=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
+            int rest=(int)Math.ceil((100.0-progresses[i])/speeds[i]);
+            q.offer(rest);
         }
         
-        List<Integer> list=new ArrayList<>();
-        Deque<Integer> q=new ArrayDeque<>();
-        for(int d : day){
-            if(!q.isEmpty() && q.peekFirst()<d){
-                list.add(q.size());
-                q.clear();
-                q.offer(d);
-            } else{
-                q.offerLast(d);
+        int finish=0;
+        int cnt=1;
+        while(!q.isEmpty()){
+            int first=q.poll();
+            while(!q.isEmpty() && first>=q.peek()){
+                q.poll();
+                cnt++;
             }
+            list.add(cnt);
+            cnt=1;
         }
-        list.add(q.size());
         
         return list.stream().mapToInt(i->i).toArray();
     }
