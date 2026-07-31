5abf38dc9f1c474cea5828da5067b0783facacdf
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-07-31 17:02:34 +0900

    [level 2] Title: 피로도, Time: 1.57 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,20 +1,22 @@
 import java.util.*;
 
 class Solution {
-    static int max=0;
-    public int solution(int k, int[][] dungeons) {        
-        boolean[] visited=new boolean[dungeons.length];
-        dfs(dungeons, 0, visited, k);
-        return max;
+    int answer=0;
+    public int solution(int k, int[][] dungeons) {
+        int n=dungeons.length;
+        boolean[] visited=new boolean[n];
+        
+        dfs(k,0,dungeons, visited);
+        
+        return answer;
     }
-    static void dfs(int[][] dungeons, int cnt, boolean[] visited, int cur){
-
-        max=Math.max(max,cnt);
+    void dfs(int k, int cnt, int[][] dungeons, boolean[] visited){
+        answer=Math.max(answer, cnt);
         
         for(int i=0;i<dungeons.length;i++){
-            if(!visited[i] && cur>=dungeons[i][0]){
+            if(!visited[i] && k>=dungeons[i][0]){
                 visited[i]=true;
-                dfs(dungeons, cnt+1, visited, cur-dungeons[i][1]);
+                dfs(k-dungeons[i][1], cnt+1, dungeons, visited);
                 visited[i]=false;
             }
         }
