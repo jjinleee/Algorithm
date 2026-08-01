f29a71f7904d0588a14d89f115f37f80259c540c
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-01 15:50:52 +0900

    [level 2] Title: 오픈채팅방, Time: 142.11 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,36 +1,38 @@
 import java.util.*;
 
 class Solution {
-    
     public String[] solution(String[] record) {
-        Map<String,String> map=new HashMap<>();
+        Map<String,String> user=new HashMap<>();
         List<String[]> list=new ArrayList<>();
-        
         for(String r : record){
             String[] tmp=r.split(" ");
             String cmd=tmp[0];
-            String uid=tmp[1];
+            String id=tmp[1];
+            String name="";
             
-            if(cmd.equals("Leave")){
-                list.add(new String[]{"Leave",uid});
-                continue;
+            if(tmp.length>2) {
+                name=tmp[2];
             }
-            String name=tmp[2];
             
             if(cmd.equals("Enter")){
-                list.add(new String[]{"Enter",uid});
-            } 
-            map.put(uid, name);
+                user.put(id,name);
+                list.add(new String[]{cmd,id});
+            } else if(cmd.equals("Leave")){
+                list.add(new String[]{cmd,id});
+            } else user.put(id,name);
         }
         
-        String[] answer=new String[list.size()];
-        for(int i=0;i<list.size();i++){
-            String[] tmp=list.get(i);
-            String user=map.get(tmp[1]);
-            if(tmp[0].equals("Enter")) answer[i]=user+"님이 들어왔습니다.";
-            else answer[i]=user+"님이 나갔습니다.";
+        int n=list.size();
+        String[] answer=new String[n];
+        for(int i=0;i<n;i++){
+            String name=user.get(list.get(i)[1]);
+            String cmd=list.get(i)[0];
+            if(cmd.equals("Enter")){
+                answer[i]=name+"님이 들어왔습니다.";
+            } else {
+                answer[i]=name+"님이 나갔습니다.";
+            }
         }
-        
         return answer;
     }
 }
