faf49852b4408ea0b9a6c17182cea1d9a0b23785
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-02 13:40:38 +0900

    [level 2] Title: 호텔 대실, Time: 5.67 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,23 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        List<Integer> list=new ArrayList<>();
-        Arrays.sort(book_time, (a,b)->a[0].compareTo(b[0]));
-        
+        int answer = 0;
+        Arrays.sort(book_time, (a,b)-> a[0]==b[0] ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
+        PriorityQueue<Integer> q= new PriorityQueue<>();
         for(String[] b : book_time){
-            int start=toMin(b[0]);
-            int end=toMin(b[1]);
+            int start=toTime(b[0]);
+            int end=toTime(b[1])+10;
             
-            if(!list.isEmpty() && start>=list.get(0)){
-                list.remove(0);
-            }
-            list.add(end+10);
-            Collections.sort(list);
+            if(!q.isEmpty() && q.peek()<=start){
+                q.poll();
+                q.offer(end);
+            } else {
+                q.offer(end);
+                answer++;
+            }  
         }
-        return list.size();
+        return answer;
     }
-    int toMin(String a){
-        String[] tmp=a.split(":");
+    int toTime(String t){
+        String[] tmp=t.split(":");
         int h=Integer.parseInt(tmp[0]);
         int m=Integer.parseInt(tmp[1]);
         
