796304744a4a54b4511675289710ef47fc270650
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-15 20:54:04 +0900

    [level 2] Title: 미로 탈출, Time: 3.87 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -3,63 +3,72 @@ import java.util.*;
 class Solution {
     int[] dx={-1,1,0,0};
     int[] dy={0,0,-1,1};
-    int n,m;
-    int sx,sy,lx,ly,ex,ey;
     char[][] map;
+    int m,n;
     public int solution(String[] maps) {
-        int sum = 0;
-        n=maps.length;
-        m=maps[0].length();
-        map=new char[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                map[i][j]=maps[i].charAt(j);
-                if(map[i][j]=='S'){
-                    sx=i; sy=j;
-                } else if(map[i][j]=='E'){
-                    ex=i; ey=j;
-                } else if(map[i][j]=='L'){
-                    lx=i; ly=j;
+        m=maps.length;
+        n=maps[0].length();
+        map=new char[m][n];
+        
+        int sx=0; 
+        int sy=0;
+        int ex=0;
+        int ey=0;;
+        int lx=0;
+        int ly=0;
+        
+        for(int i=0;i<m;i++){
+            String s=maps[i];
+            for(int j=0;j<n;j++){
+                char c=s.charAt(j);
+                if(c=='S'){
+                   sx=i;
+                    sy=j;
+                } else if(c=='E'){
+                    ex=i;
+                    ey=j;
+                } else if(c=='L'){
+                    lx=i;
+                    ly=j;
                 }
+                
+                map[i][j]=c;
             }
         }
         
-        int toLever=bfs(sx,sy,lx,ly);
-        if(toLever==-1) return -1;
+        int s1=bfs(sx,sy,lx,ly);
+        int s2=bfs(lx,ly,ex,ey);
         
-        int toExit=bfs(lx,ly,ex,ey);
-        if(toExit==-1) return -1;
-        
-        return toLever+toExit;
+        if(s1==-1 || s2==-1) return -1;
+            
+        return s1+s2;
     }
-    int bfs(int sx, int sy, int ex, int ey){
-        int sum=0;
-        
-        boolean[][] visited=new boolean[n][m];
+    
+    int bfs(int startX, int startY, int distX, int distY){
+        boolean[][] visited=new boolean[m][n];
         Queue<int[]> q= new LinkedList<>();
-        q.offer(new int[]{sx,sy,0});
-        visited[sx][sy]=true;
+        visited[startX][startY]=true;
+        q.offer(new int[]{startX,startY,0});
         
         while(!q.isEmpty()){
             int[] cur=q.poll();
             int cx=cur[0];
             int cy=cur[1];
-            int distance=cur[2];
+            int dist=cur[2];
             
-            if(cx==ex && cy==ey){
-                return distance;
-            }
+            if(cx==distX && cy==distY) return dist;
             
             for(int i=0;i<4;i++){
                 int nx=cx+dx[i];
                 int ny=cy+dy[i];
                 
-                if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && map[nx][ny]!='X'){
+                if(nx>=0 && nx<m && ny>=0 && ny<n && !visited[nx][ny] && map[nx][ny]!='X'){
                     visited[nx][ny]=true;
-                    q.offer(new int[]{nx,ny,distance+1});
+                    q.offer(new int[]{nx,ny,dist+1});
                 }
             }
         }
+        
         
         return -1;
     }
