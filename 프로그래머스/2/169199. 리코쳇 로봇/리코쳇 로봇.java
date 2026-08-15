81dee44862e7ca2cc777d11acd9244bb1fefd5fe
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-15 20:27:29 +0900

    [level 2] Title: 리코쳇 로봇, Time: 1.76 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,60 +1,58 @@
 import java.util.*;
 
 class Solution {
-    int[] dx={-1,1,0,0};
-    int[] dy={0,0,-1,1};
     public int solution(String[] board) {
-        int answer = 0;
-        int n=board.length;
-        int m=board[0].length();
-        int[] cur=new int[2];
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         
-        char[][] map=new char[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int m=board.length;
+        int n=board[0].length();
+        char[][] map=new char[m][n];
+        
+        int x=0;
+        int y=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 char c=board[i].charAt(j);
-                map[i][j]=c;
-                if(c=='R') {
-                    cur[0]=i;
-                    cur[1]=j;
+                if(c=='R'){
+                    x=i;
+                    y=j;
                 }
-                
+                map[i][j]=c;
             }
         }
         
-        Queue<int[]> q=new LinkedList<>();
-        boolean[][] visited=new boolean[n][m];
-        q.offer(new int[]{cur[0],cur[1],0});
-        visited[cur[0]][cur[1]]=true;
+        boolean[][] visited= new boolean[m][n];
+        Queue<int[]> q= new LinkedList<>();
+        q.offer(new int[]{x,y,0});
+        visited[x][y]=true;
         
         while(!q.isEmpty()){
-            int[] cu=q.poll();
-            int cx=cu[0];
-            int cy=cu[1];
-            int dist=cu[2];
+            int[] cur=q.poll();
+            int cx=cur[0];
+            int cy=cur[1];
+            int dist=cur[2];
             
             if(map[cx][cy]=='G') return dist;
             
             for(int i=0;i<4;i++){
                 int nx=cx;
                 int ny=cy;
-                
                 while(true){
                     int tx=nx+dx[i];
                     int ty=ny+dy[i];
-                    if(tx<0 || tx>=n || ty<0 || ty>=m) break;
+
+                    if(tx<0 || tx>=m || ty<0 || ty>=n) break;
                     if(map[tx][ty]=='D') break;
-                    
+
                     nx=tx;
-                    ny=ty;
-                    
+                    ny=ty;  
                 }
                 if(!visited[nx][ny]){
                     visited[nx][ny]=true;
                     q.offer(new int[]{nx,ny,dist+1});
                 }
-            }
-            
+            }  
             
         }
         return -1;
