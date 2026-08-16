9bd28f884fa647feaeb548b7dddd6664ea3b3e55
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-16 16:10:07 +0900

    [level 1] Title: 공원 산책, Time: 0.27 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -1,48 +1,75 @@
 import java.util.*;
 
 class Solution {
+    char[][] map;
+    int h,w;
     public int[] solution(String[] park, String[] routes) {
-        int x=0, y=0;
-        int n=park.length;
-        int m=park[0].length();
+        h=park.length;
+        w=park[0].length();
+        map=new char[h][w];
         
-        char[][] map=new char[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                map[i][j]=park[i].charAt(j);
-                if(map[i][j]=='S'){
+        int x=0;
+        int y=0;
+        
+        for(int i=0;i<h;i++){
+            for(int j=0;j<w;j++){
+                char c=park[i].charAt(j);
+                map[i][j]=c;
+                if(c=='S'){
                     x=i;
                     y=j;
                 }
+                
             }
         }
         
-        for(String r  : routes){
+        for(String r : routes){
             String[] tmp=r.split(" ");
             char dir=tmp[0].charAt(0);
-            int t=Integer.parseInt(tmp[1]);
+            int n=Integer.parseInt(tmp[1]);
             
-            int nx=x;
-            int ny=y;
-            boolean canMove=true;
-            
-            for(int i=0;i<t;i++){
-              if(dir=='N') nx--; 
-                else if(dir=='S') nx++;
-                else if(dir=='E') ny++;
-                else ny--;
-                
-                if(nx<0 || nx>=n || ny<0 || ny>=m || map[nx][ny]=='X'){
-                    canMove=false;
-                    break;
-                }
-            }
-            if(canMove){
-                x=nx;
-                y=ny;
+            if(dir=='S'){ //남
+               if(x+n<h && isObstacle(x,y,n,dir)){
+                  x+=n;
+               } 
+            } else if(dir=='N'){ //북
+               if(x-n>=0  &&  isObstacle(x,y,n,dir)){
+                  x-=n;
+               }         
+            } else if(dir=='W'){ //서
+               if( y-n>=0 &&  isObstacle(x,y,n,dir)){
+                  y-=n;
+               } else continue;                
+            } else { //동
+               if(y+n<w &&  isObstacle(x,y,n,dir)){
+                  y+=n;
+               } else continue;                
             }
         }
         
+        
         return new int[]{x,y};
+    }
+    boolean isObstacle(int x,int y, int n, char dir){
+        if(dir=='E'){
+            for(int i=1;i<=n;i++){
+                if(map[x][y+i]=='X') return false;
+            }
+            
+        }else if(dir=='W'){
+            for(int i=1;i<=n;i++){
+                if(map[x][y-i]=='X') return false;
+            }            
+        }else if(dir=='N'){
+            for(int i=1;i<=n;i++){
+                if(map[x-i][y]=='X') return false;
+            }            
+        }else {
+            for(int i=1;i<=n;i++){
+                if(map[x+i][y]=='X') return false;
+            }            
+        }
+        
+        return true;
     }
 }
