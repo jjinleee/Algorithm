fde9807896e90fcd66c74c6d4f0be65ac38d0d87
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-16 16:35:22 +0900

    [level 2] Title: 무인도 여행, Time: 6.42 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -3,43 +3,46 @@ import java.util.*;
 class Solution {
     int[] dx={-1,1,0,0};
     int[] dy={0,0,-1,1};
-    char[][] map;
-    int n,m;
     List<Integer> list=new ArrayList<>();
+    char[][] map;
+    boolean[][] visited;
+    int m,n;
     public int[] solution(String[] maps) {
-        n=maps.length;
-        m=maps[0].length();
+        m=maps.length;
+        n=maps[0].length();
         
-        int idx=0;
-        map=new char[n][m];
-        for(String m : maps){
-            map[idx++]=m.toCharArray();
+        map=new char[m][n];
+        visited=new boolean[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                map[i][j]=maps[i].charAt(j);
+            }
         }
-        boolean[][] visited=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(!visited[i][j] && map[i][j]!='X'){
-                    list.add(dfs(visited, i,j));
+                    list.add(dfs(i,j));
                 }
             }
         }
-        list.sort(Comparator.naturalOrder());
+        
         if(list.isEmpty()) return new int[]{-1};
+        Collections.sort(list);
         
         return list.stream().mapToInt(i->i).toArray();
     }
-    int dfs(boolean[][] visited, int x, int y){        
+    int dfs(int x, int y){
         visited[x][y]=true;
-        
-        int sum=map[x][y]-'0';
+        int sum=(map[x][y]-'0');
         
         for(int i=0;i<4;i++){
             int nx=x+dx[i];
             int ny=y+dy[i];
             
-            if(nx>=0 && nx<n && ny>=0 && ny<m && !visited[nx][ny] && map[nx][ny]!='X'){
-                visited[nx][ny]=true;
-                sum+=dfs(visited, nx, ny);
+            if(nx>=0 && nx<m && ny>=0 && ny<n &&!visited[nx][ny]&& map[nx][ny]!='X'){
+                sum+=dfs(nx,ny);
             }
         }
         
