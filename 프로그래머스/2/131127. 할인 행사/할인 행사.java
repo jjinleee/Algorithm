eca4e06db44ce437c577b9bb9ab60a8e445c777c
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-22 15:48:19 +0900

    [level 2] Title: 할인 행사, Time: 37.08 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,20 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String,Integer> stuff= new HashMap<>();
-        for(int i=0;i<want.length;i++){
-            stuff.put(want[i],number[i]);
+        int n=discount.length;
+        Map<String,Integer> wants=new HashMap<>();
+        for(int i=0;i<want.length;i++) wants.put(want[i],number[i]);
+        
+        
+        Map<String,Integer> got=new HashMap<>();
+        for(int start=0;start<=n-10;start++){
+            got=new HashMap<>();
+            for(int i=start;i<start+10;i++){
+                got.put(discount[i],got.getOrDefault(discount[i],0)+1);
+            }
+            
+            boolean isPossible=true;
+            for(String w : wants.keySet()){
+                if(wants.get(w)!=got.get(w)){
+                    isPossible=false;
+                    break;
+                } 
+            }
+            
+            if(isPossible) answer++;
         }
         
-        for(int i=0;i<discount.length-9;i++){
-            HashMap<String,Integer> market=new HashMap<>();
-            for(int j=i;j<i+10;j++){
-               if(stuff.containsKey(discount[i])){
-                   market.put(discount[j],market.getOrDefault(discount[j],0)+1);
-               }
-            }
-            if(market.equals(stuff)) answer++;
-        }
         return answer;
     }
 }
