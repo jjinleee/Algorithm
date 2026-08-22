98467f0f7de7d3172cf8d6cc1281dc1416f88f28
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-08-22 13:36:45 +0900

    [level 2] Title: 후보키, Time: 2.80 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/README.md
+++ 프로그래머스/2/42890. 후보키/README.md
@@ -1,10 +1,10 @@
 # [level 2] 후보키 - 42890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42890) 
 
 ### 성능 요약
 
-메모리: 77.1 MB, 시간: 3.21 ms
+메모리: 81.5 MB, 시간: 2.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 15일 17:58:45
+2026년 08월 22일 13:36:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -2,51 +2,43 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] relation) {
-        //행열개수
-        int rowCount = relation.length;
-        int colCount = relation[0].length;
+        int answer = 0;
+        int row=relation.length;
+        int col=relation[0].length;
         
-        //후보키-비트마스킹
-        List<Integer> candidateKeys = new ArrayList<>();
-
-        //모든 열조합 만듦
-        for (int combination = 1; combination < (1 << colCount); combination++) {
-            boolean isMinimal = true; //최소성 검사
-
-            //후보키와 현재조합 최소성 검사
-            for (int candidateKey : candidateKeys) {
-                if ((candidateKey & combination) == candidateKey) { //현재조합에 후보키가 포함되면 최소성 탈락
-                    isMinimal = false;
+        List<Integer> candidates=new ArrayList<>();
+        
+        for(int bit=1;bit<(1<<col) ; bit++){
+            boolean minimal=true;
+            
+            for(int key : candidates){
+                if((key&bit)==key){
+                    minimal=false;
                     break;
                 }
             }
+            
+            if(!minimal) continue;
+            Set<String> set = new HashSet<>();
 
-            if (!isMinimal) continue;
+            for (int i = 0; i < row; i++) {
+                StringBuilder sb = new StringBuilder();
 
-            //유일성 검사
-            Set<String> tuples = new HashSet<>(); //행별 조합 저장
+                for (int j = 0; j < col; j++) {
 
-            for (int row = 0; row < rowCount; row++) {
-                StringBuilder key = new StringBuilder();
-
-                //현재조합이 각 열에 포함되는지 확인
-                for (int col = 0; col < colCount; col++) {
-                    if ((combination & (1 << col)) != 0) {
-                        //선택된 열의 값 이어붙이기
-                        key.append(relation[row][col]).append("|");
+                    // 현재 조합 bit에 j번 컬럼이 포함되어 있는지 확인
+                    if ((bit & (1 << j)) != 0) {
+                        sb.append(relation[i][j])
+                          .append("/");
                     }
                 }
 
-                //현재 행 조합값 추가
-                tuples.add(key.toString());
+                set.add(sb.toString());
             }
 
-            //모든 행이 현재 열조합으로 구분된다는 뜻 즉 유일성, 최소성 만족
-            if (tuples.size() == rowCount) {
-                candidateKeys.add(combination);
-            }
+            
+            if(set.size()==row) candidates.add(bit);
         }
-
-        return candidateKeys.size();
+        return candidates.size();
     }
 }
