953608aa410fddbcfdb7532ce7788989d2c73372
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-07 23:54:08 +0900

    [level 2] Title: 무인도 여행, Time: 6.19 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 class Solution {
-    int[] dx={-1,1,0,0};
-    int[] dy={0,0,-1,1};
     List<Integer> list=new ArrayList<>();
     char[][] map;
     boolean[][] visited;
-    int m,n;
+    int n;
+    int m;
     public int[] solution(String[] maps) {
         m=maps.length;
         n=maps[0].length();
-        
         map=new char[m][n];
         visited=new boolean[m][n];
+        
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
@@ -22,30 +21,35 @@ class Solution {
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!visited[i][j] && map[i][j]!='X'){
+                if(map[i][j]!='X' && !visited[i][j]){
                     list.add(dfs(i,j));
                 }
             }
         }
         
         if(list.isEmpty()) return new int[]{-1};
-        Collections.sort(list);
+        
+        list.sort(Comparator.naturalOrder());
         
         return list.stream().mapToInt(i->i).toArray();
     }
     int dfs(int x, int y){
+        int cnt=map[x][y]-'0';
         visited[x][y]=true;
-        int sum=(map[x][y]-'0');
+        
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,-1,1};
         
         for(int i=0;i<4;i++){
             int nx=x+dx[i];
             int ny=y+dy[i];
             
-            if(nx>=0 && nx<m && ny>=0 && ny<n &&!visited[nx][ny]&& map[nx][ny]!='X'){
-                sum+=dfs(nx,ny);
+            if(nx>=0 && nx<m && ny>=0 && ny<n && !visited[nx][ny] && map[nx][ny]!='X'){
+                visited[nx][ny]=true;
+                cnt+=dfs(nx,ny);
             }
         }
         
-        return sum;
+        return cnt;
     }
 }
