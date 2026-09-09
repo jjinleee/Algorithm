976cea5c4f115164ece5047505166efaeeb5f201
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-09 22:18:50 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,10 +1,12 @@
 -- 코드를 작성해주세요
-select d.id, d.email, d.first_name, d.last_name
-from developers d
-join (
-select sum(code) as mask
-from skillcodes
-where name in ('Python','C#')
-) r
-where  ( d.skill_code & r.mask) >0
-order by d.id
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE SKILL_CODE &
+(SELECT CODE
+FROM SKILLCODES
+WHERE NAME ='C#' ) > 0 or 
+skill_code & (
+    SELECT CODE
+FROM SKILLCODES
+WHERE NAME ='Python' ) >0
+order by id
