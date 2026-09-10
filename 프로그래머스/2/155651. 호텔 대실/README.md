fc09083ce6419a7218c9a25d96b63da68f84808a
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-10 23:18:32 +0900

    [level 2] Title: 호텔 대실, Time: 11.50 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 80.5 MB, 시간: 5.67 ms
+메모리: 79.5 MB, 시간: 11.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 02일 13:40:34
+2026년 09월 10일 23:18:29
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -3,27 +3,27 @@ import java.util.*;
 class Solution {
     public int solution(String[][] book_time) {
         int answer = 0;
-        Arrays.sort(book_time, (a,b)-> a[0]==b[0] ? a[1].compareTo(b[1]) : a[0].compareTo(b[0]));
+        Arrays.sort(book_time, (a,b)->a[0].compareTo(b[0]));
         PriorityQueue<Integer> q= new PriorityQueue<>();
+        
         for(String[] b : book_time){
-            int start=toTime(b[0]);
-            int end=toTime(b[1])+10;
+            int start=toMin(b[0]);
+            int end=toMin(b[1])+10;
             
-            if(!q.isEmpty() && q.peek()<=start){
+            if(!q.isEmpty() && q.peek()<=start){//사용가능한 룸 있으면
                 q.poll();
                 q.offer(end);
-            } else {
+            } else{
                 q.offer(end);
                 answer++;
-            }  
+            }
+            
         }
         return answer;
     }
-    int toTime(String t){
+    int toMin(String t){
         String[] tmp=t.split(":");
-        int h=Integer.parseInt(tmp[0]);
-        int m=Integer.parseInt(tmp[1]);
         
-        return 60*h+m;
+        return Integer.parseInt(tmp[0])*60+Integer.parseInt(tmp[1]);
     }
 }
