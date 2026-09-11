49315ad9caf412b956c65e6285b6dccf4238d8a5
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-12 00:15:20 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.05 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,23 +1,53 @@
 import java.util.*;
 
 class Solution {
+    class Job{
+        int num;
+        int start;
+        int need;
+        
+        Job(int num, int start, int need){
+            this.num=num;
+            this.start=start;
+            this.need=need;
+        }
+    }
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a,b)-> a[0]-b[0]); //요청시각 순으로 정렬
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)-> a[1]-b[1]);
+        int answer = 0;
+        int n=jobs.length;
+        Arrays.sort(jobs, (a,b)-> a[0]-b[0]);
+        
+        PriorityQueue<Job> pq=new PriorityQueue<>(
+            (a,b)->{
+                if(a.need==b.need){
+                    if(a.start==b.start) return a.num-b.num;
+                    return a.start-b.start;
+                }
+                return a.need-b.need;
+            }
+        );
         
         int time=0;
+        int cnt=0;
         int total=0;
         int i=0;
-        
-        while(i<jobs.length || !pq.isEmpty()){
-            while(i<jobs.length && jobs[i][0]<=time) pq.offer(jobs[i++]);
+        while(cnt<n){
+            //시간이 된것 대기큐에 넣음
+            while(i<n &&jobs[i][0]<=time){
+                pq.offer(new Job(i,jobs[i][0],jobs[i][1])); 
+                i++;
+            }
+            
+            //디스크 실행
             if(!pq.isEmpty()){
-                int[] cur=pq.poll();
-                time+=cur[1];
-                total+=time-cur[0];
+                Job cur=pq.poll();
+                time+=cur.need;
+                total+=(time-cur.start); //반환시각=종료시각-요청시각
+                cnt++;
             } else time=jobs[i][0];
         }
         
-        return total/jobs.length;
+        
+        return total/n;
     }
 }
