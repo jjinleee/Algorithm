18e89c18349f7c80cfbf856febcaa1847c0327c8
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-18 22:50:51 +0900

    [level 2] Title: 피로도, Time: 20.86 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 82.5 MB, 시간: 1.57 ms
+메모리: 92.9 MB, 시간: 20.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 31일 17:02:30
+2026년 09월 18일 22:50:47
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,46 @@
 import java.util.*;
 
 class Solution {
-    int answer=0;
+    List<List<Integer>> orders=new ArrayList<>();
+    int n;
     public int solution(int k, int[][] dungeons) {
-        int n=dungeons.length;
+        int answer = 0;
+        n=dungeons.length;
         boolean[] visited=new boolean[n];
         
-        dfs(k,0,dungeons, visited);
+        dfs(new ArrayList<>(),visited);
+        
+        int left=k;
+        for(List<Integer> list : orders){
+            int cnt=0;
+            left=k;
+            for(int l : list){
+                int need=dungeons[l][0];
+                if(left>=need){
+                    left-=dungeons[l][1];
+                    cnt++;
+                } else{
+                    break;
+                }
+            }
+            answer=Math.max(answer, cnt);
+        }
         
         return answer;
     }
-    void dfs(int k, int cnt, int[][] dungeons, boolean[] visited){
-        answer=Math.max(answer, cnt);
+    void dfs(List<Integer> cur, boolean[] visited){
+        if(cur.size()==n){
+            orders.add(new ArrayList<>(cur));
+            return;
+        }
         
-        for(int i=0;i<dungeons.length;i++){
-            if(!visited[i] && k>=dungeons[i][0]){
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
                 visited[i]=true;
-                dfs(k-dungeons[i][1], cnt+1, dungeons, visited);
+                cur.add(i);
+                dfs(cur, visited);
                 visited[i]=false;
+                cur.remove(cur.size()-1);
             }
         }
     }
