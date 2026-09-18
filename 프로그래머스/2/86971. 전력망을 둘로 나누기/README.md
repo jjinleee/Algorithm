a65fd85642c44de113401c7e293344441ff7ba95
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-18 22:29:41 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.48 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 7.97 ms
+메모리: 83.5 MB, 시간: 6.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 31일 16:20:35
+2026년 09월 18일 22:29:37
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,35 +1,33 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
-        
-        int cnt=wires.length;
-        for(int i=0;i<cnt;i++){
-            List<List<Integer>> graph=new ArrayList<>();
-            for(int j=0;j<=n;j++) graph.add(new ArrayList<>());
-            for(int j=0;j<cnt;j++){
-                if(i==j) continue;
-                graph.get(wires[j][0]).add(wires[j][1]);
-                graph.get(wires[j][1]).add(wires[j][0]);
-            }
-            boolean[] visited= new boolean[n+1];
-            int num=dfs(graph,visited, 1);
-            int sub=Math.abs(2*num-n);
+        int answer = n;
+        for(int i=0;i<wires.length;i++){
+            //초기화
+            List<List<Integer>> list=new ArrayList<>();
+            for(int j=0;j<=n;j++) list.add(new ArrayList<>());
             
-            answer=Math.min(answer, sub);
+            for(int j=0;j<wires.length;j++){
+                if(i==j) continue; //하나씩 건너뜀
+                list.get(wires[j][0]).add(wires[j][1]);
+                list.get(wires[j][1]).add(wires[j][0]);
+            }
+            
+            boolean[] visited=new boolean[n+1];
+            int cnt=dfs(1,list,visited);
+            answer=Math.min(answer, Math.abs(n-2*cnt));//최소차이갱신
         }
-        
         return answer;
     }
-    int dfs(List<List<Integer>> graph, boolean[] visited, int node){
-        int cnt=1;
+    int dfs(int node,List<List<Integer>> list, boolean[] visited){
         visited[node]=true;
+        int cnt=1;
         
-        for(int next: graph.get(node)){
+        for(int next : list.get(node)){
             if(!visited[next]){
-                cnt+=dfs(graph, visited, next);
+                visited[next]=true;
+                cnt+=dfs(next, list, visited);
             }
         }
         
