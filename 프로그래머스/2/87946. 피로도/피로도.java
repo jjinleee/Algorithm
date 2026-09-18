e65a9a5e38d13fabee15f22e747315f2f483ecb4
Jin Lee <93428123+jjinleee@users.noreply.github.com>
2026-09-18 22:54:51 +0900

    [level 2] Title: 피로도, Time: 1.63 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,47 +1,25 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> orders=new ArrayList<>();
-    int n;
+    int answer=0;
+    boolean[] visited;
     public int solution(int k, int[][] dungeons) {
-        int answer = 0;
-        n=dungeons.length;
-        boolean[] visited=new boolean[n];
+        visited=new boolean[dungeons.length];
         
-        dfs(new ArrayList<>(),visited);
-        
-        int left=k;
-        for(List<Integer> list : orders){
-            int cnt=0;
-            left=k;
-            for(int l : list){
-                int need=dungeons[l][0];
-                if(left>=need){
-                    left-=dungeons[l][1];
-                    cnt++;
-                } else{
-                    break;
-                }
-            }
-            answer=Math.max(answer, cnt);
-        }
+        dfs(k,dungeons,0);
+    
         
         return answer;
     }
-    void dfs(List<Integer> cur, boolean[] visited){
-        if(cur.size()==n){
-            orders.add(new ArrayList<>(cur));
-            return;
-        }
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
+    void dfs(int left, int[][] dungeons, int cnt){
+        answer=Math.max(answer, cnt);
+        for(int i=0;i<dungeons.length;i++){
+            if(!visited[i] && left>=dungeons[i][0]){
                 visited[i]=true;
-                cur.add(i);
-                dfs(cur, visited);
+                dfs(left-dungeons[i][1],dungeons, cnt+1);
                 visited[i]=false;
-                cur.remove(cur.size()-1);
             }
         }
+       
     }
 }
